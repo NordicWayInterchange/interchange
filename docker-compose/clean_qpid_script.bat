5f9d15486448afe747fa4a94b884db1ee2010140
Kyrre Kristiansen <kyrre.kristiansen@gmail.com>
2018-10-18 16:32:55 +0200

    Tok bort hard-wired paths i script

--- docker-compose/clean_qpid_script.bat
+++ docker-compose/clean_qpid_script.bat
@@ -1,1 +1,1 @@
-docker run --rm -v c:\Work\borealis\interchange\docker-compose\qpid:/root/qpid -it busybox dos2unix /root/qpid/docker-entrypoint.sh 
+docker run --rm -v %cd%\qpid:/root/qpid -it busybox dos2unix /root/qpid/docker-entrypoint.sh 
