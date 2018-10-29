9cb51bc6de514bb3cdb465a04eb7b32dcb5e57c5
ida-berge <ida.berge@bouvet.no>
2018-10-29 14:37:05 +0100

    Removed @SpringBootTest from IxnMessageTest

--- interchangenode/src/test/java/no/vegvesen/ixn/IxnMessageTest.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/IxnMessageTest.java
@@ -3,17 +3,12 @@ package no.vegvesen.ixn;
 import no.vegvesen.ixn.model.IxnMessage;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class IxnMessageTest {
 
     private final long DEFAULT_TTL = 86_400_000L;
