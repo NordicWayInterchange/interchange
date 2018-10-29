7499a9455b2777318d1eb9361265a4550ee2889a
roger.martinsen <roger.martinsen@bouvet.no>
2018-10-29 10:36:31 +0100

    Db-test is integration test. Wire only db connection for Db-test.

--- interchangenode/src/test/java/no/vegvesen/ixn/geo/GeoLookupIT.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/geo/GeoLookupIT.java
@@ -1,22 +1,35 @@
 package no.vegvesen.ixn.geo;
 
+import no.vegvesen.ixn.TestAutowireApp;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class GeoLookupTest {
+@SpringBootTest(classes = {TestAutowireApp.class, GeoLookup.class})
+@ActiveProfiles("it")
+public class GeoLookupIT {
 
 	@Autowired
-	GeoLookup geoLookup;
+	DataSource dataSource;
+
+	private GeoLookup geoLookup;
+
+	@Before
+	public void setup() {
+		geoLookup = new GeoLookup(new JdbcTemplate(dataSource));
+	}
 
 	@Test
 	public void getPointAtBorderNorwaySwedenReturnsTwoCountries() {
