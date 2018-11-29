b3e72e7504f0478b78c7acdade98be5b5ebd78b8
roger.martinsen <roger.martinsen@bouvet.no>
2018-11-29 13:37:11 +0100

    Verify routing of messages to out-queues

--- interchangenode/src/test/java/no/vegvesen/ixn/QpidIT.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/QpidIT.java
@@ -1,7 +1,8 @@
 package no.vegvesen.ixn;
 
-import no.vegvesen.ixn.messaging.TestIxnMessageConsumer;
+import no.vegvesen.ixn.messaging.CountIxnMessageConsumer;
 import no.vegvesen.ixn.messaging.TestOnrampMessageProducer;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,23 +10,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QpidIT {
 
+    private static final long RECEIVE_TIMEOUT = 2000;
+    private static final String NO_OUT = "NO-out";
+    private static final String SE_OUT = "SE-out";
+    private static final String DLQUEUE = "dlqueue";
+    private static final String NO_OBSTRUCTION = "NO-Obstruction";
     @Autowired
     TestOnrampMessageProducer producer;
 
     @Autowired
-    TestIxnMessageConsumer consumer;
+    CountIxnMessageConsumer consumer;
 
 	@BeforeClass
 	public static void setUp() {
 		TestKeystoreHelper.useTestKeystore();
 	}
 
+    @Before
+    public void before()throws Exception{
+        consumer.emptyQueue(NO_OUT);
+        consumer.emptyQueue(NO_OBSTRUCTION);
+        consumer.emptyQueue(SE_OUT);
+        consumer.emptyQueue(DLQUEUE);
+        Thread.sleep(RECEIVE_TIMEOUT);
+    }
 
-    public void sendMessageOneCountry(){
+    public void sendMessageOneCountry(String messageId){
         long systemTime = System.currentTimeMillis();
         long timeToLive = 3_600_000; // 5 hrs
         long expiration = systemTime+timeToLive;
@@ -35,11 +52,11 @@ public class QpidIT {
                 "Statens Vegvesen",
                 "1234",
                 "Obstruction",
-                "Message with one country",
+                "Message with one country - " + messageId ,
                 expiration);
     }
 
-    public void sendMessageTwoCountries(){
+    public void sendMessageTwoCountries(String s){
         long systemTime = System.currentTimeMillis();
         long timeToLive = 3_600_000;
         long expiration = systemTime+timeToLive;
@@ -49,11 +66,11 @@ public class QpidIT {
                 "Statens Vegvesen",
                 "1234",
                 "Obstruction",
-                "Message with two countries",
+                "Message with two countries - " + s,
                 expiration);
     }
 
-    public void sendBadMessage(){
+    public void sendBadMessage(String s){
         long systemTime = System.currentTimeMillis();
         long timeToLive = 3_600_000;
         long expiration = systemTime+timeToLive;
@@ -64,43 +81,41 @@ public class QpidIT {
                 null,
                 "1234",
                 "Obstruction",
-                "Message with two countries",
+                "Message with two countries - " + s,
                 expiration);
     }
 
-
     @Test
     public void messageToNorwayGoesToNorwayQueue() throws Exception{
-        sendMessageOneCountry(); // NO
-        //Thread.sleep(2*1000);
+        sendMessageOneCountry("1"); // NO
+        Thread.sleep(RECEIVE_TIMEOUT);
         // The queue should have one message
-        //TODO: Receive NO-out-messages
+        assertThat(consumer.numberOfMessages(NO_OUT)).isEqualTo(1);
     }
 
 
     @Test
     public void messageWithTwoCountriesGoesToTwoQueues() throws Exception{
-        sendMessageTwoCountries(); // NO and SE
-        //Thread.sleep(2*1000);
+        sendMessageTwoCountries("3"); // NO and SE
+        Thread.sleep(RECEIVE_TIMEOUT);
         // Each queue should have one message
-        //TODO: Receive NO-out-messages
-        //TODO: Receive SE-out-messages
+        assertThat(consumer.numberOfMessages(SE_OUT)).isEqualTo(1);
+        assertThat(consumer.numberOfMessages(NO_OUT)).isEqualTo(1);
     }
 
     @Test
     public void messageWithCountryAndSituationGoesToRightQueue() throws Exception{
-        sendMessageOneCountry(); // NO and Obstruction
-        //Thread.sleep(2*1000);
-        //
-        //TODO: Receive NO-messages
+        sendMessageOneCountry("2"); // NO and Obstruction
+        Thread.sleep(RECEIVE_TIMEOUT);
+        assertThat(consumer.numberOfMessages(NO_OUT)).isEqualTo(1);
     }
 
     @Test
     public void badMessageGoesDoDeadLetterQueue() throws Exception{
-        sendBadMessage();
-        //Thread.sleep(2*1000);
+        consumer.emptyQueue(DLQUEUE);
+        sendBadMessage("4");
+        Thread.sleep(RECEIVE_TIMEOUT);
         // Expecting one message on dlqueue because message is invalid.
-
-        //TODO: Receive dlqueue
+        assertThat(consumer.numberOfMessages(DLQUEUE)).isEqualTo(1);
     }
 }
---
+++ interchangenode/src/test/java/no/vegvesen/ixn/messaging/CountIxnMessageConsumer.java
@@ -0,0 +1,65 @@
+package no.vegvesen.ixn.messaging;
+
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@EnableJms
+@Component
+public class CountIxnMessageConsumer {
+	private Map<String, List<TextMessage>> receivedMessages = new HashMap<>();
+
+	@JmsListener(destination = "NO-out")
+	public void receiveNoMessage(TextMessage textMessage) throws JMSException {
+		recordReceivedMessage("NO-out", textMessage);
+		debugMessage(textMessage);
+	}
+
+	@JmsListener(destination = "SE-out")
+	public void receiveSeMessage(TextMessage textMessage) throws JMSException {
+		recordReceivedMessage("SE-out", textMessage);
+		debugMessage(textMessage);
+	}
+
+	@JmsListener(destination = "dlqueue")
+	public void receiveDlqueueMessage(TextMessage textMessage) throws JMSException {
+		recordReceivedMessage("dlqueue", textMessage);
+		debugMessage(textMessage);
+	}
+
+	private void debugMessage(TextMessage textMessage) throws JMSException {
+		System.out.println("timestamp:    " + textMessage.getJMSTimestamp());
+		System.out.println("deliveryTime: " + textMessage.getJMSDeliveryTime());
+		long now = System.currentTimeMillis();
+		System.out.println("now         : " + now);
+		System.out.println("latency : " + (now - textMessage.getJMSDeliveryTime()));
+		System.out.println("where1: " + textMessage.getStringProperty("where1"));
+		System.out.println("body: " + textMessage.getText());
+	}
+
+	private synchronized void recordReceivedMessage(String key, TextMessage textMessage) {
+		List<TextMessage> textMessages = receivedMessages.get(key);
+		System.out.println("received message to " + key);
+		if (textMessages == null) {
+			textMessages = new LinkedList<>();
+		}
+		textMessages.add(textMessage);
+		receivedMessages.put(key, textMessages);
+	}
+
+	public synchronized void emptyQueue(String queue) {
+		receivedMessages.put(queue, new LinkedList<>());
+	}
+
+	public int numberOfMessages(String queue) {
+		List<TextMessage> receivedMessagesOfQueue = receivedMessages.get(queue);
+		return receivedMessagesOfQueue == null ? 0 : receivedMessagesOfQueue.size();
+	}
+}
