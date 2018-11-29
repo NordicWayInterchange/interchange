0fe88b4b869a324617dbbb40e378289d56115ae0
Jamie Wiebe <jamie.wiebe@bouvet.no>
2018-11-29 15:30:33 +0100

    qpid add cert-vhost name check

--- qpid/entrypoint.sh
+++ qpid/entrypoint.sh
@@ -4,17 +4,25 @@ pushd ${QPID_WORK}
 
 ENV=${1:-default}
 
-echo "ENTRYPOINT - using vhost file ${VHOST_FILE}"
-echo "ENTRYPOINT - using private key file ${PRIVATE_KEY_FILE}"
-echo "ENTRYPOINT - using certificate file ${CERTIFICATE_FILE}"
-echo "ENTRYPOINT - using users file ${PASSWD_FILE}"
-echo "ENTRYPOINT - using groups file ${GROUPS_FILE}"
+verifyExternalName() {
+    echo "verifying that vhost name and certificate CN match..."
+    local vhost_name="$(jq -r '.name' < ${VHOST_FILE})"
+    local cert_cn="$(openssl x509 -noout -subject -in ${CERTIFICATE_FILE} | sed 's/\//\n/g' | grep '^CN=' | cut -d= -f2)"
+    if [[ ${vhost_name} != ${cert_cn} ]]; then
+        echo "ERROR - vhost ${vhost_name} does not match CN ${cert_cn}"
+        return 1
+    fi
+}
+
+echo "${VHOST_FILE} ${PRIVATE_KEY_FILE} ${CERTIFICATE_FILE} ${PASSWD_FILE} ${GROUPS_FILE}" > /dev/null
 
 mkdir -p ./default/config
 cp ${VHOST_FILE} ./default/config/default.json
 
 if [[ ${ENV} == "dev" ]]; then
     /scripts/generate-keys.sh
+else
+    verifyExternalName
 fi
 
 /usr/local/qpid/bin/qpid-server
