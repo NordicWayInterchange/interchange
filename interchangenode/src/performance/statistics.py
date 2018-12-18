a8fbcec523bdb7c97ae83a83a291c42c7e574543
ida-berge <ida.berge@bouvet.no>
2018-12-18 16:32:28 +0100

    ordered messages

--- interchangenode/src/performance/statistics.py
+++ interchangenode/src/performance/statistics.py
@@ -6,29 +6,39 @@ import sys
 
 
 # A script that outputs basic statistics about the file passed as argument. 
-# Run with ./statistics.py [filename] 
-
-
+# Run with ./statistics.py [filename]
 
 latency_file = open(sys.argv[1], "r")
 latencies = latency_file.read().splitlines()
 
-latencies = map(int, latencies)
+latency_tuples = []
+for message in latencies:
+    latency_tuples.append(tuple(filter(None, message.split())))
 
-print("min: " + str(min(latencies)) + " ms")
-print("max: "+ str(max(latencies)) + " ms")
+int_tuples=[]
+for elem in latency_tuples:
+    int_tuples.append(tuple(map(int, elem)))
 
-avg = sum(latencies) / float(len(latencies))
+sorted_tuples = sorted(int_tuples, key=lambda tup: tup[1])
+
+
+sorted_latencies = [i[0] for i in sorted_tuples]
+
+
+print("min: " + str(min(sorted_latencies)) + " ms")
+print("max: " + str(max(sorted_latencies)) + " ms")
+
+avg = sum(sorted_latencies) / float(len(sorted_latencies))
 print("avg: " + str(avg) + " ms")
 
-median = np.median(np.array(latencies))
+median = np.median(np.array(sorted_latencies))
 print("median: " + str(median)+ " ms")
 
-print("message loss: " + str(2000-len(latencies)) )
+print("message loss: " + str(2000 - len(sorted_latencies)))
 
-print(len(latencies))
+print(len(sorted_latencies))
 
-plt.plot(latencies)
+plt.plot(sorted_latencies)
 plt.xlabel('Number of messages', fontsize=18)
 plt.ylabel('Latency in milliseconds', fontsize=18)
 plt.show()
