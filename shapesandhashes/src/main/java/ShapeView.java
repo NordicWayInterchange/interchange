5a416ef2e7440a519a4ec320afcbcdcb8229e334
Kyrre Kristiansen <kyrre.kristiansen@gmail.com>
2019-01-08 16:39:08 +0100

    litt kode for å kunne se shapefiler

---
+++ shapesandhashes/src/main/java/ShapeView.java
@@ -0,0 +1,40 @@
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ShapeView {
+
+    public static void main(String[] args) throws IOException {
+
+        MapContent map = new MapContent();
+        map.setTitle("Geohashing tiles for Norway w/10km buffer");
+        if (args.length == 0) {
+            //TODO not on my computer!
+            map.addLayer(createLayer(new File("C:\\interchange\\shapefiler\\oresund3\\norway_shape.shp")));
+            map.addLayer(createLayer(new File("C:\\interchange\\shapefiler\\oresund3\\norway_inner_hashes.shp")));
+            map.addLayer(createLayer(new File("C:\\interchange\\shapefiler\\oresund3\\norway_outer_hashes.shp")));
+        } else {
+            for (String filename : args) {
+                map.addLayer(createLayer(new File(filename)));
+            }
+        }
+        JMapFrame.showMap(map);
+    }
+
+    public static Layer createLayer(File file) throws IOException {
+        FileDataStore dataStore = FileDataStoreFinder.getDataStore(file);
+        SimpleFeatureSource featureSource = dataStore.getFeatureSource();
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        return new FeatureLayer(featureSource,style);
+
+    }
+}
