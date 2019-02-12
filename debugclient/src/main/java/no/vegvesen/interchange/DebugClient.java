f7fc901745a01ec81721a1d698c79f7f63819d9f
ida-berge <ida.berge@bouvet.no>
2019-02-12 15:31:25 +0100

    Changed DebugClient to only send one message. Used it for testing federation of messages.

--- debugclient/src/main/java/no/vegvesen/interchange/DebugClient.java
+++ debugclient/src/main/java/no/vegvesen/interchange/DebugClient.java
@@ -17,16 +17,16 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Hashtable;
 
-public class DebugClient implements MessageListener {
-	private static final String GREEN = "[1;32m";
+public class DebugClient{
+
 	private static final String BROWN = "[1;33m";
 	private static final String BLACK = "[0m";
 	private static final String YELLOW = "[33m";
 	private static final String TURQUOISE = "[36m";
-	private static final String GREY = "[37m";
 
-	private static final String USER = System.getProperty("USER");
-	private static final String PASSWORD = System.getProperty("PASSWORD");
+	private static final String USER = "interchange";
+	private static final String PASSWORD = "12345678";
+	private static final int TIME_TO_LIVE_THIRTY_SECONDS = 30000;
 
 	private Connection connection;
 	private Session session;
@@ -61,85 +61,17 @@ public class DebugClient implements MessageListener {
 
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-			MessageConsumer messageConsumer = session.createConsumer(queueR);
 			messageProducer = session.createProducer(queueS);
 			printWithColor(YELLOW, " Waiting for messages..");
-			messageConsumer.setMessageListener(this);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	@Override
-	public void onMessage(Message msg) {
-		try {
-			msg.acknowledge();
-
-			int delay = -1;
-			if (msg.getStringProperty("when") != null) {
-				try {
-					delay = (int) ZonedDateTime.parse(msg.getStringProperty("when")).until(ZonedDateTime.now(), ChronoUnit.MILLIS);
-				} catch (Exception e) {
-					System.err.println("Could not parse \"when\"-field to calculate delay; " + msg.getStringProperty("when"));
-				}
-			}
-
-			printWithColor(GREEN, " Got message from " + msg.getStringProperty("who") + " @ delay=" + delay + "ms:");
-			printWithColor(GREEN, " (Msg type: " + msg.getStringProperty("what") + ")\n");
-			printWithColor(GREEN, " Msg props:");
-			printWithColor(GREEN, " UserID:" + ((JmsMessage) msg).getFacade().getUserId());
-			printWithColor(GREEN, " Type:" + ((JmsMessage) msg).getFacade().getType());
-			printWithColor(GREEN, " Expiration:" + ((JmsMessage) msg).getFacade().getExpiration());
-			printWithColor(GREEN, " App props:");
-			printWithColor(GREEN, " who: " + msg.getStringProperty("who"));
-			printWithColor(GREEN, " how: " + msg.getStringProperty("how"));
-			printWithColor(GREEN, " what: " + msg.getStringProperty("what"));
-			try {
-				printWithColor(GREEN, " lat: " + msg.getStringProperty("lat"));
-				printWithColor(GREEN, " lon: " + msg.getStringProperty("lon"));
-			} catch (Exception ignored) {
-			}
-			try {
-				printWithColor(GREEN, " lat: " + msg.getDoubleProperty("lat"));
-				printWithColor(GREEN, " lon: " + msg.getDoubleProperty("lon"));
-			} catch (Exception ignored) {
-			}
-			try {
-				printWithColor(GREEN, " lat: " + msg.getFloatProperty("lat"));
-				printWithColor(GREEN, " lon: " + msg.getFloatProperty("lon"));
-			} catch (Exception ignored) {
-			}
-			printWithColor(GREEN, " where1: " + msg.getStringProperty("where1"));
-			printWithColor(GREEN, " when: " + msg.getStringProperty("when") + "\n");
-
-			try {
-				printWithColor(GREY, "\t" + ((JmsTextMessage) msg).getText());
-			} catch (ClassCastException e) {
-				@SuppressWarnings("ConstantConditions") JmsBytesMessage jmsBytesMessage = (JmsBytesMessage) msg;
-				for (int i = 0; i < jmsBytesMessage.getBodyLength(); i++) {
-					char c = (char) jmsBytesMessage.readByte();
-					if (c == '\n') {
-						System.out.print(c + "\t");
-					} else {
-						System.out.print(c);
-					}
-				}
-			}
-			printWithColor(GREEN, "\n End of message from " + msg.getStringProperty("who") + "\n");
-			printWithColor(BLACK, " ");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	private static void printWithColor(String color, String s) {
 		System.out.println((char) 27 + color + s);
-	}
-
-	private void sendMessage(String msg) {
-		sendMessage("no", msg);
 	}
 
 	private void sendMessage(String where, String msg) {
@@ -147,8 +79,9 @@ public class DebugClient implements MessageListener {
 			JmsTextMessage message = (JmsTextMessage) session.createTextMessage(msg);
 			message.getFacade().setUserId(USER);
 			message.setStringProperty("who", "Norwegian Public Roads Administration");
-			message.setStringProperty("how", "Datex2");
-			message.setStringProperty("what", "Conditions");
+			message.setStringProperty("how", "datex2");
+			message.setStringProperty("what", "Obstruction");
+			message.setStringProperty("version", "1.0");
 			message.setStringProperty("lat", "63.0");
 			message.setStringProperty("lon", "10.0");
 			message.setStringProperty("where1", where);
@@ -171,9 +104,9 @@ public class DebugClient implements MessageListener {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		String url = "amqp://localhost:5672";
-		String sendQueue = "onramp";
+		String sendQueue = "nwEx";
 		String receiveQueue = "test-out";
 
 		if (args.length == 3) {
@@ -189,55 +122,10 @@ public class DebugClient implements MessageListener {
 
 		BufferedReader commandLine = new BufferedReader(new InputStreamReader(System.in));
 
-		while (true) {
-			try {
-				String s = commandLine.readLine();
+		String message = "Hello world";
+		c.sendMessage("SE", message);
 
-				if (s.equalsIgnoreCase("exit") || s.equalsIgnoreCase("e") || s.equalsIgnoreCase("c")) {
-					c.close();
-					System.out.println("exiting..");
-					System.exit(0);
-				} else if (s.startsWith("s ") || s.startsWith("send ")) {
-					c.sendMessage(s.substring(s.indexOf(" ")));
-				}
-				//example: d onramp thisismymessage
-				else if (s.startsWith("d")) {
-					int firstSpace = s.indexOf(" ");
-					int secSpace = s.indexOf(" ", firstSpace + 1);
-
-					String dest = s.substring(firstSpace, secSpace).trim();
-					System.out.println("dest: " + dest);
-					String msg = s.substring(secSpace).trim();
-					System.out.println("msg: " + msg);
-					c.sendMessage(dest, msg);
-				} else if (s.startsWith("f ")) {
-					String filename = s.substring(s.indexOf(" ")).trim();
-					String everything = readFile(filename);
-					c.sendMessage(everything);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
-	private static String readFile(String filename) throws IOException {
-		FileInputStream is = new FileInputStream(filename);
-		InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-
-		String everything;
-		try (BufferedReader br = new BufferedReader(isr)) {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-
-			while (line != null) {
-				sb.append(line);
-				sb.append(System.lineSeparator());
-				line = br.readLine();
-			}
-			everything = sb.toString();
-		}
-		return everything;
-	}
 
 }
