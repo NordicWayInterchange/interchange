a2949a6dd361cfdbbf8e1eb8e80e2fab05ef3d0e
ida-berge <ida.berge@bouvet.no>
2019-02-13 10:41:43 +0100

    Changed package name.

--- neighbour-server/src/main/java/no/vegvesen/ixn/federation/SpringbootrestapiApplication.java
+++ neighbour-server/src/main/java/no/vegvesen/ixn/federation/SpringbootrestapiApplication.java
@@ -1,21 +1,13 @@
-package idaberge.springbootrestapi;
+package no.vegvesen.ixn.federation;
 
-import ch.qos.logback.core.net.ObjectWriter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import idaberge.springbootrestapi.Model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringbootrestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootrestapiApplication.class, args);
-
-
-
 
 
 	}
