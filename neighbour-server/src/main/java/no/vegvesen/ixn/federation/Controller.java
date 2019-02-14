e5cce9cd4c4d69bae04448c44aec2a8797550b24
ida-berge <ida.berge@bouvet.no>
2019-02-14 15:55:16 +0100

    Persistence of java objects generated by the REST API controller in a CRUD repository using JPA and Hibernate in a H2 in-memory database.

--- neighbour-server/src/main/java/no/vegvesen/ixn/federation/Controller.java
+++ neighbour-server/src/main/java/no/vegvesen/ixn/federation/Controller.java
@@ -1,199 +1,88 @@
 package no.vegvesen.ixn.federation;
 
-import no.vegvesen.ixn.federation.Model.Capability;
 import no.vegvesen.ixn.federation.Model.Interchange;
-import no.vegvesen.ixn.federation.Model.Subscription;
-import no.vegvesen.ixn.federation.Model.DataType;
-import org.json.JSONObject;
+import no.vegvesen.ixn.federation.repository.InterchangeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.*;
-
-/**
- * A Spring Boot REST api controller that uses Qpid's REST api to
- * change/create queues/bindings based on incoming requests.
- */
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Controller{
 
-	Logger logger = LoggerFactory.getLogger(Controller.class);
+	@Autowired
+	InterchangeRepository interchangeRepository;
 
-	private List<Interchange> neighbours = new ArrayList<>();
-	private List<Subscription> subscriptions = new ArrayList<>();
-	private List<Capability> neighbourCapabilities = new ArrayList<>();
-
-	String exchangeURL = "http://localhost:8081/api/latest/exchange/default/qpid.test.io/nwEx";
-	String queueURL = "http://localhost:8081/api/latest/queue/default/qpid.test.io";
-
-	// A method that posts a json object to the Qpid REST api, using a given URI and a given command.
-	public void callQpid(String urlString, String message, String command) throws Exception{
-		String url = urlString + command;
-
-		logger.info("URL: " + url);
-		URL sourceURL = new URL(url);
-		HttpURLConnection source = (HttpURLConnection) sourceURL.openConnection();
-
-		// HTTP Basic Authentication. This must be enabled in the qpid broker for this to work.
-		String userpass = "interchange:12345678";
-		String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
-		source.setRequestProperty ("Authorization", basicAuth);
-
-		source.setRequestMethod("POST");
-		source.setRequestProperty("User-Agent", "Java client");
-		source.setDoOutput(true);
-		source.setRequestProperty("Content-Type", "application/json");
-
-		DataOutputStream wr = new DataOutputStream(source.getOutputStream());
-		wr.writeBytes(message);
-
-		InputStream is;
-
-		try {
-			is = source.getInputStream();
-		} catch (IOException ioe) {
-			if (source instanceof HttpURLConnection) {
-				HttpURLConnection httpConn = (HttpURLConnection) source;
-				int statusCode = httpConn.getResponseCode();
-				if (statusCode != 200) {
-					is = httpConn.getErrorStream();
-				}
-			}
-		}
-
-		logger.info("Response code: " + source.getResponseCode());
-		logger.info("Closing connection...");
-		wr.close();
-	}
-
-	// Updates the binding of a queue to a new binding.
-	public void updateBinding(String binding, String queueName) throws Exception{
-
-		JSONObject json = new JSONObject();
-		json.put("destination", queueName);
-		json.put("bindingKey", "where1");
-		json.put("replaceExistingArguments", true);
-
-		JSONObject innerjson = new JSONObject();
-		innerjson.put("x-filter-jms-selector", binding);
-
-		json.put("arguments", innerjson);
-		String jsonString = json.toString();
-
-		logger.info("Json string: " + jsonString);
-		callQpid(exchangeURL, jsonString, "/bind");
-	}
-
-	// Creates a new queue for a neighbouring Interchange.
-	public void createQueue(Interchange interchange) throws Exception{
-		JSONObject json = new JSONObject();
-		json.put("name", interchange.getId());
-		json.put("durable", true);
-
-		String jsonString = json.toString();
-		logger.info("Creating queue:" + jsonString);
-
-		callQpid(queueURL, jsonString, "/");
-
-		logger.info("Creating binding for queue..");
-		String binding = createBinding(interchange);
-		updateBinding(binding, interchange.getId());
-	}
-
-	// Creates a binding based on the subscriptions of a neighbouring Interchange.
-	public String createBinding(Interchange interchange){
-
-		String binding = "";
-
-		for(Subscription subscription : interchange.getSubscriptions()) {
-
-			binding += "(where1='" + subscription.getCountry() + "' AND ("; // opening parentesis for how/version/what
-
-			for (DataType elem : subscription.getDataSets()) {
-				binding = binding + "how='" + elem.getHow() +
-						"' AND version='" + elem.getVersion() +
-						"' AND ("; // opening parenthesis for what
-
-				for (String situationRecord : elem.getWhat()) {
-					binding = binding + "what='" + situationRecord + "'";
-
-					if (elem.getWhat().indexOf(situationRecord) != elem.getWhat().size() - 1) {
-						binding += " OR ";
-					} else {
-						binding += ")"; // closing parentesis for what
-					}
-				}
-
-				if (subscription.getDataSets().indexOf(elem) != subscription.getDataSets().size() - 1) {
-					binding += " OR ";
-				}
-			}
-
-			binding += ")"; // closing parenthesis for how/version/what
-
-			if(interchange.getSubscriptions().indexOf(subscription) != interchange.getSubscriptions().size()-1 ){
-				// subscription is not the last element in the list of subscriptions, add OR.
-				binding += ") OR ";
-			}else{
-				binding += ")";
-			}
-		}
-		logger.info("Queue binding:" + binding);
-		return binding;
-	}
-
+	private Logger logger = LoggerFactory.getLogger(Controller.class);
 
 	@PostMapping("/updateSubscription")
-	public Interchange updateSubscription(@RequestBody Interchange interchange)throws Exception{
+	public Interchange updateSubscription(@RequestBody Interchange interchange){
 
-		if(!neighbours.contains(interchange)) {
-			// We have not seen this interchange before.
-			createInterchange(interchange);
-		}else {
-			// We have seen the interchange before. Update the bindings.
-			String binding = createBinding(interchange);
-			updateBinding(binding, interchange.getId());
+		Interchange mod = interchangeRepository.findByInterchangeId(interchange.getId());
+
+		if(mod == null){
+			interchangeRepository.save(interchange);
+			logger.info("*** NEW INTERCHANGE **");
+		}else{
+			logger.info("---- UPDATING INTERCHANGE ----");
+			mod.setSubscriptions(interchange.getSubscriptions());
+			interchangeRepository.save(mod);
 		}
 
 		return interchange;
 	}
+
+	@PostMapping("/updateCapabilities")
+	public Interchange updateCapabilities(@RequestBody Interchange interchange){
+
+		Interchange mod = interchangeRepository.findByInterchangeId(interchange.getId());
+
+		if(mod == null){
+			interchangeRepository.save(interchange);
+			logger.info("*** NEW INTERCHANGE **");
+		}else{
+			logger.info("---- UPDATING INTERCHANGE ----");
+			mod.setCapabilities(interchange.getCapabilities());
+			interchangeRepository.save(mod);
+		}
+
+		return interchange;
+	}
+
+
+	@GetMapping("/getSubscription")
+	public Interchange getSubscription(@RequestBody Interchange interchange){
+		// TODO: return this node's subscriptions
+		return interchange;
+	}
+
+	@GetMapping("/getCapabilities")
+	public Interchange getCapabilities(@RequestBody Interchange interchange){
+		// TODO: return this node's capabilities
+		return interchange;
+	}
+
 
 	@GetMapping("/neighbours")
 	public List<Interchange> getAllSubscriptions(){
-		// Returns a list of all neighbouring interchanges.
-		return neighbours;
-	}
+		// Return a list of all the neighbours for debugging purposes.
 
-	@PostMapping("/interchange")
-	public Interchange createInterchange(@RequestBody Interchange interchange)throws Exception{
-		// Check if Interchange already exists as a neighbour.
-		// If not - add it to the list of neighbours and create a new queue.
-		if(!neighbours.contains(interchange)){
-			neighbours.add(interchange);
-			createQueue(interchange);
+		Iterable<Interchange> list = interchangeRepository.findAll();
+
+		List<Interchange> ret = new ArrayList<>();
+
+		for (Interchange i : list){
+			ret.add(i);
 		}
-		return interchange;
+
+		return ret;
 	}
 
-
-	@PostMapping("/neighbourCapabilities")
-	public Capability createNewCapability(@RequestBody Capability capability){
-		// TODO
-		neighbourCapabilities.add(capability);
-		return capability;
-	}
-
-	@GetMapping("/capabilities")
-	public List<Capability> getNeighbourCapabilities(){
-		// TODO
-		return neighbourCapabilities;
-	}
 
 }
