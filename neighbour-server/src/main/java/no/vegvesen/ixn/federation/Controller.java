5d308155ae571fd991d3c2e94e83b0cf903a1ce1
ida-berge <ida.berge@bouvet.no>
2019-02-19 09:41:00 +0100

    Moved federation model classes to separate module. Updated this module to depend on the neighbour-model module. Starting tomcat server on port 8090

--- neighbour-server/src/main/java/no/vegvesen/ixn/federation/Controller.java
+++ neighbour-server/src/main/java/no/vegvesen/ixn/federation/Controller.java
@@ -1,30 +1,32 @@
 package no.vegvesen.ixn.federation;
 
-import no.vegvesen.ixn.federation.Model.Interchange;
-import no.vegvesen.ixn.federation.repository.InterchangeRepository;
+import no.vegvesen.ixn.federation.model.Model.Capability;
+import no.vegvesen.ixn.federation.model.Model.Interchange;
+import no.vegvesen.ixn.federation.model.Model.Subscription;
+import no.vegvesen.ixn.federation.model.Repository.InterchangeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-public class Controller{
+public class Controller {
 
 	@Autowired
 	InterchangeRepository interchangeRepository;
 
-	private Logger logger = LoggerFactory.getLogger(Controller.class);
+	Logger logger = LoggerFactory.getLogger(Controller.class);
 
 	@PostMapping("/updateSubscription")
 	public Interchange updateSubscription(@RequestBody Interchange interchange){
 
-		Interchange mod = interchangeRepository.findByInterchangeId(interchange.getId());
+		Interchange mod = interchangeRepository.findByInterchangeId(interchange.getName());
 
 		if(mod == null){
 			interchangeRepository.save(interchange);
@@ -41,7 +43,7 @@ public class Controller{
 	@PostMapping("/updateCapabilities")
 	public Interchange updateCapabilities(@RequestBody Interchange interchange){
 
-		Interchange mod = interchangeRepository.findByInterchangeId(interchange.getId());
+		Interchange mod = interchangeRepository.findByInterchangeId(interchange.getName());
 
 		if(mod == null){
 			interchangeRepository.save(interchange);
@@ -56,20 +58,69 @@ public class Controller{
 	}
 
 
-	@GetMapping("/getSubscription")
-	public Interchange getSubscription(@RequestBody Interchange interchange){
-		// TODO: return this node's subscriptions
-		return interchange;
+	@RequestMapping(path="/{ixnId}/subscriptions")
+	public List<Subscription> getSubscription(@PathVariable String ixnId){
+		// TODO: return all the subscriptions
+
+		Interchange mod = interchangeRepository.findByInterchangeId(ixnId);
+
+		if(mod != null){
+			return mod.getSubscriptions();
+		}
+
+		return Arrays.asList();
 	}
 
-	@GetMapping("/getCapabilities")
-	public Interchange getCapabilities(@RequestBody Interchange interchange){
+	@RequestMapping(path="/{ixnId}/capabilities")
+	public List<Capability> getCapabilities(@PathVariable String ixnId){
 		// TODO: return this node's capabilities
-		return interchange;
+
+		Interchange mod = interchangeRepository.findByInterchangeId(ixnId);
+
+		if(mod != null){
+			return mod.getCapabilities();
+		}
+
+		return Arrays.asList();
+	}
+
+	@RequestMapping(path="/{ixnId}")
+	public Interchange getInterchange(@PathVariable String ixnId){
+		// TODO: return this node's capabilities
+
+		Interchange mod = interchangeRepository.findByInterchangeId(ixnId);
+
+		if(mod != null){
+			return mod;
+		}
+
+		return null;
 	}
 
 
-	@GetMapping("/neighbours")
+	@RequestMapping(path = "/checkForChanges/{timestamp}")
+	public List<Interchange> checkForChanges(@PathVariable String timestamp){
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd HH:mm:ss");
+		LocalDateTime formattedTime = LocalDateTime.parse(timestamp, formatter);
+
+		String queryAllOlderThan = formattedTime.format(formatter);
+
+		logger.info("Querying for Interchanges last updated before: " + queryAllOlderThan);
+
+		try {
+
+			return interchangeRepository.findOlderThan(timestamp);
+		}catch(Exception e){
+			logger.info(e.getClass().getName());
+			logger.info("Timestamp: " + timestamp + ", found no objects older than this time. ");
+		}
+
+		return Arrays.asList();
+	}
+
+
+	@GetMapping("/printNeighbours")
 	public List<Interchange> getAllSubscriptions(){
 		// Return a list of all the neighbours for debugging purposes.
 
@@ -81,7 +132,12 @@ public class Controller{
 			ret.add(i);
 		}
 
-		return ret;
+		if(ret.size() == 0){
+			return Arrays.asList();
+		}else{
+			return ret;
+		}
+
 	}
 
 
--- neighbour-server/src/main/java/no/vegvesen/ixn/federation/SpringbootrestapiApplication.java
+++ neighbour-server/src/main/java/no/vegvesen/ixn/federation/SpringbootrestapiApplication.java
@@ -2,8 +2,12 @@ package no.vegvesen.ixn.federation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages={"no.vegvesen.ixn.federation.model.*"})
+@EntityScan(basePackages={"no.vegvesen.ixn.federation.model.*"})
 public class SpringbootrestapiApplication {
 
 	public static void main(String[] args) {
