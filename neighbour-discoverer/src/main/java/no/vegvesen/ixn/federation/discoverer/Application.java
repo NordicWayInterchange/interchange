778ef6b6e68f5282220d331c3dbdcaa2be9daf4c
ida-berge <ida.berge@bouvet.no>
2019-02-22 10:56:21 +0100

    A spring boot appliation to run the NeighbourDiscoverer

---
+++ neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/Application.java
@@ -0,0 +1,18 @@
+package no.vegvesen.ixn.federation.discoverer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableJpaRepositories(basePackages={"no.vegvesen.ixn.federation.model.*"})
+@EntityScan(basePackages={"no.vegvesen.ixn.federation.model.*"})
+public class Application {
+
+	public static void main(String[] args){
+		SpringApplication.run(Application.class);
+	}
+}
