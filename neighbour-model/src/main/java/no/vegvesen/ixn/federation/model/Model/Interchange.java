f2d362371319225965dfe6f86bc047e574a5f8c7
ida-berge <ida.berge@bouvet.no>
2019-02-22 10:33:19 +0100

    An Interchange has a hostname and a port nr.

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/Model/Interchange.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/Model/Interchange.java
@@ -1,11 +1,10 @@
 package no.vegvesen.ixn.federation.model.Model;
 
-import org.hibernate.annotations.Fetch;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -15,29 +14,45 @@ public class Interchange {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ixn_generator")
 	@SequenceGenerator(name="ixn_generator", sequenceName = "ixn_seq", allocationSize=50)
-	@Column(name="id")
+	@Column(name="ixn_id")
 	Integer id;
 
 	@Column(name = "name", unique = true)
 	String name;
 
+	@JsonIgnore
+	@Column(name = "hostname")
+	String hostname;
+
+	@JsonIgnore
+	@Column(name = "port_nr")
+	String portNr;
+
 	@OneToMany(cascade= CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "id")
+	@JoinColumn(name = "ixn_id")
 	Set<Capability> capabilities;
 
 	@OneToMany(cascade= CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "id")
+	@JoinColumn(name = "ixn_id")
 	Set<Subscription> subscriptions;
+
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "ixn_id")
+	Set<Subscription> fedIn;
 
 	@UpdateTimestamp
 	private LocalDateTime lastUpdated;
 
 	public Interchange(){}
 
-	public Interchange(String name, Set<Capability> capabilities, Set<Subscription> subscriptions) {
+	public Interchange(String name, Set<Capability> capabilities, Set<Subscription> subscriptions, Set<Subscription> fedIn, String hostname, String portNr) {
 		this.name = name;
+		this.hostname = hostname;
+		this.portNr = portNr;
 		this.capabilities = capabilities;
 		this.subscriptions = subscriptions;
+		this.fedIn = fedIn;
 	}
 
 	public Set<Capability> getCapabilities() {
@@ -54,6 +69,22 @@ public class Interchange {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public String getPortNr() {
+		return portNr;
+	}
+
+	public void setPortNr(String portNr) {
+		this.portNr = portNr;
 	}
 
 	public Set<Subscription> getSubscriptions() {
