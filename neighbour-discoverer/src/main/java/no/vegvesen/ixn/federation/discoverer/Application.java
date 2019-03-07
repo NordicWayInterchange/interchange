e82fb68f5310c28a7ffe368884c5370ccc056eec
ida-berge <ida.berge@bouvet.no>
2019-03-07 13:29:15 +0100

    New Bean rest template added to Application

--- neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/Application.java
+++ neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/Application.java
@@ -3,14 +3,22 @@ package no.vegvesen.ixn.federation.discoverer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaRepositories(basePackages={"no.vegvesen.ixn.federation.repository"})
 @EntityScan(basePackages={"no.vegvesen.ixn.federation.model"})
 public class Application {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 
 	public static void main(String[] args){
 		SpringApplication.run(Application.class);
