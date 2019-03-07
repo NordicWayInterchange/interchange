642695c34f0011c0d403e24e2c212b06d18035d1
roger.martinsen <roger.martinsen@bouvet.no>
2019-03-07 10:24:47 +0100

    Provide the jms-selector-filter as a parameter in the test. Use only one queue to test the routing.

--- interchangenode/src/test/java/no/vegvesen/ixn/GeoHashJmsRoutingTest.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/GeoHashJmsRoutingTest.java
@@ -20,8 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GeoHashJmsRoutingTest extends IxnBaseIT {
 
-	private static final String SEND_QUEUE = "nwEx";
-	private static final String READ_QUEUE = "NO-out";
+	private static final String EXCHANGE_NAME = "my-queue";
 
 	private static int randomPort;
 	private static String URI;
@@ -44,8 +43,7 @@ public class GeoHashJmsRoutingTest extends IxnBaseIT {
 	@Before
 	public void setUp() throws Exception {
 		session = getSession(URI, USER, PASSWORD);
-		messageProducer = session.createProducer(new JmsQueue(SEND_QUEUE));
-		consumer = session.createConsumer(new JmsQueue(READ_QUEUE));
+		messageProducer = session.createProducer(new JmsQueue(EXCHANGE_NAME));
 	}
 
 	@AfterClass
@@ -67,6 +65,7 @@ public class GeoHashJmsRoutingTest extends IxnBaseIT {
 	@Test
 	public void messageWithMatchingGeohashIsRouted() throws Exception {
 		JmsTextMessage message = createTestMessage(".aaaa123213213");
+		consumer = createConsumerWithFilter("geohash like '%.aaaa%'");
 		messageProducer.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
 		Message receivedMessage = readMessage(1000L); // wait long enough for the Interchange routing to complete
 		assertThat(receivedMessage).isNotNull();
@@ -76,6 +75,7 @@ public class GeoHashJmsRoutingTest extends IxnBaseIT {
 	@Test
 	public void messageWithoutMatchingGeohashIsNotRouted() throws Exception {
 		JmsTextMessage message = createTestMessage(".aaac123213213");
+		consumer = createConsumerWithFilter("geohash like '%.aaaa%'");
 		messageProducer.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
 		Message receivedMessage = readMessage(1000L); // wait long enough for the Interchange routing to complete
 		assertThat(receivedMessage).isNull();
@@ -85,6 +85,7 @@ public class GeoHashJmsRoutingTest extends IxnBaseIT {
 	@Test
 	public void messageForAreaMatchingGeohashAreaIsRouted() throws Exception {
 		JmsTextMessage messageRouted = createTestMessage(".cccf123213213.cccb123213213.cccg123213213.ccch123213213");
+		consumer = createConsumerWithFilter("geohash like '%.ccca%' OR geohash like '%.cccb%' OR geohash like '%.cccc%'");
 		messageProducer.send(messageRouted, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
 		Message receivedMessage = readMessage(1000L); // wait long enough for the Interchange routing to complete
 		assertThat(receivedMessage).isNotNull();
@@ -94,11 +95,14 @@ public class GeoHashJmsRoutingTest extends IxnBaseIT {
 	@Test
 	public void messageForAreaNotMatchingGeohashAreaIsNotRouted() throws Exception {
 		JmsTextMessage messageNotRouted = createTestMessage(".cccf123213213.ccci123213213.cccg123213213.ccch123213213");
+		consumer = createConsumerWithFilter("geohash like '%.ccca%' OR geohash like '%.cccb%' OR geohash like '%.cccc%'");
 		messageProducer.send(messageNotRouted, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
 		Message receivedMessage = readMessage(1000L); // wait long enough for the Interchange routing to complete
 		assertThat(receivedMessage).isNull();
 		session.close();
 	}
 
-
+	private MessageConsumer createConsumerWithFilter(String jmsSelectorFilter) throws JMSException {
+		return session.createConsumer(new JmsQueue(EXCHANGE_NAME), jmsSelectorFilter);
+	}
 }
