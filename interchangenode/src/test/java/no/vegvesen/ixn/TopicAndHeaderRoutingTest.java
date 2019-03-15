cfcae4c7b4110021840d3ab0eeee2321767e2bc6
roger.martinsen <roger.martinsen@bouvet.no>
2019-03-15 16:00:50 +0100

    Alternative binding to bind both topic and headers exchange

--- interchangenode/src/test/java/no/vegvesen/ixn/TopicAndHeaderRoutingTest.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/TopicAndHeaderRoutingTest.java
@@ -1,6 +1,7 @@
 package no.vegvesen.ixn;
 
 import no.vegvesen.ixn.broker.EmbeddedBroker;
+import org.apache.qpid.jms.JmsQueue;
 import org.apache.qpid.jms.JmsTopic;
 import org.apache.qpid.jms.message.JmsTextMessage;
 import org.junit.AfterClass;
@@ -18,8 +19,6 @@ import static org.assertj.core.api.Assertions.assertThat;
  **/
 
 public class TopicAndHeaderRoutingTest extends IxnBaseIT {
-
-	private static final String EXCHANGE_NAME = "topicEx";
 
 	private static String URI;
 	private static final String USER = "admin";
@@ -45,17 +44,42 @@ public class TopicAndHeaderRoutingTest extends IxnBaseIT {
 		broker.stop();
 	}
 
-	private void sendToTopic() throws JMSException {
-		messageProducer = session.createProducer(new JmsTopic(EXCHANGE_NAME));
-		JmsTextMessage message = (JmsTextMessage) session.createTextMessage("hello " + getClass().getSimpleName());
+	private void sendToTopic(String destinationName) throws JMSException {
+		messageProducer = session.createProducer(new JmsTopic(destinationName));
+		JmsTextMessage message = (JmsTextMessage) session.createTextMessage("hello " + messageProducer.getDestination().toString());
 		message.getFacade().setUserId("admin");
 		messageProducer.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
 	}
 
 	@Test
-	public void messageWithExchangeNameIsRouted() throws Exception {
-		consumer = session.createConsumer(new JmsTopic(EXCHANGE_NAME));
-		sendToTopic();
+	public void headerFiskTorsk() throws JMSException {
+		consumer = session.createConsumer(new JmsQueue("headerFiskTorsk"));
+		sendWithHeader("fisk", "torsk");
+		assertThat(consumer.receive(1000L)).isNotNull();
+	}
+
+	private void sendWithHeader(String how, String what) throws JMSException {
+		messageProducer = session.createProducer(new JmsTopic("topicEx"));
+		JmsTextMessage message = (JmsTextMessage) session.createTextMessage("hello " + messageProducer.getDestination().toString());
+		message.getFacade().setUserId("admin");
+		message.setStringProperty("how", how);
+		message.setStringProperty("what", what);
+		messageProducer.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+	}
+
+	@Test
+	public void messageWithSameTopicIsRouted() throws Exception {
+		consumer = session.createConsumer(new JmsTopic("topicEx/fisk"));
+		sendToTopic("topicEx/fisk");
+		Message receivedMessage = consumer.receive(1000L);
+		assertThat(receivedMessage).isNotNull();
+		session.close();
+	}
+
+	@Test
+	public void messageWithBroaderTopicIsReceived() throws Exception {
+		consumer = session.createConsumer(new JmsTopic("topicEx/fisk/#"));
+		sendToTopic("topicEx/fisk/torsk");
 		Message receivedMessage = consumer.receive(1000L);
 		assertThat(receivedMessage).isNotNull();
 		session.close();
