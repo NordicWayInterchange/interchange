5843caad35a66ea5d0b9b01ef9b2879eda36fa21
roger.martinsen <roger.martinsen@bouvet.no>
2019-03-27 14:07:35 +0100

    Build docker images for federation modules

--- neighbour-discoverer/entrypoint.sh
+++ neighbour-discoverer/entrypoint.sh
@@ -4,4 +4,4 @@ echo "ENTRYPOINT - connecting to PGSQL server ${POSTGRES_URI}"
 
 java -Dspring.datasource.url=${POSTGRES_URI} \
      -Dinterchange.node-provider.name=${SERVER_NAME} \
-     -jar neighbour-server.jar
+     -jar neighbour-discoverer.jar
