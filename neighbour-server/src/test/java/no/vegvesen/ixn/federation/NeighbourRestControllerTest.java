186fcb0d50bd9d55d17c9587f5b193499bc7da6c
ida-berge <ida.berge@bouvet.no>
2019-03-29 10:35:54 +0100

    empty test for now to avoid problems in mvn install

--- neighbour-server/src/test/java/no/vegvesen/ixn/federation/NeighbourRestControllerTest.java
+++ neighbour-server/src/test/java/no/vegvesen/ixn/federation/NeighbourRestControllerTest.java
@@ -1,11 +1,17 @@
 package no.vegvesen.ixn.federation;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class NeighbourRestControllerTest {
+
+	@Test
+	public void firstTest(){
+
+	}
 
 
 }
