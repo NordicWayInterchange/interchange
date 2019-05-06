f1ca75b0bd5e987680204ec3d481879805416739
Kyrre Kristiansen <kyrre.kristiansen@gmail.com>
2019-05-06 11:06:37 +0200

    Integration test to check that we can get the correct rows from the database.

--- message-forwarder/src/test/java/no/vegvesen/ixn/federation/forwarding/NeighbourFetcherIT.java
+++ message-forwarder/src/test/java/no/vegvesen/ixn/federation/forwarding/NeighbourFetcherIT.java
@@ -4,6 +4,7 @@ import no.vegvesen.ixn.federation.model.Capabilities;
 import no.vegvesen.ixn.federation.model.Interchange;
 import no.vegvesen.ixn.federation.model.SubscriptionRequest;
 import no.vegvesen.ixn.federation.repository.InterchangeRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +23,27 @@ public class NeighbourFetcherIT {
     @Autowired
     private InterchangeRepository repository;
 
-    @Test
-    public void testFetchingNeighbourWithCorrectStatus() {
-
+    @Before
+    public void setUp() {
         repository.save(new Interchange("Intermittent Interchange",
                 new Capabilities(Capabilities.CapabilitiesStatus.KNOWN, Collections.emptySet()),
                 new SubscriptionRequest(SubscriptionRequest.SubscriptionRequestStatus.ESTABLISHED,Collections.EMPTY_SET),
                 new SubscriptionRequest(SubscriptionRequest.SubscriptionRequestStatus.ESTABLISHED,Collections.EMPTY_SET)));
+        repository.save(new Interchange("Other Interchange",
+                new Capabilities(Capabilities.CapabilitiesStatus.KNOWN, Collections.emptySet()),
+                new SubscriptionRequest(SubscriptionRequest.SubscriptionRequestStatus.REQUESTED,Collections.EMPTY_SET),
+                new SubscriptionRequest(SubscriptionRequest.SubscriptionRequestStatus.ESTABLISHED,Collections.EMPTY_SET)));
+
+    }
+
+    @Test
+    public void testFetchingNeighbourWithCorrectStatus() {
+
 
         NeighbourFetcher fetcher = new NeighbourFetcher(repository);
 
         List<Interchange> interchanges = fetcher.listNeighbourCandidates();
-        assertThat(interchanges).size().isEqualTo(0);
+        assertThat(interchanges).size().isEqualTo(1);
 
 
     }
