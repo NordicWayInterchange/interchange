5440148ae9fcf5a171c3c99fb941f25e8b3e202d
roger.martinsen <roger.martinsen@bouvet.no>
2019-05-09 10:30:21 +0200

    Separate certificate authority certificate generated

--- qpid/entrypoint.sh
+++ qpid/entrypoint.sh
@@ -7,23 +7,28 @@ ENV=${1:-default}
 verifyExternalName() {
     echo "verifying that vhost name and certificate CN match..."
     local vhost_name="$(jq -r '.name' < ${VHOST_FILE})"
-    local cert_cn="$(openssl x509 -noout -subject -in ${CERTIFICATE_FILE} | sed 's/\//\n/g' | grep '^CN=' | cut -d= -f2)"
+    local cert_cn="$(openssl x509 -noout -subject -in ${SERVER_CERTIFICATE_FILE} | sed 's/\//\n/g' | grep '^CN=' | cut -d= -f2)"
     if [[ ${vhost_name} != ${cert_cn} ]]; then
-        echo "ERROR - vhost ${vhost_name} does not match CN ${cert_cn}"
+        echo "ERROR - vhost '${vhost_name}' does not match CN '${cert_cn}'"
         return 1
     fi
 }
-
-echo "${VHOST_FILE} ${PRIVATE_KEY_FILE} ${CERTIFICATE_FILE} ${PASSWD_FILE} ${GROUPS_FILE}" > /dev/null
 
 mkdir -p ./default/config
 cp ${VHOST_FILE} ./default/config/default.json
 
 if [[ ${ENV} == "dev" ]]; then
-    /scripts/generate-keys.sh
+    echo "${CA_CN} ${SERVER_CN}" > /dev/null
+    export CA_CERTIFICATE_FILE=/tmp/keys/${CA_CN}.crt
+    export CA_PRIVATE_KEY_FILE=/tmp/keys/${CA_CN}.key
+    export SERVER_CERTIFICATE_FILE=/tmp/keys/${SERVER_CN}.crt
+    export SERVER_PRIVATE_KEY_FILE=/tmp/keys/${SERVER_CN}.key
+    /scripts/generate-keys.sh /tmp
 else
     verifyExternalName
 fi
+
+echo "${VHOST_FILE} ${SERVER_PRIVATE_KEY_FILE} ${SERVER_CERTIFICATE_FILE} ${CA_CERTIFICATE_FILE} ${PASSWD_FILE} ${GROUPS_FILE}" > /dev/null
 
 for file in $(find ${QPID_WORK} -name *.lck); do
     set -x
--- qpid/generate-keys.sh
+++ qpid/generate-keys.sh
@@ -7,32 +7,35 @@ SERVER_CN=${SERVER_CN:-localhost}
 USER_CNS=${USER_CNS:-king_gustaf king_harald}
 KEYS_DIR=../tmp/keys
 
-serverCert() {
-    echo "generating server side certificate with common name '${SERVER_CN}'"
-    openssl req -new -newkey rsa:2048 -nodes -keyout server.key \
-        -out server.csr -subj "/CN=${SERVER_CN}/O=Nordic Way/C=NO"
-    openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-    rm -f server.csr server.srl
-    keytool -import -trustcacerts -file server.crt -keystore truststore.jks \
+genCaCert() {
+    local cn=$1
+    local key_file=${cn}.key
+    local cert_file=${cn}.crt
+    echo "generating CA certificate with common name '${cn}'"
+    openssl req -new -newkey rsa:2048 -nodes -keyout ${key_file} \
+        -out ca.csr -subj "/CN=${cn}/O=Nordic Way/C=NO"
+    openssl x509 -req -days 365 -in ca.csr -signkey ${key_file} -out ${cert_file}
+    rm -f ca.csr ca.srl
+    keytool -import -trustcacerts -file ${cert_file} -keystore truststore.jks \
         -storepass password -noprompt
-    openssl pkcs12 -export -name server -out server.p12 -inkey server.key \
-        -in server.crt -CAfile server.crt -password pass:password
 }
 
-userCert() {
+generateCertificate() {
     local cn=$1
-    echo "generating user side certificate with common name '${cn}'"
+    echo "generating certificate with common name '${cn}'"
     openssl req -new -newkey rsa:2048 -nodes -keyout ${cn}.key -out ${cn}.csr \
         -subj "/CN=${cn}/O=Nordic Way/C=NO"
-    openssl x509 -req -days 365 -in ${cn}.csr -out ${cn}.crt -CA server.crt \
-        -CAkey server.key -CAcreateserial -sha256
-    rm -f ${cn}.csr
+    openssl x509 -req -days 365 -in ${cn}.csr -out ${cn}.crt -CA ${CA_CERTIFICATE_FILE} \
+        -CAkey ${CA_PRIVATE_KEY_FILE} -CAcreateserial -sha256
+    rm -f ${cn}.csr ${cn}.srl
     openssl pkcs12 -export -out ${cn}.p12 -inkey ${cn}.key -in ${cn}.crt \
-        -CAfile server.crt -password pass:password
+        -CAfile ${CA_CERTIFICATE_FILE} -password pass:password
 }
 
 rm -rf ${KEYS_DIR} && mkdir -p ${KEYS_DIR} && pushd ${KEYS_DIR}
-serverCert
+
+genCaCert ${CA_CN}
+generateCertificate ${SERVER_CN}
 for user in ${USER_CNS}; do
-    userCert ${user}
+    generateCertificate ${user}
 done
