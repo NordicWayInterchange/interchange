f5add6807e6e3b628745ea722b95ffa6e15e2ca3
ida-berge <ida.berge@bouvet.no>
2019-05-20 10:37:23 +0200

    Deleted sql files that are not in use. Deleted integration test with no content

--- neighbour-server/src/test/java/no/vegvesen/ixn/federation/NeighbourRestApiApplicationIT.java
+++ neighbour-server/src/test/java/no/vegvesen/ixn/federation/NeighbourRestApiApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public abstract class NeighbourRestApiApplicationIT {
+public class NeighbourRestApiApplicationIT {
 
 	@Test
 	public void contextLoads() {
