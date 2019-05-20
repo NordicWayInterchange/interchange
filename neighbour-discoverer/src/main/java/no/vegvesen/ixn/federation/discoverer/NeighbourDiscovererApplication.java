54fb887b6fa20924dd802f0aa5eb8ce1f041ead9
ida-berge <ida.berge@bouvet.no>
2019-05-20 10:21:02 +0200

    Work in progress: Implementing SSL in communication between client and server

--- neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/NeighbourDiscovererApplication.java
+++ neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/NeighbourDiscovererApplication.java
@@ -1,27 +1,52 @@
 package no.vegvesen.ixn.federation.discoverer;
 
+import no.vegvesen.ixn.ssl.KeystoreDetails;
+import no.vegvesen.ixn.ssl.KeystoreType;
+import no.vegvesen.ixn.ssl.SSLContextFactory;
+import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+import javax.net.ssl.SSLContext;
+
+@SpringBootApplication(scanBasePackages = "no.vegvesen.ixn.federation")
 @EnableScheduling
 @EnableJpaRepositories(basePackages={"no.vegvesen.ixn.federation.repository"})
 @EntityScan(basePackages={"no.vegvesen.ixn.federation.model"})
 public class NeighbourDiscovererApplication {
 
+
+	// WORK IN PROGRESS: ssl in communication between server and client
+
+	private static SSLContext createSSLContext() {
+
+		String keystoreFilename = "/Users/ida.berge/Documents/Borealis/interchange/routing-configurer/src/test/resources/jks/localhost.p12";
+		String truststoreFilename = "/Users/ida.berge/Documents/Borealis/interchange/routing-configurer/src/test/resources/jks/truststore.jks";
+
+		return SSLContextFactory.sslContextFromKeyAndTrustStores(
+				new KeystoreDetails(keystoreFilename, "password", KeystoreType.PKCS12, "password"),
+				new KeystoreDetails(truststoreFilename, "password", KeystoreType.JKS));
+
+	}
+
+	@Bean
+	HttpClient createHttpClient() {
+		return HttpClients.custom().setSSLContext(createSSLContext()).build();
+	}
+
 	@Bean
 	public RestTemplate restTemplate() {
-		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
-		return new RestTemplate(requestFactory);
+
+		return new RestTemplate(new HttpComponentsClientHttpRequestFactory(createHttpClient()));
 	}
+
 
 
 
