7ce0ddb70e58de9fd371706d5fbd19691b4dbc75
ida-berge <ida.berge@bouvet.no>
2019-05-27 13:30:34 +0200

    Class not abstract. Reverting application properties.

--- neighbour-discoverer/src/test/java/no/vegvesen/ixn/federation/discoverer/NeighbourDiscovererIT.java
+++ neighbour-discoverer/src/test/java/no/vegvesen/ixn/federation/discoverer/NeighbourDiscovererIT.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public abstract class NeighbourDiscovererIT {
+public class NeighbourDiscovererIT {
 
 	// TODO: WORK IN PROGRESS
 
