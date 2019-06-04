8a4270ce98ebdab4147676fdf834e6f2dc335b12
ida-berge <ida.berge@bouvet.no>
2019-06-04 09:33:04 +0200

    SSL config for neighbour discoverer updated to use properties class

--- neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/SSLContextConfig.java
+++ neighbour-discoverer/src/main/java/no/vegvesen/ixn/federation/discoverer/SSLContextConfig.java
@@ -3,6 +3,7 @@ package no.vegvesen.ixn.federation.discoverer;
 import no.vegvesen.ixn.ssl.KeystoreDetails;
 import no.vegvesen.ixn.ssl.KeystoreType;
 import no.vegvesen.ixn.ssl.SSLContextFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,26 +15,17 @@ import javax.net.ssl.SSLContext;
 @Profile("prod")
 public class SSLContextConfig {
 
-	@Value("${neighbour.ssl.trust-store-password}")
-	String truststorePassword;
-	@Value("${neighbour.ssl.trust-store-type}")
-	String truststoreType;
-	@Value("${neighbour.ssl.trust-store}")
-	String truststoreName;
+	private NeighbourSSLProperties sslProperties;
 
-	@Value("${neighbour.ssl.key-store-type}")
-	String keystoreType;
-	@Value("${neighbour.ssl.key-store}")
-	String keystoreName;
-	@Value("${neighbour.ssl.key-store-password}")
-	String keystorePassword;
-	@Value("${neighbour.ssl.key-password}")
-	String keyPassword;
+	@Autowired
+	public SSLContextConfig(NeighbourSSLProperties sslProperties){
+		this.sslProperties = sslProperties;
+	}
 
 	@Bean
 	public SSLContext sslContextFromKeystoreAndTruststore() {
 		return SSLContextFactory.sslContextFromKeyAndTrustStores(
-				new KeystoreDetails(keystoreName, keystorePassword, KeystoreType.valueOf(keystoreType), keyPassword),
-				new KeystoreDetails(truststoreName, truststorePassword, KeystoreType.valueOf(truststoreType)));
+				new KeystoreDetails(sslProperties.getKeyStore(), sslProperties.getKeyStorePassword(), KeystoreType.valueOf(sslProperties.getKeyStoreType()),sslProperties.getKeyPassword()),
+				new KeystoreDetails(sslProperties.getTrustStore(), sslProperties.getTrustStorePassword(), KeystoreType.valueOf(sslProperties.getTrustStoreType())));
 	}
 }
