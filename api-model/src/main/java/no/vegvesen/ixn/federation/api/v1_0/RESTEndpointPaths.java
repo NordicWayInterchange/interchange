bfba1a95eddc57c9be4e465faacbd847c33ffa4e
ida-berge <ida.berge@bouvet.no>
2019-06-05 14:46:09 +0200

    subscription polling path starts with "/"

--- api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/RESTEndpointPaths.java
+++ api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/RESTEndpointPaths.java
@@ -4,5 +4,5 @@ public final class RESTEndpointPaths {
 
 	public static final String CAPABILITIES_PATH = "/capabilities";
 	public static final String SUBSCRIPTION_PATH = "/subscription";
-	public static final String SUBSCRIPTION_POLLING_PATH = "{ixnName}/subscription/{subscriptionId}";
+	public static final String SUBSCRIPTION_POLLING_PATH = "/{ixnName}/subscription/{subscriptionId}";
 }
