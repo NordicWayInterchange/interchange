b7fd8138727eb27c28c85fe4caadce9d0d84b0bb
ida-berge <ida.berge@bouvet.no>
2019-06-11 16:32:45 +0200

    removed import of ActiveProfiles

--- neighbour-model/src/test/java/no/vegvesen/ixn/federation/repository/InterchangeRepositorySelectorIT.java
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/repository/InterchangeRepositorySelectorIT.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
