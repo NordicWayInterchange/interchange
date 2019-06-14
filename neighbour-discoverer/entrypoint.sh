65122e49be17ef8b32ab954da96ffe25917cf97c
ida-berge <ida.berge@bouvet.no>
2019-06-14 10:02:38 +0200

    Added a fake DNS server to the systemtest so other containers can use real DNS lookup instead of a mock.

--- neighbour-discoverer/entrypoint.sh
+++ neighbour-discoverer/entrypoint.sh
@@ -3,12 +3,13 @@
 echo "ENTRYPOINT - connecting to PGSQL server ${POSTGRES_URI}"
 
 java -Dspring.datasource.url=${POSTGRES_URI} \
-     -Ddns.mock.mock-names=${DNS_MOCK_NAMES} \
      -Ddns.type=${DNS_TYPE} \
+     -Ddns.domain-name=${DOMAIN_NAME}\
+     -Ddns.control-channel-port=${CTRL_CHNL_PORT} \
      -Dneighbour.ssl.trust-store-password=password \
      -Dneighbour.ssl.trust-store=/jks/keys/truststore.jks \
      -Dneighbour.ssl.key-store=/jks/keys/${SERVER_NAME}.p12 \
      -Dneighbour.ssl.key-store-password=password \
      -Dneighbour.ssl.key-password=password \
-     -Dinterchange.node-provider.name=${SERVER_NAME} \
+     -Dinterchange.node-provider.name=${NODE_PROVIDER_NAME} \
      -jar neighbour-discoverer.jar
--- neighbour-server/entrypoint.sh
+++ neighbour-server/entrypoint.sh
@@ -3,12 +3,13 @@
 echo "ENTRYPOINT - connecting to PGSQL server ${POSTGRES_URI}"
 
 java -Dspring.datasource.url=${POSTGRES_URI} \
-     -Ddns.mock.mock-names=${DNS_MOCK_NAMES} \
      -Ddns.type=${DNS_TYPE} \
+     -Ddns.domain-name=${DOMAIN_NAME}\
+     -Ddns.control-channel-port=${CTRL_CHNL_PORT} \
      -Dserver.ssl.key-store=/jks/keys/${SERVER_NAME}.p12\
      -Dserver.ssl.key-store-password=password \
      -Dserver.ssl.key-alias=${SERVER_NAME} \
      -Dserver.ssl.trust-store=/jks/keys/truststore.jks \
      -Dserver.ssl.trust-store-password=password \
-     -Dinterchange.node-provider.name=${SERVER_NAME} \
+     -Dinterchange.node-provider.name=${NODE_PROVIDER_NAME} \
      -jar neighbour-server.jar
