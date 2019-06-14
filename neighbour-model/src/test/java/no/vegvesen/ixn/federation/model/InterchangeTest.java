2b77badd54fa57846f820e86d6175aa2745391c2
ida-berge <ida.berge@bouvet.no>
2019-06-14 15:28:10 +0200

    Removed references to domain name. Using fully qualified domain name as interchange name.

--- neighbour-model/src/test/java/no/vegvesen/ixn/federation/model/InterchangeTest.java
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/model/InterchangeTest.java
@@ -9,8 +9,7 @@ public class InterchangeTest {
 
 	@Test
 	public void getControlChannelUrlWithDomainNameAndSpecifiedNonDefaultPorts() {
-		Interchange fullDomainName = new Interchange("my-host", null, null, null);
-		fullDomainName.setDomainName("my-domain.top");
+		Interchange fullDomainName = new Interchange("my-host.my-domain.top", null, null, null);
 		fullDomainName.setControlChannelPort("1234");
 		fullDomainName.setMessageChannelPort("5678");
 		assertThat(fullDomainName.getControlChannelUrl("/alive")).isEqualTo("https://my-host.my-domain.top:1234/alive");
@@ -19,8 +18,7 @@ public class InterchangeTest {
 
 	@Test
 	public void getMessageAndControlChannelUrlWithDomainNameAndSpecifiedDefaultPorts() {
-		Interchange fullDomainName = new Interchange("my-host", null, null, null);
-		fullDomainName.setDomainName("my-domain.top");
+		Interchange fullDomainName = new Interchange("my-host.my-domain.top", null, null, null);
 		fullDomainName.setControlChannelPort("443");
 		fullDomainName.setMessageChannelPort("5671");
 		assertThat(fullDomainName.getControlChannelUrl("/alive")).isEqualTo("https://my-host.my-domain.top/alive");
@@ -29,8 +27,7 @@ public class InterchangeTest {
 
 	@Test
 	public void getMessageAndControlChannelUrlWithDomainNameDefaultPorts() {
-		Interchange fullDomainName = new Interchange("my-host", null, null, null);
-		fullDomainName.setDomainName("my-domain.top");
+		Interchange fullDomainName = new Interchange("my-host.my-domain.top", null, null, null);
 		assertThat(fullDomainName.getControlChannelUrl("/alive")).isEqualTo("https://my-host.my-domain.top/alive");
 		assertThat(fullDomainName.getMessageChannelUrl()).isEqualTo("amqps://my-host.my-domain.top/");
 	}
@@ -38,17 +35,10 @@ public class InterchangeTest {
 	@Test
 	public void expectedUrlIsCreated() {
 		String expectedURL = "https://ericsson.itsinterchange.eu:8080/";
-		Interchange ericsson = new Interchange("ericsson", null, null, null);
+		Interchange ericsson = new Interchange("ericsson.itsinterchange.eu", null, null, null);
 		ericsson.setControlChannelPort("8080");
-		ericsson.setDomainName("itsinterchange.eu");
 		String actualURL = ericsson.getControlChannelUrl("/");
 		assertThat(expectedURL).isEqualTo(actualURL);
-	}
-
-	@Test(expected = DiscoveryException.class)
-	public void domainNameValidationStartWithoutDot() {
-		Interchange ericsson = new Interchange("ericsson", null, null, null);
-		ericsson.setDomainName(".itsinterchange.eu");
 	}
 
 	@Test(expected = DiscoveryException.class)
