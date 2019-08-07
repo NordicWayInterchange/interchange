7149a0a4f4d00dba5097b395a154bb2a815f8df9
ida-berge <ida.berge@bouvet.no>
2019-08-07 15:07:20 +0200

    Updated to use the Neighbour object instead of Interchange.

--- api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/CapabilityTransformer.java
+++ api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/CapabilityTransformer.java
@@ -1,32 +1,52 @@
 package no.vegvesen.ixn.federation.api.v1_0;
 
 import no.vegvesen.ixn.federation.model.Capabilities;
-import no.vegvesen.ixn.federation.model.Interchange;
+import no.vegvesen.ixn.federation.model.Neighbour;
+import no.vegvesen.ixn.federation.model.ServiceProvider;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CapabilityTransformer {
 
-	public CapabilityApi interchangeToCapabilityApi(Interchange interchange){
+	public CapabilityApi neighbourToCapabilityApi(Neighbour neighbour){
 
 		CapabilityApi capabilityApi = new CapabilityApi();
-		capabilityApi.setName(interchange.getName());
-		capabilityApi.setCapabilities(interchange.getCapabilities().getDataTypes());
+		capabilityApi.setName(neighbour.getName());
+		capabilityApi.setCapabilities(neighbour.getCapabilities().getDataTypes());
 
 		return capabilityApi;
-
 	}
 
-	public Interchange capabilityApiToInterchange(CapabilityApi capabilityApi){
+	public Neighbour capabilityApiToNeighbour(CapabilityApi capabilityApi){
 
-		Interchange interchange = new Interchange();
-		interchange.setName(capabilityApi.getName());
+		Neighbour neighbour = new Neighbour();
+		neighbour.setName(capabilityApi.getName());
 
 		Capabilities capabilitiesObject = new Capabilities();
 		capabilitiesObject.setDataTypes(capabilityApi.getCapabilities());
-		interchange.setCapabilities(capabilitiesObject);
+		neighbour.setCapabilities(capabilitiesObject);
 
-		return interchange;
+		return neighbour;
+	}
 
+	public ServiceProvider capabilityApiToServiceProvider(CapabilityApi capabilityApi){
+
+		ServiceProvider serviceProvider = new ServiceProvider();
+		serviceProvider.setName(capabilityApi.getName());
+
+		Capabilities serviceProviderCapabilities = new Capabilities();
+		serviceProviderCapabilities.setDataTypes(capabilityApi.getCapabilities());
+		serviceProvider.setCapabilities(serviceProviderCapabilities);
+
+		return serviceProvider;
+	}
+
+	public CapabilityApi serviceProviderToCapabilityApi(ServiceProvider serviceProvider){
+
+		CapabilityApi capabilityApi = new CapabilityApi();
+		capabilityApi.setName(serviceProvider.getName());
+		capabilityApi.setCapabilities(serviceProvider.getCapabilities().getDataTypes());
+
+		return capabilityApi;
 	}
 }
