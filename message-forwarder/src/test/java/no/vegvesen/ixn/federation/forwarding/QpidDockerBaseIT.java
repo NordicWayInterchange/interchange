94fb74b45d97223a15037cc870b152c66d943e0b
roger.martinsen <roger.martinsen@bouvet.no>
2019-09-25 12:12:10 +0200

    Lookup docker image folder in CIRCLE CI or local build, log resolved path

--- message-forwarder/src/test/java/no/vegvesen/ixn/federation/forwarding/QpidDockerBaseIT.java
+++ message-forwarder/src/test/java/no/vegvesen/ixn/federation/forwarding/QpidDockerBaseIT.java
@@ -10,32 +10,33 @@ public class QpidDockerBaseIT {
 	private static Logger logger = LoggerFactory.getLogger(QpidDockerBaseIT.class);
 	static final int HTTPS_PORT = 443;
 	static final int AMQPS_PORT = 5671;
-	private static final String PROJECT_CHECKOUT_DIRECTORY = "interchange";
 	private static final String CI_WORKDIR = "CIRCLE_WORKING_DIRECTORY";
 	static final Path QPID_DOCKER_PATH = getQpidDockerPath();
 
 	private static Path getQpidDockerPath() {
-		Path projectRoot = null;
+		String projectFolder = "interchange";
+
 		String ciWorkdir = System.getenv(CI_WORKDIR);
 		if (ciWorkdir != null) {
-			projectRoot = Paths.get(ciWorkdir);
-			logger.debug("Workdir {}, resolved to {}", ciWorkdir, projectRoot.toAbsolutePath().toString());
-			return projectRoot.resolve("qpid");
-		} else {
-			Path run = Paths.get(".").toAbsolutePath();
-			logger.debug("Resolving qpid path from run path: " + run.toAbsolutePath().toString());
-			while (projectRoot == null && run.getParent() != null) {
-				if (run.endsWith(PROJECT_CHECKOUT_DIRECTORY)) {
-					projectRoot = run;
-				}
-				run = run.getParent();
-			}
-			if (projectRoot != null) {
-				Path qpid = projectRoot.resolve("qpid");
-				logger.debug("Resolved qpid path {}", qpid.toAbsolutePath().toString());
-				return qpid;
-			}
-			throw new RuntimeException("Could not resolve path to qpid docker folder in parent folder of " + run.toString());
+			Path ciWorkdirPath = Paths.get(ciWorkdir);
+			logger.debug("ci workdir {} resolves to {} full path", ciWorkdir, ciWorkdirPath.getFileName().toString(), ciWorkdirPath.toAbsolutePath());
+			projectFolder = ciWorkdirPath.getFileName().toString();
 		}
+
+		Path run = Paths.get(".").toAbsolutePath();
+		logger.debug("Resolving qpid path from run path: " + run.toAbsolutePath().toString());
+		Path projectRoot = null;
+		while (projectRoot == null && run.getParent() != null) {
+			if (run.endsWith(projectFolder)) {
+				projectRoot = run;
+			}
+			run = run.getParent();
+		}
+		if (projectRoot != null) {
+			Path qpid = projectRoot.resolve("qpid");
+			logger.debug("Resolved qpid path {}", qpid.toAbsolutePath().toString());
+			return qpid;
+		}
+		throw new RuntimeException("Could not resolve path to qpid docker folder in parent folder of " + run.toString());
 	}
 }
