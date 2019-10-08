d6d5144de5b9723462e4b5325201b74330360607
roger.martinsen <roger.martinsen@bouvet.no>
2019-10-08 14:58:37 +0200

    gcloud docker --authorize-only

--- build-push-images.sh
+++ build-push-images.sh
@@ -9,9 +9,11 @@ for image in ${IMAGES}; do
     pushd ${image}
 
     docker build -t ${image}:${TAG} .
-    docker tag ${image}:${TAG} ${image}:${BRANCH}
     docker tag ${image}:${TAG} ${REGISTRY}/${image}:${TAG}
-    gcloud docker -- push ${REGISTRY}/${image}:${TAG}
-    gcloud docker -- push  ${REGISTRY}/${image}:${BRANCH}
+    docker push ${REGISTRY}/${image}:${TAG}
+
+    docker tag ${image}:${TAG} ${image}:${BRANCH}
+    docker tag ${image}:${BRANCH} ${REGISTRY}/${image}:${BRANCH}
+    docker push ${REGISTRY}/${image}:${BRANCH}
     popd
 done
