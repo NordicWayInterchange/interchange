998d9b8eb6ded66c04b3b76a0c2113dba986529a
roger.martinsen <roger.martinsen@bouvet.no>
2019-10-10 12:07:02 +0200

    Local docker build is not for pushing to registry

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -eu
 
-REGISTRY=eu.gcr.io/nordic-way-aad182cc
 TAG="$(git rev-parse --short HEAD)"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-PUSH=${PUSH:-false}
 IMAGES="interchangenode qpid postgis message-forwarder neighbour-discoverer neighbour-server routing-configurer onboard-server"
 
 mvn clean install -PPKG
@@ -12,20 +10,13 @@ for image in ${IMAGES}; do
     pushd ${image}
 
     docker build -t ${image}:${TAG} .
-    docker tag ${image}:${TAG} ${REGISTRY}/${image}:${TAG}
-
     docker tag ${image}:${TAG} ${image}:${BRANCH}
-    docker tag ${image}:${BRANCH} ${REGISTRY}/${image}:${BRANCH}
 
-    if [[ ${PUSH} == true ]]; then
-        docker push ${REGISTRY}/${image}:${TAG}
-        docker push ${REGISTRY}/${image}:${BRANCH}
-    fi
     popd
 done
 
 echo ""
 for image in ${IMAGES}; do
-    echo "${REGISTRY}/${image}:${TAG}"
-    echo "${REGISTRY}/${image}:${BRANCH}"
+    echo ${image}:${TAG}"
+    echo ${image}:${BRANCH}"
 done
