d97ff48b956608f6d97191cff4898095eb08cdda
roger.martinsen <roger.martinsen@bouvet.no>
2019-10-16 15:08:39 +0200

    System test pulls images for the branch from docker registry

--- systemtest.sh
+++ systemtest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eu
 
-mvn package -PPKG
+#Set environment variable to choose what docker image tag to use in the test
+export SYSTEMTEST_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+echo Running system test on branch $SYSTEMTEST_BRANCH
+
 [ -f tmp/keys/remote.itsinterchange.eu.p12 ] || docker-compose -f systemtest-keys.yml up --build
 docker-compose -f systemtest.yml up --build
