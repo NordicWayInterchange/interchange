a23b013af106dc5da6f88962a45ff97c78d53b60
roger.martinsen <roger.martinsen@bouvet.no>
2019-10-30 14:07:19 +0100

    Build docker images on master with circle ci

---
+++ build-push-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+REGISTRY=eu.gcr.io/nordic-way-aad182cc
+TAG="$(git rev-parse --short HEAD)"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+IMAGES="interchangenode qpid postgis"
+
+for image in ${IMAGES}; do
+    pushd ${image}
+
+    docker build -t ${image}:${TAG} .
+    docker tag ${image}:${TAG} ${REGISTRY}/${image}:${TAG}
+    docker push ${REGISTRY}/${image}:${TAG}
+
+    docker tag ${image}:${TAG} ${image}:${BRANCH}
+    docker tag ${image}:${BRANCH} ${REGISTRY}/${image}:${BRANCH}
+    docker push ${REGISTRY}/${image}:${BRANCH}
+    popd
+done
