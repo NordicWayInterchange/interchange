431a4e443aab5c41b42496c50ac40cbf56a0e5a7
roger.martinsen <roger.martinsen@bouvet.no>
2019-11-11 14:33:26 +0100

    Use testcontainer for GeoLookup

--- interchangenode/src/test/java/no/vegvesen/ixn/geo/GeoLookupIT.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/geo/GeoLookupIT.java
@@ -1,12 +1,19 @@
 package no.vegvesen.ixn.geo;
 
+import no.vegvesen.ixn.federation.forwarding.DockerBaseIT;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -16,7 +23,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GeoLookupIT {
+@ContextConfiguration(initializers = {GeoLookupIT.Initializer.class})
+public class GeoLookupIT extends DockerBaseIT {
+
+	@ClassRule
+	public static GenericContainer postgisContainer = getPostgisContainer("interchangenode/src/test/docker/postgis");
+
+	static class Initializer
+			implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues.of(
+					"spring.datasource.url: jdbc:postgresql://localhost:" + postgisContainer.getMappedPort(JDBC_PORT) + "/geolookup",
+					"spring.datasource.username: geolookup",
+					"spring.datasource.password: geolookup",
+					"spring.datasource.driver-class-name: org.postgresql.Driver"
+			).applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
 
 	@Autowired
 	DataSource dataSource;
@@ -39,4 +62,5 @@ public class GeoLookupIT {
 		List<String> countries = geoLookup.getCountries(60d, 4.0d);
 		assertThat(countries).hasSize(0);
 	}
+
 }
