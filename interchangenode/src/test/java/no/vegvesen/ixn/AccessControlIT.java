177b4cd3402abef914e9feba567765b8e789ef70
roger.martinsen <roger.martinsen@bouvet.no>
2019-11-13 08:55:36 +0100

    Simplify test by using Source and Sink

--- interchangenode/src/test/java/no/vegvesen/ixn/AccessControlIT.java
+++ interchangenode/src/test/java/no/vegvesen/ixn/AccessControlIT.java
@@ -1,14 +1,14 @@
 package no.vegvesen.ixn;
 
 import no.vegvesen.ixn.federation.forwarding.DockerBaseIT;
-import org.apache.qpid.jms.message.JmsTextMessage;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 
-import javax.jms.*;
+import javax.jms.JMSException;
+import javax.jms.JMSSecurityException;
 
 /**
  * Verifies access control lists where username comes from the common name (CN) of the user certificate.
@@ -23,8 +23,12 @@ public class AccessControlIT extends DockerBaseIT {
 	private static final String JKS_IMPOSTER_KING_HARALD_P_12 = "jks/imposter_king_harald.p12";
 	private static final String TRUSTSTORE_JKS = "jks/truststore.jks";
 
+	//Queues used in testing
 	private static final String SE_OUT = "SE-out";
 	private static final String NO_OUT = "NO-out";
+	private static final String ONRAMP = "onramp";
+	private static final String NW_EX = "nwEx";
+	private static final String TEST_OUT = "test-out";
 
 	@ClassRule
 	public static GenericContainer localContainer = getQpidContainer("qpid", "jks", "localhost.crt", "localhost.crt", "localhost.key");
@@ -37,81 +41,40 @@ public class AccessControlIT extends DockerBaseIT {
 
 	@Test(expected = JMSSecurityException.class)
 	public void testKingHaraldCanNotConsumeSE_OUT() throws Exception {
-		IxnContext context = new IxnContext(getQpidURI(), "onramp", SE_OUT);
-
-		Connection connection = context.createConnection(TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
-		Destination queueR = context.getReceiveQueue();
-		connection.start();
-
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		session.createConsumer(queueR);
+		Sink seOut = new Sink(getQpidURI(), SE_OUT, TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
+		seOut.start();
 	}
 
 	@Test(expected = JMSSecurityException.class)
 	public void testKingGustafCanNotConsumeNO_OUT() throws Exception {
-		IxnContext context = new IxnContext(getQpidURI(), "onramp", NO_OUT);
-		Connection connection = context.createConnection(TestKeystoreHelper.sslContext(JKS_KING_GUSTAF_P_12, TRUSTSTORE_JKS));
-		Destination queueR = context.getReceiveQueue();
-		connection.start();
-
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-		session.createConsumer(queueR);
+		Sink noOut = new Sink(getQpidURI(), NO_OUT, TestKeystoreHelper.sslContext(JKS_KING_GUSTAF_P_12, TRUSTSTORE_JKS));
+		noOut.start();
 	}
 
 	@Test(expected = JMSSecurityException.class)
 	public void KingHaraldCanNotConsumeFromOnramp() throws Exception {
-		IxnContext context = new IxnContext(getQpidURI(), "onramp", "onramp");
-		Connection connection = context.createConnection(TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
-		Destination queueR = context.getReceiveQueue();
-		connection.start();
-
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		session.createConsumer(queueR);
+		Sink onramp = new Sink(getQpidURI(), ONRAMP, TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
+		onramp.start();
 	}
 
 
 	@Test(expected = JMSException.class)
 	public void KingHaraldCanNotSendToNwEx() throws Exception {
-		IxnContext context = new IxnContext(getQpidURI(), "nwEx", NO_OUT);
-
-		Connection connection = context.createConnection(TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
-		Destination queueR = context.getReceiveQueue();
-		Destination queueS = context.getSendQueue();
-		connection.start();
-
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-		session.createConsumer(queueR);
-		MessageProducer messageProducer = session.createProducer(queueS);
-
-		JmsTextMessage message = (JmsTextMessage) session.createTextMessage("hello world");
-		message.getFacade().setUserId("king_harald");
-		message.setStringProperty("who", "Norwegian Public Roads Administration");
-		message.setStringProperty("how", "Datex2");
-		message.setStringProperty("what", "Conditions");
-		message.setStringProperty("lat", "63.0");
-		message.setStringProperty("lon", "10.0");
-		message.setStringProperty("where", "NO");
-
-		messageProducer.send(message, DeliveryMode.PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+		Source nwEx = new Source(getQpidURI(), NW_EX, TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
+		nwEx.start();
+		nwEx.send("Not allowed");
 	}
 
 	@Test(expected = JMSException.class)
 	public void userWithInvalidCertificateCannotConnect() throws Exception {
-		IxnContext context = new IxnContext(getQpidURI(), "onramp", "test-out");
-
-		Connection connection = context.createConnection(TestKeystoreHelper.sslContext(JKS_IMPOSTER_KING_HARALD_P_12, TRUSTSTORE_JKS));
-		connection.start();
+		Sink testOut = new Sink(getQpidURI(), TEST_OUT, TestKeystoreHelper.sslContext(JKS_IMPOSTER_KING_HARALD_P_12, TRUSTSTORE_JKS));
+		testOut.start();
 	}
 
 	@Test
 	public void userWithValidCertificateCanConnect() throws Exception {
-		IxnContext context = new IxnContext(getQpidURI(), "onramp", NO_OUT);
-
-		Connection connection = context.createConnection(TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
-		connection.start();
-		connection.close();
+		Sink noOut = new Sink(getQpidURI(), NO_OUT, TestKeystoreHelper.sslContext(JKS_KING_HARALD_P_12, TRUSTSTORE_JKS));
+		noOut.start();
+		noOut.createConsumer();
 	}
-
 }
