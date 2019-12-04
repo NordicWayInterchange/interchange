f079c78dbde79e0cf28158ee21af8a245c335baa
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2019-12-04 13:44:19 +0100

    Done the rest of the mandatory properties.

--- interchangenode/src/main/java/no/vegvesen/ixn/model/IxnBaseMessage.java
+++ interchangenode/src/main/java/no/vegvesen/ixn/model/IxnBaseMessage.java
@@ -6,6 +6,7 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 
 //TODO abstract?
+//TODO have userId, but how do I solve TTL?
 public class IxnBaseMessage {
 
     private final Message message;
@@ -31,19 +32,45 @@ public class IxnBaseMessage {
         return getStringProperty(CommonApplicationProperties.PROTOCOL_VERSION);
     }
 
+    public String getMessageType() throws JMSException {
+        return getStringProperty(CommonApplicationProperties.MESSAGE_TYPE);
+    }
+
     public Message getMessage() {
         return message;
     }
 
     //TODO should probably be abstract, implemented in inherited classes.
+    //TODO could use the mandatory field in CommonApplicationProperties.
     public boolean isValid() throws JMSException {
         return  getUserId() != null &&
                 getPublisherName() != null &&
                 getOriginatingCountry() != null &&
-                getProtocolVersion() != null;
+                getProtocolVersion() != null &&
+                getMessageType() != null &&
+                hasDoubleProperty(CommonApplicationProperties.LATITUDE) &&
+                hasDoubleProperty(CommonApplicationProperties.LONGITUDE);
     }
 
     private String getStringProperty(CommonApplicationProperties property) throws JMSException {
         return message.getStringProperty(property.name());
     }
+
+    private double getDoubleProperty(CommonApplicationProperties property) throws JMSException {
+        return message.getDoubleProperty(property.name());
+    }
+
+    /*
+        NumberFormatException is thrown when the header does not exist.
+        see https://docs.oracle.com/javaee/6/api/javax/jms/Message.html
+     */
+    private boolean hasDoubleProperty(CommonApplicationProperties property) throws JMSException {
+        try {
+            getDoubleProperty(property);
+            return true; //No exception thrown
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
 }
