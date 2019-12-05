dbcf559282a75f05cd25a56a69338f6d59fe22dc
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2019-12-05 14:47:16 +0100

    Created MessageFactory, that creates the correct type of message wrapper based on the messageType header.

---
+++ interchangenode/src/main/java/no/vegvesen/ixn/model/MessageFactoryException.java
@@ -0,0 +1,7 @@
+package no.vegvesen.ixn.model;
+
+public class MessageFactoryException extends RuntimeException {
+   public MessageFactoryException (String message) {
+      super(message);
+   }
+}
