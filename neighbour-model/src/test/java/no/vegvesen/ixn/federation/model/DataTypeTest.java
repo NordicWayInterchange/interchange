55789802b5a2aab90857bef2fc36f9a1aabeabf1
roger.martinsen <roger.martinsen@bouvet.no>
2019-12-16 14:30:05 +0100

    DataType has generic map of header / values in capabilities

--- neighbour-model/src/test/java/no/vegvesen/ixn/federation/model/DataTypeTest.java
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/model/DataTypeTest.java
@@ -1,16 +1,18 @@
 package no.vegvesen.ixn.federation.model;
 
 import no.vegvesen.ixn.federation.api.v1_0.Datex2DataTypeApi;
+import no.vegvesen.ixn.properties.MessageProperty;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Set;
 
 public class DataTypeTest {
 
-    private DataType firstDataType = new DataType(Datex2DataTypeApi.DATEX_2, "NO");
-    private DataType secondDataType = new DataType(Datex2DataTypeApi.DATEX_2, "SE");
+    private DataType firstDataType = new DataType(getDatexHeaders( "NO"));
+    private DataType secondDataType = new DataType(getDatexHeaders("SE"));
 
 	@Test
 	public void dataTypeInCapabilitiesReturnsTrue(){
@@ -24,4 +26,10 @@ public class DataTypeTest {
 		Assert.assertFalse(firstDataType.isContainedInSet(testCapabilities));
 	}
 
+	private HashMap<String, String> getDatexHeaders(String originatingCountry) {
+		HashMap<String, String> datexHeaders = new HashMap<>();
+		datexHeaders.put(MessageProperty.MESSAGE_TYPE.getName(), Datex2DataTypeApi.DATEX_2);
+		datexHeaders.put(MessageProperty.ORIGINATING_COUNTRY.getName(), originatingCountry);
+		return datexHeaders;
+	}
 }
--- neighbour-model/src/test/java/no/vegvesen/ixn/federation/transformer/CapabilityTransformerTest.java
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/transformer/CapabilityTransformerTest.java
@@ -3,9 +3,11 @@ package no.vegvesen.ixn.federation.transformer;
 import no.vegvesen.ixn.federation.api.v1_0.DataTypeApi;
 import no.vegvesen.ixn.federation.api.v1_0.Datex2DataTypeApi;
 import no.vegvesen.ixn.federation.model.DataType;
+import no.vegvesen.ixn.properties.MessageProperty;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,7 +19,6 @@ public class CapabilityTransformerTest {
 
 	@Test
 	public void capabilityApiIsConvertedToDataTypes(){
-		final String messageType = "DATEX2";
 		final String originatingCountry = "NO";
 		final String publicationType = "MeasuredDataPublication";
 		final String [] publicationSubType = new String[] {"SiteMeasurements", "TravelTimeData"};
@@ -28,16 +29,15 @@ public class CapabilityTransformerTest {
 		Set<DataType> dataTypes = capabilityTransformer.dataTypeApiToDataType(capabilities);
 
 		assertThat(dataTypes).hasSize(1);
-		assertThat(dataTypes).containsExactly(new DataType(messageType,originatingCountry, publicationType, publicationSubTypeString));
+		assertThat(dataTypes).containsExactly(getDatexHeaders(originatingCountry, publicationType, publicationSubTypeString));
 	}
 
 	@Test
 	public void dataTypeIsConvertedToApi(){
-		final String messageType = "DATEX2";
 		final String originatingCountry = "NO";
 		final String publicationType = "MeasuredDataPublication";
 		final String publicationSubType = ",SiteMeasurements,TravelTimeData,";
-		DataType dataType = new DataType(messageType, originatingCountry, publicationType, publicationSubType);
+		DataType dataType = getDatexHeaders(originatingCountry, publicationType, publicationSubType);
 
 		Set<DataTypeApi> dataTypeApis = capabilityTransformer.dataTypeToDataTypeApi(Collections.singleton(dataType));
 
@@ -52,12 +52,31 @@ public class CapabilityTransformerTest {
 	public void unknownDataTypeIsConvertedToApi() {
 		final String messageType = "myfantasticmessagetype";
 		final String originatingCountry = "ON";
-		DataType dataType = new DataType(Datex2DataTypeApi.DATEX_2, messageType, originatingCountry, null);
+		HashMap<String, String> dataTypeHeaders = new HashMap<>();
+		dataTypeHeaders.put(MessageProperty.MESSAGE_TYPE.getName(), messageType);
+		dataTypeHeaders.put(MessageProperty.ORIGINATING_COUNTRY.getName(), originatingCountry);
+
+		DataType dataType = new DataType(dataTypeHeaders);
 
 		Set<DataTypeApi> dataTypeApis = capabilityTransformer.dataTypeToDataTypeApi(Collections.singleton(dataType));
 
 		assertThat(dataTypeApis).hasSize(1);
 		DataTypeApi dataTypeApi = dataTypeApis.iterator().next();
 		assertThat(dataTypeApi).isInstanceOf(DataTypeApi.class);
+		assertThat(dataTypeApi.getMessageType()).isEqualTo(messageType);
+		assertThat(dataTypeApi.getOriginatingCountry()).isEqualTo(originatingCountry);
+	}
+
+	private DataType getDatexHeaders(String originatingCountry, String publicationType, String publicationSubType) {
+		HashMap<String, String> datexHeaders = new HashMap<>();
+		datexHeaders.put(MessageProperty.MESSAGE_TYPE.getName(), Datex2DataTypeApi.DATEX_2);
+		datexHeaders.put(MessageProperty.ORIGINATING_COUNTRY.getName(), originatingCountry);
+		if (publicationType != null) {
+			datexHeaders.put(MessageProperty.PUBLICATION_TYPE.getName(), publicationType);
+		}
+		if (publicationSubType  != null) {
+			datexHeaders.put(MessageProperty.PUBLICATION_SUB_TYPE.getName(), publicationSubType);
+		}
+		return new DataType(datexHeaders);
 	}
 }
