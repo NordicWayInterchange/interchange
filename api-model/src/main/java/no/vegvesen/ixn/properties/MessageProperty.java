4724445dbaa58ddfa2b1ac5090a480821d03df50
roger.martinsen <roger.martinsen@bouvet.no>
2019-12-17 10:58:29 +0100

    Introduce flag for filterable on MessageProperty

--- api-model/src/main/java/no/vegvesen/ixn/properties/MessageProperty.java
+++ api-model/src/main/java/no/vegvesen/ixn/properties/MessageProperty.java
@@ -10,46 +10,46 @@ import java.util.stream.Stream;
 @SuppressWarnings({"WeakerAccess", "ArraysAsListWithZeroOrOneArgument"})
 public class MessageProperty {
 
-	public static final MessageProperty MESSAGE_TYPE = new MessageProperty("messageType", true);
-    public static final MessageProperty QUAD_TREE = new MessageProperty("quadTree", false);
-    public static final MessageProperty USER_ID = new MessageProperty("JMSXUserID", true);
-	public static final MessageProperty ORIGINATING_COUNTRY = new MessageProperty("originatingCountry", true);
+	public static final MessageProperty MESSAGE_TYPE = new MessageProperty("messageType", true, true);
+    public static final MessageProperty QUAD_TREE = new MessageProperty("quadTree", false, false);
+    public static final MessageProperty USER_ID = new MessageProperty("JMSXUserID", true, true);
+	public static final MessageProperty ORIGINATING_COUNTRY = new MessageProperty("originatingCountry", true, true);
 
 	public static final List<MessageProperty> commonApplicationProperties = Arrays.asList(
             MESSAGE_TYPE,
             QUAD_TREE,
-            new MessageProperty("publisherId", false),
-            new MessageProperty("publisherName", true),
+            new MessageProperty("publisherId", false, true),
+            new MessageProperty("publisherName", true, true),
 			ORIGINATING_COUNTRY,
-            new MessageProperty("protocolVersion", true),
-            new MessageProperty("contentType", false),
-            new MessageProperty("latitude", true),
-            new MessageProperty("longitude", true),
-            new MessageProperty("timestamp", false),
-            new MessageProperty("relation", false)
+            new MessageProperty("protocolVersion", true, true),
+            new MessageProperty("contentType", false, true),
+            new MessageProperty("latitude", true, true),
+            new MessageProperty("longitude", true, true),
+            new MessageProperty("timestamp", false, true),
+            new MessageProperty("relation", false, true)
     );
 
 
-	public static final MessageProperty PUBLICATION_TYPE = new MessageProperty("publicationType", true);
-	public static final MessageProperty PUBLICATION_SUB_TYPE = new MessageProperty("publicationSubType", false);
+	public static final MessageProperty PUBLICATION_TYPE = new MessageProperty("publicationType", true, true);
+	public static final MessageProperty PUBLICATION_SUB_TYPE = new MessageProperty("publicationSubType", false, true);
 	public static final List<MessageProperty> datex2ApplicationProperties = Arrays.asList(
             PUBLICATION_TYPE,
 			PUBLICATION_SUB_TYPE
     );
 
     public static final List<MessageProperty> itsG5ApplicationProperties = Arrays.asList(
-            new MessageProperty("serviceType",false)
+            new MessageProperty("serviceType",false, true)
     );
 
     public static final List<MessageProperty> denmApplicationProperties = Arrays.asList(
-            new MessageProperty("causeCode",true),
-            new MessageProperty("subCauseCode",false)
+            new MessageProperty("causeCode",true, true),
+            new MessageProperty("subCauseCode",false, true)
     );
 
     public static final List<MessageProperty> iviApplicationProperties = Arrays.asList(
-            new MessageProperty("IviType",false),
-            new MessageProperty("pictogramCategoryCode",false),
-            new MessageProperty("iviContainer",false)
+            new MessageProperty("IviType",false, true),
+            new MessageProperty("pictogramCategoryCode",false, true),
+            new MessageProperty("iviContainer",false, true)
     );
 
 	public static Set<String> mandatoryDatex2PropertyNames = Stream.of(commonApplicationProperties,datex2ApplicationProperties)
@@ -71,22 +71,28 @@ public class MessageProperty {
             .collect(Collectors.toSet());
 
 
-    public static Set<String> allPropertyNames = Stream.of(
+    public static Set<MessageProperty> allProperties = Stream.of(
             commonApplicationProperties,
             datex2ApplicationProperties,
             itsG5ApplicationProperties,
             denmApplicationProperties,
             iviApplicationProperties).flatMap(messageProperties -> messageProperties.stream())
-            .map(MessageProperty::getName)
             .collect(Collectors.toSet());
+
+	public static Set<MessageProperty> filterableProperties = Stream.of(
+			allProperties).flatMap(messageProperties -> messageProperties.stream())
+			.filter(MessageProperty::isFilterable)
+			.collect(Collectors.toSet());
 
     private String name;
     private boolean mandatory;
+    private boolean filterable;
 
-    private MessageProperty(String name, boolean mandatory) {
+    private MessageProperty(String name, boolean mandatory, boolean filterable) {
         this.name = name;
         this.mandatory = mandatory;
-    }
+		this.filterable = filterable;
+	}
 
     public String getName() {
         return name;
@@ -96,7 +102,11 @@ public class MessageProperty {
         return mandatory;
     }
 
-    @Override
+	public boolean isFilterable() {
+		return filterable;
+	}
+
+	@Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
