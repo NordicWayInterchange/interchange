303da5a355ae7752adac5fba9aedf6028a509c04
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2020-02-06 13:11:47 +0100

    Made the onboard client more "springy"

--- onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/OnboardRESTClientConfig.java
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/OnboardRESTClientConfig.java
@@ -3,33 +3,40 @@ package no.vegvesen.ixn.serviceprovider.client;
 import no.vegvesen.ixn.ssl.KeystoreDetails;
 import no.vegvesen.ixn.ssl.KeystoreType;
 import no.vegvesen.ixn.ssl.SSLContextFactory;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
 
-//TODO this is hardcoded. Need to take it out into either properties or arbuments.
 @Configuration
 public class OnboardRESTClientConfig {
 
 
+    @Value("${onboardclient.keystorepath}")
+    private String keyStorePath;
+    @Value("${onboardclient.keystorepassword}")
+    private String keyStorePassword;
+
+    @Value("${onboardclient.keystoretype}")
+    private String keyStoreType;
+
+    @Value("${onboardclient.keypassword}")
+    private String keyPassword;
+
+    @Value("${onboardclient.truststorepath}")
+    private String trustStorePath;
+
+    @Value("${onboardclient.truststorepassword}")
+    private String trustStorePassword;
+
+    @Value("${onboardclient.truststoretype}")
+    private String trustStoreType;
 
     @Bean
-    public RestTemplate createRestTemplate() {
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(createHttpClient()));
-    }
-
-    private HttpClient createHttpClient() {
-        return HttpClients.custom().setSSLContext(createSSLContext()).build();
-    }
-
-    private SSLContext createSSLContext() {
+    public SSLContext createSSLContext() {
         return SSLContextFactory.sslContextFromKeyAndTrustStores(
-                new KeystoreDetails("c:/interchange/tmp/keys/sp-one.bouvetinterchange.no.p12","password", KeystoreType.PKCS12,"password"),
-                new KeystoreDetails("c:/interchange/tmp/keys/truststore.jks","password",KeystoreType.JKS));
+                new KeystoreDetails(keyStorePath, keyStorePassword, KeystoreType.valueOf(keyStoreType),keyPassword),
+                new KeystoreDetails(trustStorePath, trustStorePassword, KeystoreType.valueOf(trustStoreType)));
     }
 }
