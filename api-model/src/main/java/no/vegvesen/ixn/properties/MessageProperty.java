a8c39e47bd3bcf086af1504f4016fe19f1a8c211
roger.martinsen <roger.martinsen@bouvet.no>
2020-02-28 09:42:40 +0100

    Formatting

--- api-model/src/main/java/no/vegvesen/ixn/properties/MessageProperty.java
+++ api-model/src/main/java/no/vegvesen/ixn/properties/MessageProperty.java
@@ -8,8 +8,8 @@ import java.util.stream.Stream;
 public class MessageProperty {
 
 	public static final MessageProperty MESSAGE_TYPE = new MessageProperty("messageType", true, true, MessagePropertyType.STRING);
-    public static final MessageProperty QUAD_TREE = new MessageProperty("quadTree", false, true, MessagePropertyType.STRING_ARRAY);
-    public static final MessageProperty USER_ID = new MessageProperty("JMSXUserID", true, true, MessagePropertyType.STRING);
+	public static final MessageProperty QUAD_TREE = new MessageProperty("quadTree", false, true, MessagePropertyType.STRING_ARRAY);
+	public static final MessageProperty USER_ID = new MessageProperty("JMSXUserID", true, true, MessagePropertyType.STRING);
 	public static final MessageProperty ORIGINATING_COUNTRY = new MessageProperty("originatingCountry", true, true, MessagePropertyType.STRING);
 
 	public static final MessageProperty PUBLISHER_ID = new MessageProperty("publisherId", false, true, MessagePropertyType.STRING);
@@ -20,8 +20,8 @@ public class MessageProperty {
 	public static final MessageProperty CONTENT_TYPE = new MessageProperty("contentType", false, true, MessagePropertyType.STRING);
 	public static final MessageProperty TIMESTAMP = new MessageProperty("timestamp", false, false, MessagePropertyType.STRING);
 	public static final List<MessageProperty> commonApplicationProperties = Arrays.asList(
-            MESSAGE_TYPE,
-            QUAD_TREE,
+			MESSAGE_TYPE,
+			QUAD_TREE,
 			PUBLISHER_ID,
 			PUBLISHER_NAME,
 			ORIGINATING_COUNTRY,
@@ -30,67 +30,67 @@ public class MessageProperty {
 			LATITUDE,
 			LONGITUDE,
 			TIMESTAMP,
-            new MessageProperty("relation", false, true, MessagePropertyType.STRING)
-    );
+			new MessageProperty("relation", false, true, MessagePropertyType.STRING)
+	);
 
 
 	public static final MessageProperty PUBLICATION_TYPE = new MessageProperty("publicationType", true, true, MessagePropertyType.STRING);
 	public static final MessageProperty PUBLICATION_SUB_TYPE = new MessageProperty("publicationSubType", false, true, MessagePropertyType.STRING_ARRAY);
 	public static final List<MessageProperty> datex2ApplicationProperties = Arrays.asList(
-            PUBLICATION_TYPE,
+			PUBLICATION_TYPE,
 			PUBLICATION_SUB_TYPE
-    );
+	);
 
 	public static final MessageProperty SERVICE_TYPE = new MessageProperty("serviceType", false, true, MessagePropertyType.STRING);
 	public static final List<MessageProperty> itsG5ApplicationProperties = Arrays.asList(
 			SERVICE_TYPE
-    );
+	);
 
 	public static final MessageProperty CAUSE_CODE = new MessageProperty("causeCode", true, true, MessagePropertyType.STRING);
 	public static final MessageProperty SUB_CAUSE_CODE = new MessageProperty("subCauseCode", false, true, MessagePropertyType.STRING);
 	public static final List<MessageProperty> denmApplicationProperties = Arrays.asList(
 			CAUSE_CODE,
 			SUB_CAUSE_CODE
-    );
+	);
 
 	public static final MessageProperty IVI_TYPE = new MessageProperty("iviType", false, true, MessagePropertyType.INTEGER);
 	public static final MessageProperty PICTOGRAM_CATEGORY_CODE = new MessageProperty("pictogramCategoryCode", false, true, MessagePropertyType.INTEGER_ARRAY);
 	public static final List<MessageProperty> iviApplicationProperties = Arrays.asList(
 			IVI_TYPE,
 			PICTOGRAM_CATEGORY_CODE,
-            new MessageProperty("iviContainer",false, false, MessagePropertyType.STRING)
-    );
+			new MessageProperty("iviContainer", false, false, MessagePropertyType.STRING)
+	);
 
 	public static Set<String> mandatoryDatex2PropertyNames = Stream.of(
 			commonApplicationProperties,
 			datex2ApplicationProperties).flatMap(Collection::stream)
-            .filter(MessageProperty::isMandatory)
-            .map(MessageProperty::getName)
-            .collect(Collectors.toSet());
+			.filter(MessageProperty::isMandatory)
+			.map(MessageProperty::getName)
+			.collect(Collectors.toSet());
 
-    public static Set<String> mandatoryDenmPropertyNames = Stream.of(
-    		commonApplicationProperties,
+	public static Set<String> mandatoryDenmPropertyNames = Stream.of(
+			commonApplicationProperties,
 			itsG5ApplicationProperties,
 			denmApplicationProperties).flatMap(Collection::stream)
-            .filter(MessageProperty::isMandatory)
-            .map(MessageProperty::getName)
-            .collect(Collectors.toSet());
+			.filter(MessageProperty::isMandatory)
+			.map(MessageProperty::getName)
+			.collect(Collectors.toSet());
 
-    public static Set<String> mandatoryIviPropertyNames = Stream.of(
-    		commonApplicationProperties,
+	public static Set<String> mandatoryIviPropertyNames = Stream.of(
+			commonApplicationProperties,
 			itsG5ApplicationProperties,
 			iviApplicationProperties).flatMap(Collection::stream)
-            .filter(MessageProperty::isMandatory)
-            .map(MessageProperty::getName)
-            .collect(Collectors.toSet());
+			.filter(MessageProperty::isMandatory)
+			.map(MessageProperty::getName)
+			.collect(Collectors.toSet());
 
-    public static Set<MessageProperty> allProperties = Stream.of(
-            commonApplicationProperties,
-            datex2ApplicationProperties,
-            itsG5ApplicationProperties,
-            denmApplicationProperties,
-            iviApplicationProperties).flatMap(Collection::stream)
-            .collect(Collectors.toSet());
+	public static Set<MessageProperty> allProperties = Stream.of(
+			commonApplicationProperties,
+			datex2ApplicationProperties,
+			itsG5ApplicationProperties,
+			denmApplicationProperties,
+			iviApplicationProperties).flatMap(Collection::stream)
+			.collect(Collectors.toSet());
 
 	public static Set<MessageProperty> filterableProperties = Stream.of(
 			allProperties).flatMap(Collection::stream)
@@ -104,13 +104,13 @@ public class MessageProperty {
 			.collect(Collectors.toSet());
 
 	private String name;
-    private boolean mandatory;
-    private boolean filterable;
+	private boolean mandatory;
+	private boolean filterable;
 	private final MessagePropertyType messagePropertyType;
 
 	private MessageProperty(String name, boolean mandatory, boolean filterable, MessagePropertyType propertyType) {
-        this.name = name;
-        this.mandatory = mandatory;
+		this.name = name;
+		this.mandatory = mandatory;
 		this.filterable = filterable;
 		this.messagePropertyType = propertyType;
 	}
@@ -125,30 +125,30 @@ public class MessageProperty {
 	}
 
 	public String getName() {
-        return name;
-    }
+		return name;
+	}
 
-    public boolean isMandatory() {
-        return mandatory;
-    }
+	public boolean isMandatory() {
+		return mandatory;
+	}
 
 	public boolean isFilterable() {
 		return filterable;
 	}
 
 	@Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MessageProperty that = (MessageProperty) o;
-        return mandatory == that.mandatory &&
-                Objects.equals(name, that.name);
-    }
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		MessageProperty that = (MessageProperty) o;
+		return mandatory == that.mandatory &&
+				Objects.equals(name, that.name);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, mandatory);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, mandatory);
+	}
 
 	public MessagePropertyType getMessagePropertyType() {
 		return messagePropertyType;
