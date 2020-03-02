d485e0abb7e9ea48fc73ebe7c7b3d126e6984d2f
roger.martinsen <roger.martinsen@bouvet.no>
2020-03-02 15:25:20 +0100

    Capability example suitable for api descriptions

--- api-model/src/test/java/no/vegvesen/ixn/federation/api/v1_0/CapabilityApiTest.java
+++ api-model/src/test/java/no/vegvesen/ixn/federation/api/v1_0/CapabilityApiTest.java
@@ -2,6 +2,7 @@ package no.vegvesen.ixn.federation.api.v1_0;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.util.Sets;
 import org.junit.Test;
 
 import java.util.HashSet;
@@ -12,10 +13,27 @@ public class CapabilityApiTest {
     @Test
     public void createEmptyCapabilityApiJson() throws JsonProcessingException {
         CapabilityApi capabilityApi = new CapabilityApi();
-        capabilityApi.setName("sp-one.bouvetinterchange.no");
+        capabilityApi.setName("a.itsinterchange.eu");
         Set<DataTypeApi> capSet = new HashSet<>();
-        Datex2DataTypeApi dataTypeApi = new Datex2DataTypeApi("NO");
-        capSet.add(dataTypeApi);
+		Set<String> quadTree = Sets.newLinkedHashSet("01230123", "01230122");
+		DenmDataTypeApi denmRoadworksWinterService = new DenmDataTypeApi(
+				"NO-12345", "Some Norwegian publisher",
+				"NO", "DENM:1.2.2", "application/octet-stream", quadTree,
+				"some-denm-service-type?", "3", "6");
+		Datex2DataTypeApi datexSituationReroutingWinterDriving = new Datex2DataTypeApi(
+				"NO-12345", "Some Norwegian publisher",
+				"NO", "DATEX2:2.3", "application/xml", quadTree,
+				"SituationPublication", Sets.newLinkedHashSet("ReroutingManagement", "WinterDrivingManagement"));
+		IviDataTypeApi iviType128Pictograms557_559_612 = new IviDataTypeApi(
+				"NO-12345", "Some Norwegian publisher",
+				"NO", "IVI:1.2", "application/base64", quadTree,
+				"some-ivi-service-type?", 128, Sets.newLinkedHashSet(557, 559, 612)
+		);
+
+
+        capSet.add(datexSituationReroutingWinterDriving);
+        capSet.add(denmRoadworksWinterService);
+        capSet.add(iviType128Pictograms557_559_612);
         capabilityApi.setCapabilities(capSet);
         ObjectMapper mapper = new ObjectMapper();
         System.out.println(mapper.writeValueAsString(capabilityApi));
