5c8737ad520a0b48da824320db0160f4e6a20cc5
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2020-03-12 14:42:07 +0100

    Got the actual onboardRestClient to contact the server. Just need to generate the correct keys.

---
+++ onboard-rest-client/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar onboard-rest-client.jar $ONBOARD_SERVER $USER $KEY_STORE $KEY_STORE_PASSWORD $KEY_PASSWORD
+
--- onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/OnboardRestClientApplication.java
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/OnboardRestClientApplication.java
@@ -1,14 +1,41 @@
 package no.vegvesen.ixn.serviceprovider.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.vegvesen.ixn.federation.api.v1_0.CapabilityApi;
+import no.vegvesen.ixn.ssl.KeystoreDetails;
+import no.vegvesen.ixn.ssl.KeystoreType;
+import no.vegvesen.ixn.ssl.SSLContextFactory;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+
 public class OnboardRestClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnboardRestClientApplication.class);
-    }
+    public static void main(String[] args) throws IOException {
+        if (args.length < 4) {
+            System.out.println("Not enough arguments");
+        }
+        String server = args[0];
+        String user = args[1];
+        String keystorePath = args[2];
+        String keystorePassword = args[3];
+        String keyPassword = args[4];
+        KeystoreDetails keystoreDetails = new KeystoreDetails(keystorePath,
+                keystorePassword,
+                KeystoreType.PKCS12, keyPassword);
+        String trustStorePath = "/jks/truststore.jks";
+        String trustStorePassword = "password";
+        KeystoreDetails trustStoreDetails = new KeystoreDetails(trustStorePath,
+                trustStorePassword,KeystoreType.JKS);
 
+        SSLContext sslContext = SSLContextFactory.sslContextFromKeyAndTrustStores(keystoreDetails, trustStoreDetails);
+
+        OnboardRESTClient client = new OnboardRESTClient(sslContext,server,user);
+        CapabilityApi serviceProviderCapabilities = client.getServiceProviderCapabilities();
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writeValueAsString(serviceProviderCapabilities));
+
+    }
 
 }
