7ff202d2ef0ef5229bd86f941afd095015805c82
roger.martinsen <roger.martinsen@bouvet.no>
2020-03-13 08:14:22 +0100

    Trigger DoS recognition on server - amqp header not fully sent within 2 seconds

--- jms-client/src/test/java/no/vegvesen/ixn/IxnContextTest.java
+++ jms-client/src/test/java/no/vegvesen/ixn/IxnContextTest.java
@@ -1,12 +1,14 @@
 package no.vegvesen.ixn;
 
+import eu.rekawek.toxiproxy.model.ToxicDirection;
 import no.vegvesen.ixn.docker.DockerBaseIT;
-import org.apache.qpid.jms.exceptions.JmsConnectionFailedException;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.ToxiproxyContainer;
 import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.ToStringConsumer;
 
@@ -14,6 +16,9 @@ import javax.jms.Connection;
 import javax.jms.JMSException;
 import javax.naming.NamingException;
 import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -23,27 +28,31 @@ public class IxnContextTest extends DockerBaseIT {
 
 	private static Logger logger = LoggerFactory.getLogger(IxnContextTest.class);
 
+	@ClassRule
+	public static Network network = Network.newNetwork();
 
 	@ClassRule
-	public static GenericContainer localContainer = getQpidContainer("qpid", "jks", "localhost.crt", "localhost.crt", "localhost.key");
+	public static GenericContainer localContainer = getQpidContainer("qpid", "jks", "localhost.crt", "localhost.crt", "localhost.key")
+			.withNetwork(network);
+
+	@ClassRule
+	public static ToxiproxyContainer toxiproxy = new ToxiproxyContainer()
+			.withNetwork(network);
+	final ToxiproxyContainer.ContainerProxy proxy = toxiproxy.getProxy(localContainer, AMQPS_PORT);
+
 
 	private String getQpidURI() {
-		String url = "amqps://localhost:" + localContainer.getMappedPort(AMQPS_PORT);
+		final String ipAddressViaToxiproxy = proxy.getContainerIpAddress();
+		final int portViaToxiproxy = proxy.getProxyPort();
+		String url = String.format("amqps://%s:%d", ipAddressViaToxiproxy, portViaToxiproxy);
 		logger.info("connection string to local message broker {}", url);
 		return url;
 	}
-
 
 	/**
 	 * This test triggers a DoS recognition on the server to avoid too many open connections.
 	 * 2020-03-12 10:22:56,500 WARN  [IO-/10.166.15.234:58076] (o.a.q.s.t.AbstractAMQPConnection) - Connection has taken more than 10000 ms to establish.  Closing as possible DoS.
 	 * 2020-03-12 10:22:56,500 INFO  [IO-/10.166.15.234:58076] (q.m.c.idle_close) - [Broker] CON-1003 : Closed due to inactivity : Protocol connection is not established within timeout period
-	 * <p>
-	 * The situation we are trying to reproduce is the one from MultiVersionProtocolEngine.
-	 * 2020-03-12 07:22:36,813 WARN  [IO-/10.36.3.1:33884] (o.a.q.s.t.MultiVersionProtocolEngine) - Connection has taken more than 2000 ms to send complete protocol header.  Closing as possible DoS.
-	 * 2020-03-12 07:22:36,814 INFO  [IO-/10.36.3.1:33884] (q.m.c.idle_close) - [Broker] CON-1003 : Closed due to inactivity : Protocol header not received within timeout period
-	 * DoS is thrown in MultiVersionProtocolEngine#tick().
-	 * I believe we have to simulate bad network traffic to achieve this.
 	 */
 	@Test
 	public void createConnectionFailsAfterWaitingDoSLimit() throws JMSException, NamingException, InterruptedException {
@@ -52,16 +61,54 @@ public class IxnContextTest extends DockerBaseIT {
 
 		SSLContext guestSSLContext = TestKeystoreHelper.sslContext("jks/guest.p12", "jks/truststore.jks");
 		IxnContext ixnContext = new IxnContext(getQpidURI(), "onramp", null);
-		Connection questConnection = ixnContext.createConnection(guestSSLContext);
-		Thread.sleep(11000); //10 seconds is the connection creation to start limit
+		Connection guestConnection = ixnContext.createConnection(guestSSLContext);
+		Thread.sleep(11000);
 		try {
-			questConnection.start();
-			fail("one of the connections should have been terminated on the server due to DoS");
+			guestConnection.start();
+			logger.error(serverLogConsumer.toUtf8String());
+			fail("the connections should have been terminated on the server due to DoS");
 		} catch (Exception e) {
-			assertTrue(e instanceof JmsConnectionFailedException);
 			String serverLog = serverLogConsumer.toUtf8String();
+			logger.debug(serverLog);
 			assertTrue(serverLog.contains("Closing as possible DoS."));
-			e.printStackTrace();
+			assertTrue(serverLog.contains("AbstractAMQPConnection"));
+			logger.debug("The test experienced the connection problem as expected", e);
+		}
+	}
+
+	/**
+	 * This test triggers a DoS recognition on the server to avoid too many open connections.
+	 * The situation we are trying to reproduce is the one from MultiVersionProtocolEngine.
+	 * We have to simulate bad network traffic to achieve this.
+	 * 2020-03-12 07:22:36,813 WARN  [IO-/10.36.3.1:33884] (o.a.q.s.t.MultiVersionProtocolEngine) - Connection has taken more than 2000 ms to send complete protocol header.  Closing as possible DoS.
+	 * 2020-03-12 07:22:36,814 INFO  [IO-/10.36.3.1:33884] (q.m.c.idle_close) - [Broker] CON-1003 : Closed due to inactivity : Protocol header not received within timeout period
+	 * DoS is thrown in MultiVersionProtocolEngine#tick().
+	 */
+	@Test
+	public void createConnectionFailsWithDoSRecognitionDueToBadNetworkTraffic() throws NamingException, IOException {
+		ToStringConsumer serverLogConsumer = new ToStringConsumer();
+		localContainer.followOutput(serverLogConsumer, OutputFrame.OutputType.STDOUT);
+
+		SSLContext guestSSLContext = TestKeystoreHelper.sslContext("jks/guest.p12", "jks/truststore.jks");
+		IxnContext ixnContext = new IxnContext(getQpidURI(), "onramp", null);
+		proxy.toxics().latency("down", ToxicDirection.DOWNSTREAM, 1999); // push the DoS limit on 2000
+		proxy.toxics().latency("up", ToxicDirection.UPSTREAM, 1999);
+		logger.info(serverLogConsumer.toUtf8String());
+		try {
+			Set<Connection> guestConnections = new HashSet<>();
+			for (int i = 0; i < 4; i++) {
+				Connection guestConnection = ixnContext.createConnection(guestSSLContext);
+				guestConnection.start();
+				guestConnections.add(guestConnection);
+			}
+			logger.error(serverLogConsumer.toUtf8String());
+			fail("the connections should have been terminated on the server due to DoS");
+		} catch (Exception e) {
+			String serverLog = serverLogConsumer.toUtf8String();
+			logger.debug(serverLog);
+			assertTrue(serverLog.contains("Closing as possible DoS."));
+			assertTrue(serverLog.contains("MultiVersionProtocolEngine"));
+			logger.debug("The test experienced the connection problem as expected", e);
 		}
 	}
 }
