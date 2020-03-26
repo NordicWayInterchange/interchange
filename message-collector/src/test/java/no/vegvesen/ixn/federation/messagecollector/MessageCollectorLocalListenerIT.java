88613c3332351cc0892c30eb4eca03b437f73611
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2020-03-26 16:07:06 +0100

    Split the single test into two, separate test classes to avoid interferrence.

--- message-collector/src/test/java/no/vegvesen/ixn/federation/messagecollector/MessageCollectorLocalListenerIT.java
+++ message-collector/src/test/java/no/vegvesen/ixn/federation/messagecollector/MessageCollectorLocalListenerIT.java
@@ -3,12 +3,8 @@ package no.vegvesen.ixn.federation.messagecollector;
 import no.vegvesen.ixn.TestKeystoreHelper;
 import no.vegvesen.ixn.docker.DockerBaseIT;
 import no.vegvesen.ixn.federation.model.Neighbour;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
@@ -21,11 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
-public class MessageCollectorListenerIT extends DockerBaseIT {
-
-	private static final SSLContext SSL_CONTEXT = TestKeystoreHelper.sslContext("jks/localhost.p12", "jks/truststore.jks");
-	private static Logger logger = LoggerFactory.getLogger(MessageCollectorListenerIT.class);
+public class MessageCollectorLocalListenerIT extends DockerBaseIT {
+    private static final SSLContext SSL_CONTEXT = TestKeystoreHelper.sslContext("jks/localhost.p12", "jks/truststore.jks");
+	private static Logger logger = LoggerFactory.getLogger(MessageCollectorLocalListenerIT.class);
 
 	@Rule
 	public GenericContainer localContainer = getQpidContainer("docker/consumer", "jks", "my_ca.crt", "localhost.crt", "localhost.key");
@@ -36,13 +30,9 @@ public class MessageCollectorListenerIT extends DockerBaseIT {
 	private String remoteAmqpsUrl;
 	private CollectorCreator collectorCreator;
 
-	@Before
-	public void setup() {
-
-	}
 
 	@Test
-	public void testStopLocalContainerTriggersConnectionExceptionListener() throws JMSException, NamingException {
+	public void stoppingLocalContainerStopsListener() throws NamingException, JMSException {
 		remoteAmqpsUrl = String.format("amqps://localhost:%s", remoteContainer.getMappedPort(AMQPS_PORT));
 		collectorCreator = new CollectorCreator(SSL_CONTEXT, "localhost", localContainer.getMappedPort(AMQPS_PORT).toString(), "fedEx");
 		Neighbour remote = mock(Neighbour.class);
@@ -55,14 +45,5 @@ public class MessageCollectorListenerIT extends DockerBaseIT {
 		assertThat(remoteForwardListener.isRunning()).isFalse();
 
 
-		localContainer.start();
-		collectorCreator = new CollectorCreator(SSL_CONTEXT, "localhost", localContainer.getMappedPort(AMQPS_PORT).toString(), "fedEx");
-		remoteForwardListener = collectorCreator.setupCollection(remote);
-
-		//Stop the container to trigger the connection exception listener to run
-		remoteContainer.stop();
-
-		assertThat(remoteForwardListener.isRunning()).isFalse();
 	}
-
 }
---
+++ message-collector/src/test/java/no/vegvesen/ixn/federation/messagecollector/MessageCollectorRemoteListenerIT.java
@@ -0,0 +1,45 @@
+package no.vegvesen.ixn.federation.messagecollector;
+
+import no.vegvesen.ixn.TestKeystoreHelper;
+import no.vegvesen.ixn.docker.DockerBaseIT;
+import no.vegvesen.ixn.federation.model.Neighbour;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+import javax.net.ssl.SSLContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MessageCollectorRemoteListenerIT extends DockerBaseIT {
+    private static final SSLContext SSL_CONTEXT = TestKeystoreHelper.sslContext("jks/localhost.p12", "jks/truststore.jks");
+    private static Logger logger = LoggerFactory.getLogger(MessageCollectorRemoteListenerIT.class);
+
+    @Rule
+    public GenericContainer localContainer = getQpidContainer("docker/consumer", "jks", "my_ca.crt", "localhost.crt", "localhost.key");
+
+    @Rule
+    public GenericContainer remoteContainer = getQpidContainer("docker/producer", "jks", "my_ca.crt", "localhost.crt", "localhost.key");
+
+    private String remoteAmqpsUrl;
+    private CollectorCreator collectorCreator;
+
+    @Test
+    public void stoppingRemoteContainerStopsListener() throws NamingException, JMSException {
+        remoteAmqpsUrl = String.format("amqps://localhost:%s", remoteContainer.getMappedPort(AMQPS_PORT));
+        collectorCreator = new CollectorCreator(SSL_CONTEXT, "localhost", localContainer.getMappedPort(AMQPS_PORT).toString(), "fedEx");
+        Neighbour remote = mock(Neighbour.class);
+        when(remote.getMessageChannelUrl()).thenReturn(remoteAmqpsUrl);
+        MessageCollectorListener remoteForwardListener = collectorCreator.setupCollection(remote);
+        remoteContainer.stop();
+
+        assertThat(remoteForwardListener.isRunning()).isFalse();
+    }
+
+}
