819340f003e653a12e7325e4fc82d040791cebf7
roger.martinsen <roger.martinsen@bouvet.no>
2020-05-07 09:09:16 +0200

    Generation of random string without the limitation of readable characters

--- federation-st/src/test/java/no/vegvesen/ixn/federation/FederationSystemST.java
+++ federation-st/src/test/java/no/vegvesen/ixn/federation/FederationSystemST.java
@@ -137,7 +137,6 @@ public class FederationSystemST {
 		int rightLimit = 122; // letter 'z'
 		Random random = new Random();
 		return random.ints(leftLimit, rightLimit + 1)
-				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
 				.limit(length)
 				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
 				.toString();
