e5acb7dc20dfb7ce02a757db9994c82213685214
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2020-05-14 17:06:46 +0200

    Changes to make sure the Docker images are created properly

--- build-push-images.sh
+++ build-push-images.sh
@@ -3,7 +3,7 @@
 REGISTRY=eu.gcr.io/nordic-way-aad182cc
 TAG="$(git rev-parse --short HEAD)"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-IMAGES="interchangenode qpid message-collector neighbour-discoverer neighbour-server routing-configurer onboard-server"
+IMAGES="interchange-node-app qpid message-collector neighbour-discoverer neighbour-server routing-configurer onboard-server"
 BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
 
 for image in ${IMAGES}; do
