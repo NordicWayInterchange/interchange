dbe4c194dc4f51392437e528c6d2e8e197b78966
roger.martinsen <roger.martinsen@bouvet.no>
2020-05-15 15:44:56 +0200

    Centralize JPA config for neighbour model

--- onboard-server-app/src/main/java/no/vegvesen/ixn/serviceprovider/OnboardApplication.java
+++ onboard-server-app/src/main/java/no/vegvesen/ixn/serviceprovider/OnboardApplication.java
@@ -2,13 +2,8 @@ package no.vegvesen.ixn.serviceprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"no.vegvesen.ixn.federation", "no.vegvesen.ixn.serviceprovider"})
-@EnableJpaRepositories(basePackages = {"no.vegvesen.ixn.federation.repository"})
-@EntityScan(basePackages={"no.vegvesen.ixn.federation.model"})
 public class OnboardApplication {
 
 	public static void main(String[] args) {
