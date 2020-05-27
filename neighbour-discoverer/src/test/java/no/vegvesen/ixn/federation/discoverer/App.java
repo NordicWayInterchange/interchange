287d84995181d847ce4207c36842f258d5242896
roger.martinsen <roger.martinsen@bouvet.no>
2020-05-27 13:16:27 +0200

    Remove unused test resources

--- neighbour-discoverer/src/test/java/no/vegvesen/ixn/federation/discoverer/App.java
+++
@@ -1,9 +0,0 @@
-package no.vegvesen.ixn.federation.discoverer;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@EnableScheduling
-@SpringBootApplication(scanBasePackages = "no.vegvesen.ixn.federation")
-public class App {
-}
