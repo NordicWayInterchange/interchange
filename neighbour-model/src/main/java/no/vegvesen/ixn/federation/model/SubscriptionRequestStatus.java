9bc5bbb758ddf75e8c9f4f9954197e2dafbda158
roger.martinsen <roger.martinsen@bouvet.no>
2020-06-02 16:03:02 +0200

    Remove outdated FEDERATED_ACCESS_GRANTED from SubscriptionRequestStatus

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/SubscriptionRequestStatus.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/SubscriptionRequestStatus.java
@@ -1,3 +1,3 @@
 package no.vegvesen.ixn.federation.model;
 
-public enum SubscriptionRequestStatus {REQUESTED, ESTABLISHED, NO_OVERLAP, TEAR_DOWN, EMPTY, FAILED, REJECTED, FEDERATED_ACCESS_GRANTED}
+public enum SubscriptionRequestStatus {REQUESTED, ESTABLISHED, NO_OVERLAP, TEAR_DOWN, EMPTY, FAILED, REJECTED}
