ed19cd1bc7a2a54c749c0d9dfd012881bd2a7748
roger.martinsen <roger.martinsen@bouvet.no>
2020-06-04 14:54:07 +0200

    Spring autowire beans outside module

--- message-collector-app/src/main/java/no/vegvesen/ixn/federation/messagecollector/Application.java
+++ message-collector-app/src/main/java/no/vegvesen/ixn/federation/messagecollector/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "no.vegvesen.ixn")
 public class Application {
 
     public static void main(String[] args){
