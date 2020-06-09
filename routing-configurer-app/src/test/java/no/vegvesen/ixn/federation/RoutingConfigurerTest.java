b80f372abf0e10b3c11ae5bbeea46ffd9b9baf62
roger.martinsen <roger.martinsen@bouvet.no>
2020-06-09 15:28:31 +0200

    Add Routing configurer properties to the tests

--- routing-configurer-app/src/test/java/no/vegvesen/ixn/federation/RoutingConfigurerTest.java
+++ routing-configurer-app/src/test/java/no/vegvesen/ixn/federation/RoutingConfigurerTest.java
@@ -1,6 +1,7 @@
 package no.vegvesen.ixn.federation;
 
 import no.vegvesen.ixn.federation.qpid.QpidClient;
+import no.vegvesen.ixn.federation.qpid.RoutingConfigurerProperties;
 import no.vegvesen.ixn.federation.repository.NeighbourRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = {RoutingConfigurer.class, QpidClient.class})
+@SpringBootTest(classes = {RoutingConfigurer.class, QpidClient.class, RoutingConfigurerProperties.class})
 @EnableScheduling
 class RoutingConfigurerTest {
 
