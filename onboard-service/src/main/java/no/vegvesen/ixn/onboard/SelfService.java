8c5dd5aa9d9015b12b6e8924295f33cb5cd0a6f8
roger.martinsen <roger.martinsen@bouvet.no>
2020-06-25 12:39:10 +0200

    Users of property classes shall have ConfigurationPropertiesScan-annotation

--- onboard-service/src/main/java/no/vegvesen/ixn/onboard/SelfService.java
+++ onboard-service/src/main/java/no/vegvesen/ixn/onboard/SelfService.java
@@ -7,6 +7,7 @@ import no.vegvesen.ixn.federation.repository.ServiceProviderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
+@ConfigurationPropertiesScan
 public class SelfService {
 	private static Logger logger = LoggerFactory.getLogger(SelfService.class);
 	ServiceProviderRepository repository;
