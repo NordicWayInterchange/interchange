9e82601efef840d99d8c32c784a47f648e84831c
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2020-11-16 12:05:26 +0100

    small fixes to more easilly be able to run the systemtest docker-compose.

--- systemtest.sh
+++ systemtest.sh
@@ -6,5 +6,13 @@ export BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
 
 echo Running system test on branch $BRANCH with tag $BRANCH_TAG
 
-[ -f tmp/keys/remote.bouvetinterchange.eu.p12 ] || systemtest-keys.sh
-docker-compose -f systemtest.yml up --build
+cd onboard-rest-client
+docker build . -t onboard_rest_client 
+cd ..
+cd jms-client-sink 
+docker build . -t jms_client_sink
+cd ..
+cd jms-client-source
+docker builld . -t jms_client_sink
+[ -f tmp/keys/remote.bouvetinterchange.eu.p12 ] || docker-compose -f systemtest-keys.yml up --build
+docker-compose -f systemtest.yml build && docker-compose -f systemtest.yml up
