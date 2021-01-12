7c6725aa94034405fffd309bbd5f608b1b2b7da5
roger.martinsen <roger.martinsen@bouvet.no>
2021-01-12 09:09:06 +0100

    Not viable path because LikeExpression has private access to "likePattern" member variable: Parse JmsSelector and use Qpid expressions to evaluate capability match for quadTree tiles

--- neighbour-model/src/test/java/no/vegvesen/ixn/federation/capability/JMSSelectorFilterFactoryTest.java
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/capability/JMSSelectorFilterFactoryTest.java
@@ -4,12 +4,21 @@ import no.vegvesen.ixn.federation.exceptions.HeaderNotFilterable;
 import no.vegvesen.ixn.federation.exceptions.HeaderNotFoundException;
 import no.vegvesen.ixn.federation.exceptions.InvalidSelectorException;
 import no.vegvesen.ixn.federation.exceptions.SelectorAlwaysTrueException;
+import org.apache.qpid.server.filter.*;
+import org.apache.qpid.server.filter.selector.ParseException;
+import org.apache.qpid.server.filter.selector.SelectorParser;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class JMSSelectorFilterFactoryTest {
+	private static Logger logger = LoggerFactory.getLogger(JMSSelectorFilterFactoryTest.class);
 
 	//equals without quote seems to be matching one header against another
 	@Test
@@ -117,4 +126,49 @@ public class JMSSelectorFilterFactoryTest {
 	public void emptySelectorWillMatchEverythingAndThereforeNotAllowed() {
 		assertThat(JMSSelectorFilterFactory.isValidSelector("")).isFalse();
 	}
+
+	@Test
+	public void parseJmsSelectorAndUseQpidExpressionsToEvaluateCapabilityMatch() throws ParseException{
+		String selector = "messageType = 'DATEX2' AND quadTree LIKE '%,123456%' OR quadTree LIKE '%,1256%'";
+		JMSSelectorFilter jmsSelectorFilter = JMSSelectorFilterFactory.get(selector);
+		System.out.println(jmsSelectorFilter.toString());
+
+		SelectorParser<FilterableMessage> selectorParser = new SelectorParser<>();
+		selectorParser.setPropertyExpressionFactory(JMSMessagePropertyExpression.FACTORY);
+		Expression<FilterableMessage> parse = selectorParser.parse(selector);
+		debugExpression(parse);
+	}
+
+
+	private void debugExpression(Expression<FilterableMessage> expression) {
+		if (expression instanceof ComparisonExpression) {
+			System.out.println("Comparison: " + expression.toString());
+		} else if (expression instanceof UnaryExpression) {
+			UnaryExpression<FilterableMessage> unaryExpression = (UnaryExpression<FilterableMessage>) expression;
+			String expressionClassName = unaryExpression.getClass().getSimpleName();
+			if (expressionClassName.startsWith("LikeExpression")) {
+				Pattern pattern = getLikePattern(expression);
+				System.out.printf("LikeExpression: pattern %s, symbol:%s toString:%s %n", pattern, unaryExpression.getExpressionSymbol(), expression.toString());
+			}
+			System.out.println("Unary: " + expression.toString());
+		} else if (expression instanceof BinaryExpression) {
+			BinaryExpression<FilterableMessage> binaryExpression = (BinaryExpression<FilterableMessage>) expression;
+			debugExpression(binaryExpression.getLeft());
+			debugExpression(binaryExpression.getRight());
+		}
+	}
+
+	private Pattern getLikePattern(Expression<FilterableMessage> expression)  {
+		Pattern pattern = null;
+		try {
+			Class<?> likeExpressionClass = Class.forName("org.apache.qpid.server.filter.ComparisonExpression$LikeExpression");
+			Field likePattern = likeExpressionClass.getField("likePattern");
+			likePattern.setAccessible(true);
+			pattern = (Pattern) likePattern.get(expression);
+		} catch (ClassNotFoundException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return pattern;
+	}
 }
+
