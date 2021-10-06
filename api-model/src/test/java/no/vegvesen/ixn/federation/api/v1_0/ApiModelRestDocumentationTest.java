cc8e48f14320358c3bda47dc6059eeaac7e1521e
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2021-10-06 13:23:30 +0200

    Added api documentation test to print api objects for neighbour model

---
+++ api-model/src/test/java/no/vegvesen/ixn/federation/api/v1_0/ApiModelRestDocumentationTest.java
@@ -0,0 +1,133 @@
+package no.vegvesen.ixn.federation.api.v1_0;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class ApiModelRestDocumentationTest {
+
+    @Test
+    public void postCapabilitiesRequest() throws JsonProcessingException {
+        CapabilitiesApi request = new CapabilitiesApi(
+                "sp-1",
+                Collections.singleton(
+                        new DenmCapabilityApi(
+                                "NPRA",
+                                "NO",
+                                "1.0",
+                                Collections.singleton("1234"),
+                                Collections.singleton("6")
+                        )
+                )
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
+    }
+
+    @Test
+    public void CapabilitiesResponse() throws JsonProcessingException {
+        CapabilitiesApi response = new CapabilitiesApi(
+                "sp-1",
+                Collections.singleton(
+                        new DenmCapabilityApi(
+                                "NPRA",
+                                "NO",
+                                "1.0",
+                                Collections.singleton("1224"),
+                                Collections.singleton("5")
+                        )
+                )
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+    }
+
+    @Test
+    public void postSubscriptionRequest() throws JsonProcessingException {
+        SubscriptionRequestApi request = new SubscriptionRequestApi(
+                "sp-1",
+                Collections.singleton(
+                        new RequestedSubscriptionApi(
+                                "originatingCountry = 'NO' and messageType = 'DENM'",
+                                false,
+                                "node-1"
+                        )
+                )
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
+    }
+
+    @Test
+    public void subscriptionRequestResponse() throws JsonProcessingException {
+        SubscriptionResponseApi response = new SubscriptionResponseApi(
+                "sp-1",
+                Collections.singleton(
+                        new RequestedSubscriptionResponseApi(
+                                "1",
+                                "originatingCountry = 'NO' and messageType = 'DENM'",
+                                "/subscription/1",
+                                SubscriptionStatusApi.REQUESTED,
+                                false,
+                                "node-1",
+                                System.currentTimeMillis()
+                        )
+                )
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+    }
+
+    @Test
+    public void subscriptionRequestResponseWithList() throws JsonProcessingException {
+        SubscriptionResponseApi response = new SubscriptionResponseApi(
+                "sp-1",
+                new HashSet<>(Arrays.asList(
+                        new RequestedSubscriptionResponseApi(
+                                "1",
+                                "originatingCountry = 'NO' and messageType = 'DENM'",
+                                "/subscription/1",
+                                SubscriptionStatusApi.REQUESTED,
+                                false,
+                                "node-1",
+                                System.currentTimeMillis()
+                        ),
+                        new RequestedSubscriptionResponseApi(
+                                "2",
+                                "originatingCountry = 'NO' and messageType = 'DENM'",
+                                "/subscription/2",
+                                SubscriptionStatusApi.REQUESTED,
+                                false,
+                                "node-1",
+                                System.currentTimeMillis()
+                        )
+                ))
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+    }
+
+    @Test
+    public void subscriptionPollResponse() throws JsonProcessingException {
+        SubscriptionPollResponseApi response = new SubscriptionPollResponseApi(
+                "1",
+                "originatingCountry = 'NO' and messageType = 'DENM'",
+                "/subscription/1",
+                SubscriptionStatusApi.CREATED,
+                false,
+                "node-1",
+                Collections.singleton(
+                        new BrokerApi(
+                                "queue-1",
+                                "broker-1"
+                        )
+                )
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+    }
+}
