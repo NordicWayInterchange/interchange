6a2c3c9377c455eb27faebecb96e4decd61a8f17
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2021-11-17 12:39:16 +0100

    Added status RESUBSCRIBE on all variants of SubscriptionStatus

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/LocalSubscriptionStatus.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/LocalSubscriptionStatus.java
@@ -3,5 +3,6 @@ package no.vegvesen.ixn.federation.model;
 public enum LocalSubscriptionStatus {
     REQUESTED,
     CREATED,
-    TEAR_DOWN //TODO ?? What about rejected?
+    TEAR_DOWN, //TODO ?? What about rejected?
+    RESUBSCRIBE
 }
