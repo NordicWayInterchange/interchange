506ba8bb9f8305879b807a557a2254f5d2cf3b5a
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2022-02-07 17:41:49 +0100

    Updating test and adding json for delivery in systemtest

--- neighbour-service/src/test/java/no/vegvesen/ixn/federation/service/ServiceProviderServiceTest.java
+++ neighbour-service/src/test/java/no/vegvesen/ixn/federation/service/ServiceProviderServiceTest.java
@@ -163,4 +163,50 @@ public class ServiceProviderServiceTest {
                 .allMatch(b -> 5671 == b.getPort())
                 .allMatch(b -> queueName.equals(b.getSource()));
     }
+
+    @Test
+    public void updateServiceProviderDeliveryWithOnramp() {
+        ServiceProviderService serviceProviderService = new ServiceProviderService(neighbourRepository,serviceProviderRepository);
+        String selector = "originatingCountry = 'NO' AND protocolVersion = 'DENM:1.1.0' AND quadTree like '%,123%,' AND causeCode like '%,1,%'";
+        String localNodeName = "local-node";
+        String serviceProviderName = "sp-1";
+
+        LocalDelivery localDelivery = new LocalDelivery(
+                1,
+                "",
+                selector,
+                LocalDateTime.now(),
+                LocalDeliveryStatus.REQUESTED
+        );
+        DenmCapability capability = new DenmCapability(
+                "publisher-1",
+                "NO",
+                "DENM:1.1.0",
+                Collections.singleton("123"),
+                Collections.singleton("1")
+        );
+        ServiceProvider serviceProvider = new ServiceProvider(
+                1,
+                serviceProviderName,
+                new Capabilities(
+                        Capabilities.CapabilitiesStatus.KNOWN,
+                        Collections.singleton(capability)),
+                new HashSet<>(),
+                new HashSet<>(),
+                LocalDateTime.now()
+        );
+        serviceProvider.addDeliveries(Collections.singleton(localDelivery));
+        doReturn(Arrays.asList(serviceProvider)).when(serviceProviderRepository).findAll();
+        serviceProviderService.updateLocalDeliveries(localNodeName, "5671");
+        assertThat(serviceProvider.getDeliveries()).hasSize(1);
+        LocalDelivery delivery = serviceProvider.getDeliveries().stream().findFirst().get();
+        assertThat(delivery.getEndpoints())
+                .hasSize(1)
+                .allMatch(b -> b.getHost().equals(localNodeName))
+                .allMatch(b -> b.getPort() == 5671)
+                .allMatch(b -> b.getSelector().equals(selector))
+                .allMatch(b -> b.getTarget().equals("onramp"));
+        verify(serviceProviderRepository,times(1)).findAll();
+        verify(serviceProviderRepository).save(any());
+    }
 }
