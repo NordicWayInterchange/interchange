608bb4b6910d2446218cf0661382b39b72b0baf6
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2022-02-08 12:22:15 +0100

    Filled the CamCapabilityApi object

--- api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/CamCapabilityApi.java
+++ api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/CamCapabilityApi.java
@@ -6,4 +6,36 @@ import java.util.Set;
 
 public class CamCapabilityApi extends CapabilityApi {
     private Set<String> stationTypes;
+
+    public CamCapabilityApi() {
+
+    }
+
+    public CamCapabilityApi(String publisherId, String originatingCountry, String protocolVersion, Set<String> quadTree, RedirectStatusApi redirect, Integer shardCount, String infoUrl, Set<String> stationTypes) {
+        super(Constants.CAM, publisherId, originatingCountry, protocolVersion, redirect, shardCount, infoUrl, quadTree);
+        this.stationTypes = stationTypes;
+    }
+
+    public CamCapabilityApi(String publisherId, String originatingCountry, String protocolVersion, Set<String> quadTree, Set<String> stationTypes) {
+        this(publisherId, originatingCountry, protocolVersion, quadTree, null, null, null, stationTypes);
+    }
+
+    public CamCapabilityApi(String publisherId, String originatingCountry, String protocolVersion, Set<String> quadTree, RedirectStatusApi redirect, Set<String> stationTypes) {
+        this(publisherId, originatingCountry, protocolVersion, quadTree, redirect, null, null, stationTypes);
+    }
+
+    public Set<String> getStationTypes() {
+        return stationTypes;
+    }
+
+    public void setStationType(Set<String> stationTypes) {
+        this.stationTypes = stationTypes;
+    }
+
+    @Override
+    public String toString() {
+        return "CamCapabilityApi{" +
+                "stationTypes=" + stationTypes +
+                '}' + super.toString();
+    }
 }
