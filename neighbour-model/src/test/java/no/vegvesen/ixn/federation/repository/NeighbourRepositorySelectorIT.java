4974ad28efac6dbd22afff983fde737d7d028ae7
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2022-04-07 11:55:27 +0200

    Fixed an import after rebase

--- neighbour-model/src/test/java/no/vegvesen/ixn/federation/repository/NeighbourRepositorySelectorIT.java
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/repository/NeighbourRepositorySelectorIT.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
