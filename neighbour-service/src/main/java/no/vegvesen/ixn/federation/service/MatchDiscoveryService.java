8b3f72cd1ebabece8a328b92d09c6fc9f046f2ea
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2022-06-09 14:51:20 +0200

    remove matches

--- neighbour-service/src/main/java/no/vegvesen/ixn/federation/service/MatchDiscoveryService.java
+++ neighbour-service/src/main/java/no/vegvesen/ixn/federation/service/MatchDiscoveryService.java
@@ -72,7 +72,7 @@ public class MatchDiscoveryService {
     }
 
     public void syncLocalSubscriptionAndSubscriptionsToTearDownMatchResources() {
-        List<Match> matches = matchRepository.findAll();
+        List<Match> matches = matchRepository.findAllByStatus(MatchStatus.UP);
         for (Match match : matches) {
             if(match.getLocalSubscription().getStatus().equals(LocalSubscriptionStatus.TEAR_DOWN) ||
                     match.getSubscription().getSubscriptionStatus().equals(SubscriptionStatus.TEAR_DOWN)) {
