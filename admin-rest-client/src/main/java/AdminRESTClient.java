637cc2759a949dd704578cbfac40e28ed25246f6
William Shum Ye <william.ye@bouvet.no>
2022-07-01 10:12:55 +0200

    Lagt til Linnea sine endringer på AdminRESTClient

--- admin-rest-client/src/main/java/AdminRESTClient.java
+++ admin-rest-client/src/main/java/AdminRESTClient.java
@@ -1,4 +1,7 @@
 import no.vegvesen.ixn.federation.model.Neighbour;
+import no.vegvesen.ixn.serviceprovider.model.ListCapabilitiesResponse;
+import no.vegvesen.ixn.serviceprovider.model.ListDeliveriesResponse;
+import no.vegvesen.ixn.serviceprovider.model.ListSubscriptionsResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -32,5 +35,40 @@ public class AdminRESTClient {
 
     public List<Neighbour> getAllNeighbours() {
         return null;
+    }
+    //Krav 3.1
+    public ListCapabilitiesResponse getNeighbourCapabilities(String neighbourName) {
+        String url = String.format("%s/%s/%s/capabilities/", server, user, neighbourName);
+        return restTemplate.getForEntity(url, ListCapabilitiesResponse.class).getBody();
+    }
+
+    //Krav 3.2
+    public ListSubscriptionsResponse getNeighbourSubscriptions(String neighbourName) {
+        String url = String.format("%s/%s/%s/subscriptions/", server, user, neighbourName);
+        return restTemplate.getForEntity(url, ListSubscriptionsResponse.class).getBody();
+    }
+
+    //mangler krav 3.3
+
+    //Mangler krav 4
+
+    //Krav 4.1
+    //Denne må sees på
+    public ListCapabilitiesResponse getServiceProviderCapabilities(String serviceProvider) {
+        String url = String.format("%s/%s/%s/capabilities/", server, user, serviceProvider);
+        return restTemplate.getForEntity(url, ListCapabilitiesResponse.class).getBody();
+    }
+
+    //Krav 4.2
+    public ListSubscriptionsResponse getServiceProviderSubscriptions(String serviceProvider) {
+        String url = String.format("%s/%s/%s/subscriptions/", server, user, serviceProvider);
+        return restTemplate.getForEntity(url, ListSubscriptionsResponse.class).getBody();
+    }
+
+    //Krav 4.3
+    //Denne må sees på
+    public ListDeliveriesResponse listServiceProviderDeliveries(String serviceProvider) {
+        String url = String.format("%s/%s/%s/deliveries",server, user, serviceProvider);
+        return restTemplate.getForEntity(url,ListDeliveriesResponse.class).getBody();
     }
 }
