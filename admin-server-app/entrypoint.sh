18eb6834e2065cbe2af3e553a8ee6d4b41620dfd
linneagustavsen <linnea.gustavsen@gmail.com>
2022-07-07 16:06:10 +0200

    testing still doesn't work because it can't connect to the database

--- admin-server-app/entrypoint.sh
+++ admin-server-app/entrypoint.sh
@@ -6,7 +6,7 @@ LOG_LEVELS=${LOG_LEVELS:-" "}
 
 java -Dspring.datasource.url=${POSTGRES_URI} \
      -Dserver.ssl.key-store=${KEY_STORE}\
-     -Dserver.port=${SP_CHNL_PORT} \
+     -Dserver.port=${ADMIN_CHNL_PORT} \
      -Dserver.ssl.key-store-password=${KEY_STORE_PASSWORD} \
      -Dserver.ssl.key-alias=${SERVER_NAME} \
      -Dserver.ssl.trust-store=${TRUST_STORE}\
--- admin-server-app/src/main/java/no/vegvesen/ixn/admin/AdminApplication.java
+++ admin-server-app/src/main/java/no/vegvesen/ixn/admin/AdminApplication.java
@@ -4,7 +4,7 @@ package no.vegvesen.ixn.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"no.vegvesen.ixn.admin"})
+@SpringBootApplication(scanBasePackages = {"no.vegvesen.ixn.federation","no.vegvesen.ixn.admin"})
 public class AdminApplication {
 
 	public static void main(String[] args) {
