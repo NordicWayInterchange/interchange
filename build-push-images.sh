3724b1ca5d6e6c9414aff5f6ec8ca5da6b21c203
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2022-08-03 12:35:08 +0200

    Removed InterchangeApp from maven and docker build

--- build-push-images.sh
+++ build-push-images.sh
@@ -5,7 +5,7 @@ echo Building and pushing docker images to container registry $1
 REGISTRY=$1
 TAG="$(git rev-parse --short HEAD)"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-IMAGES="interchange-node-app qpid message-collector-app neighbour-discoverer-app neighbour-server-app routing-configurer-app onboard-server-app"
+IMAGES="qpid message-collector-app neighbour-discoverer-app neighbour-server-app routing-configurer-app onboard-server-app"
 BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
 
 for image in ${IMAGES}; do
