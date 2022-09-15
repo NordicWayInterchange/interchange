00ba3b22009ee194277b00a86d4c2fc60b0e75e4
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2022-09-15 12:56:04 +0200

    Externalised the user for the scripts

--- systemtest-scripts/local_onboard_client.sh
+++ systemtest-scripts/local_onboard_client.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+if [ "$#" -gt 0 ]; then
+  $SERVICE_PROVIDER=$1
+else
+  $SERVICE_PROVIDER=king_olav.bouvetinterchange.eu
+fi
+
 docker run \
   -it \
   --rm \
   --name local_onboard_client \
-  --network=systemtest-scripts_testing_net \
+  --network=systemtestscripts_testing_net \
   --dns=172.28.1.1 \
   -v $PWD/../tmp/keys:/keys \
-  -e KEY_STORE=/keys/king_olav.bouvetinterchange.eu.p12 \
+  -e KEY_STORE=/keys/${SERVICE_PROVIDER}.p12 \
   -e KEY_STORE_PASSWORD=password \
   -e KEY_PASSWORD=password \
   -e TRUST_STORE_PATH=/keys/truststore.jks \
   -e TRUST_STORE_PASSWORD=password \
   -e ONBOARD_SERVER=https://local.bouvetinterchange.eu:8797/ \
-  -e USER=king_olav.bouvetinterchange.eu \
+  -e USER=${SERVICE_PROVIDER} \
   --link local_onboard_server:local.bouvetinterchange.eu \
   --entrypoint bash onboard_rest_client
--- systemtest-scripts/remote_onboard_client.sh
+++ systemtest-scripts/remote_onboard_client.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+if [ "$#" -gt 0 ]; then
+  $SERVICE_PROVIDER=$1
+else
+  $SERVICE_PROVIDER=king_gustaf.bouvetinterchange.eu
+fi
+
 docker run \
   -it \
   --rm \
   --name remote_onboard_client \
-  --network=systemtest-scripts_testing_net \
+  --network=systemtestscripts_testing_net \
   --dns=172.28.1.1 \
   -v $PWD/../tmp/keys:/keys \
-  -e KEY_STORE=/keys/king_gustaf.bouvetinterchange.eu.p12 \
+  -e KEY_STORE=/keys/${SERVICE_PROVIDER}.p12 \
   -e KEY_STORE_PASSWORD=password \
   -e KEY_PASSWORD=password \
   -e TRUST_STORE_PATH=/keys/truststore.jks \
   -e TRUST_STORE_PASSWORD=password \
   -e ONBOARD_SERVER=https://remote.bouvetinterchange.eu:8696/ \
-  -e USER=king_gustaf.bouvetinterchange.eu \
+  -e USER=${SERVICE_PROVIDER} \
   --link remote_onboard_server:remote.bouvetinterchange.eu \
   --entrypoint bash onboard_rest_client
