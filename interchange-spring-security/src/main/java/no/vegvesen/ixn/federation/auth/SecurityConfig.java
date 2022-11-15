1d34532f1c838b1f4f17f99432a57e7af2324a24
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2022-11-15 14:41:31 +0100

    Remove code in comments

--- interchange-spring-security/src/main/java/no/vegvesen/ixn/federation/auth/SecurityConfig.java
+++ interchange-spring-security/src/main/java/no/vegvesen/ixn/federation/auth/SecurityConfig.java
@@ -9,26 +9,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
-	/*
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests()
-				.anyRequest().authenticated()
-				.and()
-				.x509()
-				.subjectPrincipalRegex("CN=(.*?)(?:,|$)")
-				.userDetailsService(createUserDetailsService())
-				.and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
-				.and()
-				.csrf().disable();
-	}
-*/
 
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
