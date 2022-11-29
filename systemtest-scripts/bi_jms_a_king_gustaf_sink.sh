018e17e3eb09eb344cb40909b02bd7177e93ca58
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2022-11-29 13:57:10 +0100

    Renaming and fixing some naming on scripts for bi-queue usage

--- systemtest-scripts/bi_jms_a_king_gustaf_sink.sh
+++ systemtest-scripts/bi_jms_a_king_gustaf_sink.sh
@@ -7,12 +7,12 @@ docker run \
     --network=systemtest-scripts_testing_net \
     --dns=172.28.1.1 \
     -v ${PWD}/../tmp/keys:/keys \
-    -e URL=amqps://local.bouvetinterchange.eu \
+    -e URL=amqps://a.bouvetinterchange.eu/ \
     -e QUEUE=bi-queue \
-    -e KEY_STORE_PATH=/keys/king_olav.bouvetinterchange.eu.p12 \
+    -e KEY_STORE_PATH=/keys/king_gustaf.bouvetinterchange.eu.p12 \
     -e KEY_STORE_PASS=password \
     -e KEY_PASS=password \
     -e TRUST_STORE_PATH=/keys/truststore.jks \
     -e TRUST_STORE_PASS=password \
-    --link local_qpid:local.bouvetinterchange.eu \
+    --link a_qpid:a.bouvetinterchange.eu \
     jms_client_sink
