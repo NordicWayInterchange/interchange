fbff06b116b40a21f7bc7dee87076c0756dcd961
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2023-01-13 12:33:28 +0100

    Done with testing qpid restart on RoutingConfigurer

--- routing-configurer/src/test/java/no/vegvesen/ixn/federation/RoutingConfigurerQpidRestartIT.java
+++ routing-configurer/src/test/java/no/vegvesen/ixn/federation/RoutingConfigurerQpidRestartIT.java
@@ -3,6 +3,7 @@ package no.vegvesen.ixn.federation;
 import no.vegvesen.ixn.docker.KeysContainer;
 import no.vegvesen.ixn.docker.QpidContainer;
 import no.vegvesen.ixn.docker.QpidDockerBaseIT;
+import no.vegvesen.ixn.federation.model.*;
 import no.vegvesen.ixn.federation.properties.InterchangeNodeProperties;
 import no.vegvesen.ixn.federation.qpid.QpidClient;
 import no.vegvesen.ixn.federation.qpid.QpidClientConfig;
@@ -25,6 +26,14 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.net.ssl.SSLContext;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest(classes = {QpidClient.class, RoutingConfigurerProperties.class, QpidClientConfig.class, TestSSLContextConfigGeneratedExternalKeys.class, TestSSLProperties.class, RoutingConfigurer.class})
 @ContextConfiguration(initializers = {RoutingConfigurerQpidRestartIT.Initializer.class})
@@ -83,7 +92,72 @@ public class RoutingConfigurerQpidRestartIT extends QpidDockerBaseIT {
     InterchangeNodeProperties properties;
 
     @Test
-    public void test() {
-        System.out.println("Hei");
+    public void testSetupRegularNeighbourSubscriptionRoutingAfterRestart() {
+        String exchangeName = "cap-" + UUID.randomUUID().toString();
+
+        Capability capability = new DenmCapability("NO12345", "NO", "1.2.2", new HashSet<>(Collections.singletonList("0123")),  new HashSet<>(Collections.singletonList("5")));
+        capability.setCapabilityExchangeName(exchangeName);
+        capability.setRedirect(RedirectStatus.OPTIONAL);
+
+        client.createTopicExchange(exchangeName);
+
+        ServiceProvider serviceProvider = new ServiceProvider(
+                "my-service-provider",
+                new Capabilities(Capabilities.CapabilitiesStatus.KNOWN, new HashSet<>(Collections.singletonList(capability))),
+                Collections.emptySet(),
+                Collections.emptySet(),
+                LocalDateTime.now());
+
+        String queueName = "sub-" + UUID.randomUUID();
+
+        NeighbourSubscription sub = new NeighbourSubscription("originatingCountry = 'NO'", NeighbourSubscriptionStatus.CREATED, "neighbour");
+        sub.setQueueName(queueName);
+
+        Neighbour neighbour = new Neighbour(
+                "neighbour",
+                new Capabilities(Capabilities.CapabilitiesStatus.KNOWN, Collections.emptySet()),
+                new NeighbourSubscriptionRequest(NeighbourSubscriptionRequestStatus.ESTABLISHED, Collections.singleton(sub)),
+                new SubscriptionRequest(SubscriptionRequestStatus.ESTABLISHED, Collections.emptySet()));
+
+
+        when(serviceProviderRouter.findServiceProviders()).thenReturn(Collections.singletonList(serviceProvider));
+        routingConfigurer.setupNeighbourRouting(neighbour);
+
+        assertThat(client.queueExists(queueName)).isFalse();
+    }
+
+    @Test
+    public void testSetupRedirectNeighbourSubscriptionRoutingAfterRestart() {
+        String exchangeName = "cap-" + UUID.randomUUID().toString();
+
+        Capability capability = new DenmCapability("NO12345", "NO", "1.2.2", new HashSet<>(Collections.singletonList("0123")),  new HashSet<>(Collections.singletonList("5")));
+        capability.setCapabilityExchangeName(exchangeName);
+        capability.setRedirect(RedirectStatus.OPTIONAL);
+
+        client.createTopicExchange(exchangeName);
+
+        ServiceProvider serviceProvider = new ServiceProvider(
+                "my-service-provider",
+                new Capabilities(Capabilities.CapabilitiesStatus.KNOWN, new HashSet<>(Collections.singletonList(capability))),
+                Collections.emptySet(),
+                Collections.emptySet(),
+                LocalDateTime.now());
+
+        String queueName = "re-" + UUID.randomUUID();
+
+        NeighbourSubscription sub = new NeighbourSubscription("originatingCountry = 'NO'", NeighbourSubscriptionStatus.CREATED, "neighbour-consumer");
+        sub.setQueueName(queueName);
+
+        Neighbour neighbour = new Neighbour(
+                "neighbour",
+                new Capabilities(Capabilities.CapabilitiesStatus.KNOWN, Collections.emptySet()),
+                new NeighbourSubscriptionRequest(NeighbourSubscriptionRequestStatus.ESTABLISHED, Collections.singleton(sub)),
+                new SubscriptionRequest(SubscriptionRequestStatus.ESTABLISHED, Collections.emptySet()));
+
+
+        when(serviceProviderRouter.findServiceProviders()).thenReturn(Collections.singletonList(serviceProvider));
+        routingConfigurer.setupNeighbourRouting(neighbour);
+
+        assertThat(client.queueExists(queueName)).isFalse();
     }
 }
