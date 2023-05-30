68289b61e700138c25dc4c40f1ce510494e6b661
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-05-30 16:47:55 +0200

    More work swapping old ACL functionality for the new one

--- routing-configurer/src/main/java/no/vegvesen/ixn/federation/qpid/NewAclRule.java
+++ routing-configurer/src/main/java/no/vegvesen/ixn/federation/qpid/NewAclRule.java
@@ -3,6 +3,7 @@ package no.vegvesen.ixn.federation.qpid;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.Map;
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NewAclRule {
@@ -19,4 +20,81 @@ public class NewAclRule {
     private String objectType;
 
     private Map<String,String> attributes;
+
+
+    public NewAclRule() {
+
+    }
+
+    public NewAclRule(String identity, String operation, String outcome, String objectType, Map<String,String> attributes) {
+        this.identity = identity;
+        this.operation = operation;
+        this.outcome = outcome;
+        this.objectType = objectType;
+        this.attributes = attributes;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(String outcome) {
+        this.outcome = outcome;
+    }
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        return "NewAclRule{" +
+                "identity='" + identity + '\'' +
+                ", operation='" + operation + '\'' +
+                ", outcome='" + outcome + '\'' +
+                ", objectType='" + objectType + '\'' +
+                ", attributes=" + attributes +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NewAclRule that = (NewAclRule) o;
+        return Objects.equals(identity, that.identity) && Objects.equals(operation, that.operation) && Objects.equals(outcome, that.outcome) && Objects.equals(objectType, that.objectType) && Objects.equals(attributes, that.attributes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identity, operation, outcome, objectType, attributes);
+    }
 }
