d95ee3817d3db39bc645ec04ef148bfdf9f1452e
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-06-02 12:53:30 +0200

    First stab at creating cert from csr. Still need to add checks and restrictions.

--- csr-signer/src/main/java/no/vegvesen/ixn/cert/CertSigner.java
+++ csr-signer/src/main/java/no/vegvesen/ixn/cert/CertSigner.java
@@ -6,18 +6,31 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.openssl.PEMException;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.bouncycastle.util.io.pem.PemReader;
+import org.bouncycastle.util.io.pem.PemWriter;
 
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -26,37 +39,43 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class CertSigner {
-    private final String caCert;
-    private final String key;
+    private final PrivateKey privateKey;
+    private final X509CertificateHolder certificateHolder;
 
-    public CertSigner(String caCert, String key) {
-
-        this.caCert = caCert;
-        this.key = key;
+    public CertSigner(String caCert, String key) throws IOException {
+        PEMParser keyParser = new PEMParser(new StringReader(key));
+        PEMParser certParser = new PEMParser(new StringReader(caCert));
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+        PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(keyParser.readObject());
+        privateKey = converter.getPrivateKey(privateKeyInfo);
+        certificateHolder = (X509CertificateHolder) certParser.readObject();
     }
 
-    public String sign(String csrAsString,String cn) throws IOException {
-        PEMParser pemParser = new PEMParser(new StringReader(caCert));
-        Object result = pemParser.readObject();
-        System.out.println(result.getClass().getName());
+    public String sign(String csrAsString,String cn) throws IOException, OperatorCreationException, CertificateException {
 
-        X509CertificateHolder certificateHolder = (X509CertificateHolder)result;
 
+        PEMParser csrParser = new PEMParser(new StringReader(csrAsString));
+        PKCS10CertificationRequest csr = (PKCS10CertificationRequest) csrParser.readObject();
+        JcaPEMKeyConverter pemKeyConverter = new JcaPEMKeyConverter();
+        PublicKey csrPublicKey = pemKeyConverter.getPublicKey(csr.getSubjectPublicKeyInfo());
         X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(
                 new X500Name(String.format("CN=%s",cn)),
                 new BigInteger("1"),
                 new Date(),
                 Date.from(LocalDateTime.now().plus(1, ChronoUnit.YEARS).atZone(ZoneId.systemDefault()).toInstant()),
-                certificateHolder.getSubject(),
-                certificateHolder.getSubjectPublicKeyInfo()
+                csr.getSubject(),
+                csr.getSubjectPublicKeyInfo()
         );
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder("SHA256withRSA");
 
-        PEMParser keyParser = new PEMParser(new StringReader(key));
-        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-        PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(keyParser.readObject());
-        PrivateKey privateKey = converter.getPrivateKey(privateKeyInfo);
+        ContentSigner signer = signerBuilder.build(privateKey);
 
-        //X509CertificateHolder issuedCertHolder = certificateBuilder.build()
-        return null;
+
+        X509CertificateHolder issuedCertHolder = certificateBuilder.build(signer);
+        X509Certificate certificate = new JcaX509CertificateConverter().getCertificate(issuedCertHolder);
+        StringWriter certWriter = new StringWriter();
+        JcaPEMWriter writer = new JcaPEMWriter(certWriter);
+        writer.writeObject(certificate);
+        return certWriter.toString();
     }
 }
--- csr-signer/src/test/java/no/vegvesen/ixn/cert/CertSignerTest.java
+++ csr-signer/src/test/java/no/vegvesen/ixn/cert/CertSignerTest.java
@@ -1,32 +1,31 @@
 package no.vegvesen.ixn.cert;
 
 import no.vegvesen.ixn.docker.DockerBaseIT;
+import org.bouncycastle.operator.OperatorCreationException;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.cert.CertificateException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CertSignerTest {
 
     @Test
-    public void testSigningCsr() throws IOException {
+    public void testSigningCsr() throws IOException, OperatorCreationException, CertificateException {
         //TODO copy this to a better location
         Path path = DockerBaseIT.getTargetFolderPathForTestClass(CertSignerIT.class);
         String intermediateCaCertAsString = Files.readString(path.resolve("int.interchangetestdomain.no.crt.pem"));
         String intermediateKeyAsString = Files.readString(path.resolve("int.interchangetestdomain.no.key.pem"));
 
         String csrAsString = Files.readString(path.resolve("testSP.csr.pem"));
-        /*
-        Path chainPath = path.resolve("chain.interchangetestdomain.crt.pem");
-        if (! Files.exists(chainPath)) {
-            System.out.println("Does not exist for some reason");
-        }
-        String intermediateCertChainAsString = Files.readString(chainPath);
-*/
+
         CertSigner signer = new CertSigner(intermediateCaCertAsString,intermediateKeyAsString);
 
-        String CertificateAsString = signer.sign(csrAsString,"testSP");
+        String certificateAsString = signer.sign(csrAsString,"testSP");
+        assertThat(certificateAsString).isNotNull();
 
 
     }
