414763ca88b8ca8182a3e979cfb58cba9778887a
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-06-13 17:12:01 +0200

    Start of generating keys and csr client-side

--- napcore-rest-client/src/main/java/no/vegvesen/ixn/napcore/client/NapRESTClient.java
+++ napcore-rest-client/src/main/java/no/vegvesen/ixn/napcore/client/NapRESTClient.java
@@ -5,6 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import no.vegvesen.ixn.napcore.model.*;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +19,12 @@ import org.apache.http.impl.client.HttpClients;
 
 
 import javax.net.ssl.SSLContext;
+import javax.security.auth.x500.X500Principal;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -79,5 +92,35 @@ public class NapRESTClient {
         parameters.put("selector",selector);
         ResponseEntity<Capability[]> response = restTemplate.getForEntity(url, Capability[].class, parameters);
         return Arrays.asList(response.getBody());
+    }
+
+
+
+    //TODO this is too simple. Need to also return the keys for the client to handle in some way.
+    private String generateCSR(String serviceProviderName, String country) {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            X500Principal x500Principal = new X500Principal(
+                    String.format(
+                            "emailAddress=test@test.com, CN=%s, O=Nordic Way, C=%s",
+                            serviceProviderName,
+                            country
+                    )
+            );
+            JcaPKCS10CertificationRequestBuilder builder = new JcaPKCS10CertificationRequestBuilder(x500Principal, keyPair.getPublic());
+            JcaContentSignerBuilder signBuilder = new JcaContentSignerBuilder("SHA256withRSA");
+            ContentSigner signer = signBuilder.build(keyPair.getPrivate());
+            PKCS10CertificationRequest csr = builder.build(signer);
+            StringWriter outputWriter = new StringWriter();
+            JcaPEMWriter pemWriter = new JcaPEMWriter(outputWriter);
+            pemWriter.writeObject(csr);
+            pemWriter.close();
+            return outputWriter.toString();
+
+        } catch (NoSuchAlgorithmException | OperatorCreationException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
