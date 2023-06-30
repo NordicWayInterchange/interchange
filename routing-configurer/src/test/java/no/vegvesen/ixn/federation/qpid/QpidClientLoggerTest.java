fc24c13783941717b98f962a0457a7a4484636ac
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-06-30 19:03:41 +0200

    Cleanup of the tests

--- routing-configurer/src/test/java/no/vegvesen/ixn/federation/qpid/QpidClientLoggerTest.java
+++ routing-configurer/src/test/java/no/vegvesen/ixn/federation/qpid/QpidClientLoggerTest.java
@@ -4,23 +4,24 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class QpidClientLoggerTest {
 
     @Test
-    public void testFoo() {
+    public void removeQueue() {
         RestTemplate template = mock(RestTemplate.class);
         Logger logger = (Logger) LoggerFactory.getLogger(QpidClient.class);
         ListAppender<ILoggingEvent> appender = new ListAppender<>();
@@ -32,8 +33,24 @@ public class QpidClientLoggerTest {
                 template
         );
         when(template.getForEntity(any(), any())).thenReturn(new ResponseEntity<>(HttpStatus.OK));
-        client.removeQueue("test");
-        assertTrue(appender.list.stream().anyMatch(e -> e.getLevel().equals(Level.INFO)));
+        String queueName = "queueName";
+        client.removeQueue(queueName);
+        assertThat(infoEvents(appender.list.stream())).hasSize(1);
+        assertThat(infoEvents(appender.list.stream()).anyMatch(formattedMessageContains(queueName))).isTrue();
+        verify(template).getForEntity(any(),any());
+    }
+
+    private static Stream<ILoggingEvent> infoEvents(Stream<ILoggingEvent> stream) {
+        return stream.filter(eventIsInfo());
+
+    }
+
+    private static Predicate<ILoggingEvent> formattedMessageContains(String queueName) {
+        return e -> e.getFormattedMessage().contains(queueName);
+    }
+
+    private static Predicate<ILoggingEvent> eventIsInfo() {
+        return e -> e.getLevel().equals(Level.INFO);
     }
 
 }
