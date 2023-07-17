f5fd5d8cf5384c363205e4a7530e9a88c365fb0a
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-07-17 17:33:28 +0200

    Small internal refactoring in generating SSLContext.

--- ssl-utils/src/main/java/no/vegvesen/ixn/ssl/SSLContextFactory.java
+++ ssl-utils/src/main/java/no/vegvesen/ixn/ssl/SSLContextFactory.java
@@ -4,25 +4,47 @@ import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 @SuppressWarnings("WeakerAccess")
 public class SSLContextFactory {
 	public static SSLContext sslContextFromKeyAndTrustStores(KeystoreDetails keystoreDetails,
 															 KeystoreDetails truststoreDetails){
-		KeyStore keystore = loadKeystoreFromFile(keystoreDetails);
-		KeyStore truststore = loadKeystoreFromFile(truststoreDetails);
+		KeyStore keystore = null;
+		try {
+			InputStream stream = new FileInputStream(keystoreDetails.getFileName());
+			keystore = loadKeystoreFromStream(stream, keystoreDetails.getType(), keystoreDetails.getPassword());
+		} catch (FileNotFoundException e) {
+			throw new InvalidSSLConfig("Could not load keystore",e);
+		}
+		KeyStore truststore = null;
+		try {
+			InputStream stream = new FileInputStream(truststoreDetails.getFileName());
+			truststore = loadKeystoreFromStream(stream, truststoreDetails.getType(), truststoreDetails.getPassword());
+		} catch (FileNotFoundException e) {
+			throw new InvalidSSLConfig("Could not load truststore",e);
+		}
 		return newSSLContext(keystore, keystoreDetails.getPassword(), truststore);
 	}
 
-	private static KeyStore loadKeystoreFromFile(KeystoreDetails details) {
+	private static KeyStore loadKeystoreFromStream(InputStream stream, KeystoreType type, String password) {
 		KeyStore keyStore;
 		try {
-			keyStore = KeyStore.getInstance(details.getType().toString());
-			keyStore.load(new FileInputStream(details.getFileName()), details.getPassword().toCharArray());
-		} catch (Throwable e1) {
-			throw new InvalidSSLConfig(e1);
+			keyStore = KeyStore.getInstance(type.toString());
+		} catch (KeyStoreException e) {
+			throw new InvalidSSLConfig("Could not get Keystore instance",e);
+		}
+		try {
+			keyStore.load(stream, password.toCharArray());
+		} catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+			throw new InvalidSSLConfig("Could not load keystore",e);
 		}
 		return keyStore;
 	}
@@ -46,6 +68,10 @@ public class SSLContextFactory {
 	}
 
 	public static class InvalidSSLConfig extends RuntimeException {
+		InvalidSSLConfig(String message, Throwable t) {
+			super(message,t);
+		}
+
 		InvalidSSLConfig(Throwable e) {
 			super(e);
 		}
