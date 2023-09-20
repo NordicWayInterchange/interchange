44ab485e19e3de9da23cddeba16115efba6b8484
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2023-09-20 15:46:05 +0200

    Enforce shardcount on metadata is either the set value in the api object or set to 1 if the shardcount is null in the metadata object

---
+++ neighbour-model/src/test/java/no/vegvesen/ixn/federation/transformer/CapabilitiesTransformerTest.java
@@ -0,0 +1,47 @@
+package no.vegvesen.ixn.federation.transformer;
+
+import no.vegvesen.ixn.federation.api.v1_0.capability.CapabilitiesSplitApi;
+import no.vegvesen.ixn.federation.api.v1_0.capability.CapabilitySplitApi;
+import no.vegvesen.ixn.federation.api.v1_0.capability.DatexApplicationApi;
+import no.vegvesen.ixn.federation.api.v1_0.capability.MetadataApi;
+import no.vegvesen.ixn.federation.model.Capabilities;
+import no.vegvesen.ixn.federation.model.capability.CapabilitySplit;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CapabilitiesTransformerTest {
+
+    CapabilitiesTransformer transformer = new CapabilitiesTransformer();
+
+    @Test
+    public void addMetadataWithoutShardCount() {
+        HashSet<CapabilitySplitApi> capabilities = new HashSet<>();
+        capabilities.add(new CapabilitySplitApi(new DatexApplicationApi("myPublisherId", "pub-1", "NO", "pv1", Collections.emptySet(), "myPublicationType"), new MetadataApi()));
+        CapabilitiesSplitApi capabilitiesApi = new CapabilitiesSplitApi("norway", capabilities);
+
+        Capabilities caps = transformer.capabilitiesApiToCapabilities(capabilitiesApi);
+
+        CapabilitySplit cap = caps.getCapabilities().stream().findFirst().get();
+
+        assertThat(cap.getMetadata().getShardCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void addMetadataWithShardCount() {
+        HashSet<CapabilitySplitApi> capabilities = new HashSet<>();
+        MetadataApi metadataApi = new MetadataApi();
+        metadataApi.setShardCount(3);
+        capabilities.add(new CapabilitySplitApi(new DatexApplicationApi("myPublisherId", "pub-1", "NO", "pv1", Collections.emptySet(), "myPublicationType"), metadataApi));
+        CapabilitiesSplitApi capabilitiesApi = new CapabilitiesSplitApi("norway", capabilities);
+
+        Capabilities caps = transformer.capabilitiesApiToCapabilities(capabilitiesApi);
+
+        CapabilitySplit cap = caps.getCapabilities().stream().findFirst().get();
+
+        assertThat(cap.getMetadata().getShardCount()).isEqualTo(3);
+    }
+}
