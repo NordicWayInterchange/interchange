ae3334e08802ca44919eaae1f87d6b62e5a98074
Anna Fossen-Helle <anna.fossen-helle@bouvet.no>
2023-10-03 13:26:20 +0200

    Import of Monotch selector matcher

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/capability/CapabilityMatcher.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/capability/CapabilityMatcher.java
@@ -8,6 +8,7 @@ import org.apache.qpid.server.filter.SelectorParsingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import com.monotch.dxp.solution.nw3selectorpoc.lib.SelectorCapabilityMatcher;
 
 import java.util.HashSet;
 import java.util.Map;
