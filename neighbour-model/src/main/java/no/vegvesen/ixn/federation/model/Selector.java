cfcfb880ce2560216d15448793147296f34cd410
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-10-17 16:28:27 +0200

    Created class for selector.

---
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/Selector.java
@@ -0,0 +1,42 @@
+package no.vegvesen.ixn.federation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Embeddable
+public class Selector {
+
+
+    @Column(columnDefinition="TEXT")
+    private String selector;
+
+    public Selector() {
+    }
+
+    public Selector(String selector) {
+        this.selector = selector;
+    }
+
+    public String getSelector() {
+        return selector;
+    }
+
+    public void setSelector(String selector) {
+        this.selector = selector;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Selector selector1 = (Selector) o;
+        return Objects.equals(selector, selector1.selector);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(selector);
+    }
+}
