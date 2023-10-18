2c31380561a984b0cf63e42b120a2ffdde073dcb
Johan <johan.hansen@bouvet.no>
2023-10-18 16:23:34 +0200

    Added test for deleting channel

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/repository/PrivateChannelRepository.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/repository/PrivateChannelRepository.java
@@ -1,9 +1,13 @@
 package no.vegvesen.ixn.federation.repository;
 
 import no.vegvesen.ixn.federation.model.PrivateChannel;
+import no.vegvesen.ixn.federation.model.PrivateChannelStatus;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface PrivateChannelRepository extends CrudRepository<PrivateChannel, Integer> {
     boolean existsByPeerName(String Peername);
     PrivateChannel findByPeerName(String Peername);
+    List<PrivateChannel> findAllByStatus(PrivateChannelStatus privateChannelStatus);
 }
