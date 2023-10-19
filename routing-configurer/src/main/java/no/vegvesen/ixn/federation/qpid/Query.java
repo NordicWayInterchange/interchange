f2e8998b18a7391d9304de49d7f668ddec518c0e
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-10-19 15:06:55 +0200

    Added another Query-structure and method, which allows for more complicated query expressions.

--- routing-configurer/src/main/java/no/vegvesen/ixn/federation/qpid/Query.java
+++ routing-configurer/src/main/java/no/vegvesen/ixn/federation/qpid/Query.java
@@ -1,13 +1,27 @@
 package no.vegvesen.ixn.federation.qpid;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Query {
 
    private String sql;
+
+   private String dateTimeFormat;
+
+   private String dateTimePattern;
+
 
 
    public Query(String sql) {
        this.sql = sql;
    }
+
+    public Query(String sql, String dateTimeFormat, String dateTimePattern) {
+        this.sql = sql;
+        this.dateTimeFormat = dateTimeFormat;
+        this.dateTimePattern = dateTimePattern;
+    }
 
     public String getSql() {
         return sql;
@@ -15,5 +29,21 @@ public class Query {
 
     public void setSql(String sql) {
         this.sql = sql;
+    }
+
+    public String getDateTimeFormat() {
+        return dateTimeFormat;
+    }
+
+    public void setDateTimeFormat(String dateTimeFormat) {
+        this.dateTimeFormat = dateTimeFormat;
+    }
+
+    public String getDateTimePattern() {
+        return dateTimePattern;
+    }
+
+    public void setDateTimePattern(String dateTimePattern) {
+        this.dateTimePattern = dateTimePattern;
     }
 }
