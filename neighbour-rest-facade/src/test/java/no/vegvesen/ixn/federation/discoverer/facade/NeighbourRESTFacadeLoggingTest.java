9eb37b80abd8ad6b1c3ec65a7a58c72d1fc7be47
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-10-25 14:30:08 +0200

    Start of log testing for neighbourFacade

--- neighbour-rest-facade/src/test/java/no/vegvesen/ixn/federation/discoverer/facade/NeighbourRESTFacadeLoggingTest.java
+++ neighbour-rest-facade/src/test/java/no/vegvesen/ixn/federation/discoverer/facade/NeighbourRESTFacadeLoggingTest.java
@@ -1,7 +1,42 @@
 package no.vegvesen.ixn.federation.discoverer.facade;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.vegvesen.ixn.federation.transformer.CapabilitiesTransformer;
+import no.vegvesen.ixn.federation.transformer.CapabilityToCapabilityApiTransformer;
+import no.vegvesen.ixn.federation.transformer.SubscriptionRequestTransformer;
+import no.vegvesen.ixn.federation.transformer.SubscriptionTransformer;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.Mockito.mock;
+
 public class NeighbourRESTFacadeLoggingTest {
 
+    @Test
+    public void postCapabilities() {
+        SubscriptionTransformer subscriptionTransformer = new SubscriptionTransformer();
+        Logger logger = (Logger)LoggerFactory.getLogger(NeighbourRESTFacade.class);
+        ListAppender<ILoggingEvent> appender = new ListAppender<>();
+        appender.start();
+        logger.addAppender(appender);
+        RestTemplate template = mock(RestTemplate.class);
+        NeighbourRESTFacade client = new NeighbourRESTFacade(
+                new NeighbourRESTClient(
+                        template,
+                        new ObjectMapper()
+                ),
+                new CapabilitiesTransformer(),
+                new CapabilityToCapabilityApiTransformer(),
+                subscriptionTransformer,
+                new SubscriptionRequestTransformer(
+                        subscriptionTransformer
+                )
+        );
+    }
 
 
 }
