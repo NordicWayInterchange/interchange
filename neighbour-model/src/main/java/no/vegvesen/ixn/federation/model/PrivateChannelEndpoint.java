3a69d5efa2e1b1f11c09823bd3213246ff1283cd
Johan <johan.hansen@bouvet.no>
2023-10-30 16:11:41 +0100

    Created PrivateChannelEndpoint and added it to PrivateChannel

---
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/PrivateChannelEndpoint.java
@@ -0,0 +1,83 @@
+package no.vegvesen.ixn.federation.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="private_channel_endpoints")
+public class PrivateChannelEndpoint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "local_endpoint_seq")
+    @Column
+    private Integer id;
+    @Column
+    private String source;
+    @Column
+    private Integer port;
+    @Column
+    private String target;
+
+    public PrivateChannelEndpoint(String host, Integer port, String target) {
+        this.source = host;
+        this.port = port;
+        this.target = target;
+    }
+
+    public PrivateChannelEndpoint() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String host) {
+        this.source = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LocalEndpoint)) return false;
+        PrivateChannelEndpoint that = (PrivateChannelEndpoint) o;
+        return source.equals(that.source) &&
+                source.equals(that.source) &&
+                port.equals(that.port);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, target, port);
+    }
+
+    @Override
+    public String toString() {
+        return "LocalEndpoint{" +
+                "id=" + id +
+                ", source='" + source + '\'' +
+                ", port=" + port +
+                ", target=" + target +
+                '}';
+    }
+}
--- routing-configurer/src/main/java/no/vegvesen/ixn/federation/ServiceProviderRouter.java
+++ routing-configurer/src/main/java/no/vegvesen/ixn/federation/ServiceProviderRouter.java
@@ -234,6 +234,7 @@ public class ServiceProviderRouter {
                 provider.addQueueReadAccess(peerName, queueName);
                 qpidClient.postQpidAcl(provider);
                 privateChannel.setStatus(PrivateChannelStatus.CREATED);
+
                 logger.info("Creating queue {} for client {}", queueName, peerName);
             }
             if (privateChannel.getStatus().equals(PrivateChannelStatus.TEAR_DOWN)) {
