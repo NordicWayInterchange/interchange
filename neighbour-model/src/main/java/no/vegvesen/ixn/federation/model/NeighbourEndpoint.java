dfaf0d3a29a9c9607135eb7231fa9951e97fda6e
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2023-11-01 16:15:25 +0100

    Adding setter for NeighbourEndpoint, this might have something to do with our duplicates (crossing fingers, as we cannot reproduce in automated tests)

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/NeighbourEndpoint.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/NeighbourEndpoint.java
@@ -75,6 +75,10 @@ public class NeighbourEndpoint {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
