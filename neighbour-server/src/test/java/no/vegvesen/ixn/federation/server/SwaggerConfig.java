91effd48d33af5b6afef06ee54bbeaedb7864cce
Johan <johan.hansen@bouvet.no>
2024-02-08 10:15:23 +0100

    Changes: Removed 'servers'-list in swagger-UI Added fields for description, license, contact Cleaned up some of the example objects

--- neighbour-server/src/test/java/no/vegvesen/ixn/federation/server/SwaggerConfig.java
+++ neighbour-server/src/test/java/no/vegvesen/ixn/federation/server/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package no.vegvesen.ixn.federation.server;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info=@Info(title = "Neighbour API", version = "1.0"))
+@OpenAPIDefinition(info=@Info(title = "Neighbour API",
+        version = "1.0",
+        contact = @Contact(name = "", email = "email@email.no")
+        ,license = @License(url = "https://www.apache.org/licenses/LICENSE-2.0", name = "Apache 2.0"),
+        description = "Description????"),
+        servers = @Server(url="", description = "test"))
 public class SwaggerConfig {
 
 }
