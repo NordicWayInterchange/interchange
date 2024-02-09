a67a57bb8843b556d042435e2b06c8cd98eaecae
Johan <johan.hansen@bouvet.no>
2024-02-09 10:02:26 +0100

    Added more examples for AddCapabilities

--- api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/capability/DenmApplicationApi.java
+++ api-model/src/main/java/no/vegvesen/ixn/federation/api/v1_0/capability/DenmApplicationApi.java
@@ -1,5 +1,6 @@
 package no.vegvesen.ixn.federation.api.v1_0.capability;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import no.vegvesen.ixn.federation.api.v1_0.Constants;
 import no.vegvesen.ixn.properties.CapabilityProperty;
 import java.util.HashMap;
@@ -7,6 +8,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+@Schema(hidden = true)
 public class DenmApplicationApi extends ApplicationApi {
 
     private Set<Integer> causeCode = new HashSet<>();
