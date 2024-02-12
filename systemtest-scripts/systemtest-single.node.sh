220c598b5ddc8d8f76a24a92bed6d2831ad127c8
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-02-12 14:04:41 +0100

    New version with single node for easy testing

---
+++ systemtest-scripts/systemtest-single.node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+#Set environment variable to choose what docker image tag to use in the test
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+export BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
+
+echo Running system test on branch $BRANCH with tag $BRANCH_TAG
+
+cd ../onboard-rest-client
+docker build . -t onboard_rest_client
+cd ../napcore-rest-client
+docker build . -t napcore_rest_client
+cd ../jms-client-source-app
+docker build . -t jms_client_source_app
+cd ../systemtest-scripts
+[ -f ../tmp/keys/a.bouvetinterchange.eu.p12 ] || ./systemtest-keys.sh
+docker-compose -f single-node-systemtest.yml build && docker-compose -f single-node-systemtest.yml up
