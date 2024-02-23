811e7ea6af6ce5bb72c50e7691cadcbdf9f336c1
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-02-23 15:55:53 +0100

    Added send and receive of messages to the JmsClientApplication

--- jms-client-app/src/main/java/no/vegvesen/ixn/JmsClientApplication.java
+++ jms-client-app/src/main/java/no/vegvesen/ixn/JmsClientApplication.java
@@ -1,6 +1,8 @@
 package no.vegvesen.ixn;
 
 import no.vegvesen.ixn.client.command.JmsTopCommand;
+import no.vegvesen.ixn.client.command.ReceiveMessages;
+import no.vegvesen.ixn.client.command.SendMessage;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -14,7 +16,8 @@ import java.util.concurrent.Callable;
         description = "Jms Client Application",
         defaultValueProvider = CommandLine.PropertiesDefaultProvider.class,
         subcommands = {
-
+                SendMessage.class,
+                ReceiveMessages.class
         })
 public class JmsClientApplication implements Callable<Integer>, JmsTopCommand {
     @Override
