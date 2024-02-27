9a962b781132adafb99ef2c32309d9fe4e8fcc26
Johan <johan.hansen@bouvet.no>
2024-02-27 10:49:04 +0100

    Tested and deploy to gh packages works. Reverted back to version 1.0.3.

--- napcore-rest-client/entrypoint.sh
+++ napcore-rest-client/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 java -jar napcore-rest-client.jar \
-  -k $KEY_STORE \
-  -s $KEY_STORE_PASSWORD \
-  -t $TRUST_STORE_PATH \
-  -w $TRUST_STORE_PASSWORD \
-  $NAP_SERVER \
-  $USER \
-  $NAP "$@"
+  -e $KEY_STORE \
+  -e $KEY_STORE_PASSWORD \
+  -e $TRUST_STORE_PATH \
+  -e $TRUST_STORE_PASSWORD \
+  -e NAP_SERVER \
+  -e USER \
