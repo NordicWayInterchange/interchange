0c13b7ba408d8d5a1e542f8c63da85a33736e353
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-02-27 10:50:18 +0100

    Added some printlines to check that the commands are being run

--- jms-client-app/src/main/java/no/vegvesen/ixn/client/command/ReceiveMessages.java
+++ jms-client-app/src/main/java/no/vegvesen/ixn/client/command/ReceiveMessages.java
@@ -2,21 +2,24 @@ package no.vegvesen.ixn.client.command;
 
 import no.vegvesen.ixn.Sink;
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
 
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "receivemessages", description = "Receive messages and print them to stdout")
+@Command(name = "receivemessages", description = "Receive messages and print them to stdout")
 public class ReceiveMessages implements Callable<Integer> {
 
-    @CommandLine.Parameters(index = "0", paramLabel = "QUEUE", description = "The queueName to connect to")
+    @Parameters(index = "0", paramLabel = "QUEUE", description = "The queueName to connect to")
     private String queueName;
-    @CommandLine.ParentCommand
+    @ParentCommand
     JmsTopCommand parentCommand;
 
     @Override
     public Integer call() throws Exception {
+        System.out.println(String.format("Listening for messages from queue [%s] on server [%s]", queueName, parentCommand.getUrl()));
         try (Sink sink = new Sink(parentCommand.getUrl(), queueName, parentCommand.createContext())) {
-            System.out.println(String.format("Listening for messages from queue [%s] on server [%s]", queueName, parentCommand.getUrl()));
             sink.start();
         }
         return 0;
