cf1c20cc7a5261ac257500f603edcf597440e94a
Johan <johan.hansen@bouvet.no>
2024-02-28 15:20:57 +0100

    Rejoined onboard-server and onboard-server-app

--- onboard-server/src/test/java/no/vegvesen/ixn/serviceprovider/TestApplication.java
+++
@@ -1,7 +0,0 @@
-package no.vegvesen.ixn.serviceprovider;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "no.vegvesen.ixn")
-public class TestApplication {
-}
