a3c55cd7bdcafb6f03ef5404e11b75defa3d0184
Johan <johan.hansen@bouvet.no>
2024-02-28 13:28:51 +0100

    Rejoined message-collector and message-collector-app

--- message-collector/src/main/java/no/vegvesen/ixn/federation/messagecollector/Application.java
+++ message-collector/src/main/java/no/vegvesen/ixn/federation/messagecollector/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = "no.vegvesen.ixn")
+@SpringBootApplication(scanBasePackages = {"no.vegvesen.ixn"})
 public class Application {
 
     public static void main(String[] args){
