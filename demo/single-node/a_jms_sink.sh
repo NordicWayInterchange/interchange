c83292acad5bf42004b6260f45ef075a21abeb63
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-03-01 14:38:45 +0100

    finally, a jms sink

---
+++ demo/single-node/a_jms_sink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run \
+  -it \
+  --rm \
+  --name jms_client_sink_app \
+  --network=single-node_singletest \
+  --dns=172.28.1.1 \
+  -v ${PWD}/../keys/a:/keys \
+  -v ${PWD}:/work \
+  --link a_qpid:a.interchangedomain.com \
+   europe-west4-docker.pkg.dev/nw-shared-w3ml/nordic-way-interchange/jms-client-sink-app:6a8a722 -k /keys/king_olav.a.interchangedomain.com.p12  -s password -t /keys/truststore.jks -w password amqps://a.interchangedomain.com "$@"
