eb784bdd1372586f811546976c3b0af05f74b17d
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-03-04 15:12:04 +0100

    Add capability command added

--- onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/capabilities/AddServiceProviderCapability.java
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/capabilities/AddServiceProviderCapability.java
@@ -2,27 +2,32 @@ package no.vegvesen.ixn.serviceprovider.client.command.capabilities;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.vegvesen.ixn.serviceprovider.client.OnboardRESTClient;
-import no.vegvesen.ixn.serviceprovider.client.OnboardRestClientApplication;
 import no.vegvesen.ixn.serviceprovider.model.AddCapabilitiesRequest;
 import no.vegvesen.ixn.serviceprovider.model.AddCapabilitiesResponse;
-import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParentCommand;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "addcapability", description = "Add service provider capability from file")
+@Command(
+        name = "add",
+        description = "Add service provider capability from file",
+        mixinStandardHelpOptions = true
+)
 public class AddServiceProviderCapability implements Callable<Integer> {
 
-    @CommandLine.ParentCommand
-    OnboardRestClientApplication parentCommand;
+    @ParentCommand
+    CapabilitiesCommand parentCommand;
 
-    @CommandLine.Option(names = {"-f", "--filename"}, description = "The capability json file")
+    @Option(names = {"-f", "--filename"}, description = "The capability json file")
     File file;
 
     @Override
     public Integer call() throws IOException {
-        OnboardRESTClient client = parentCommand.createClient();
+        OnboardRESTClient client = parentCommand.getParentCommand().createClient();
         ObjectMapper mapper = new ObjectMapper();
         AddCapabilitiesRequest capability = mapper.readValue(file, AddCapabilitiesRequest.class);
         AddCapabilitiesResponse result = client.addCapability(capability);
