d9b1d92e08f57773968acbd415502bc345f65d06
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-03-05 13:20:09 +0100

    Subscriptions moved to separate command

---
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/subscriptions/AddServiceProviderSubscription.java
@@ -0,0 +1,39 @@
+package no.vegvesen.ixn.serviceprovider.client.command.subscriptions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.vegvesen.ixn.serviceprovider.client.OnboardRESTClient;
+import no.vegvesen.ixn.serviceprovider.client.OnboardRestClientApplication;
+import no.vegvesen.ixn.serviceprovider.model.AddSubscriptionsRequest;
+import no.vegvesen.ixn.serviceprovider.model.AddSubscriptionsResponse;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParentCommand;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+@Command(
+        name = "add",
+        description = "Add a subscription for the service provider",
+        defaultValueProvider = CommandLine.PropertiesDefaultProvider.class,
+        mixinStandardHelpOptions = true
+)
+public class AddServiceProviderSubscription implements Callable<Integer> {
+
+    @ParentCommand
+    SubscriptionsCommand parentCommand;
+
+    @Option(names = {"-f", "--filename"}, description = "The subscription json file")
+    File file;
+
+    @Override
+    public Integer call() throws Exception {
+        OnboardRESTClient client = parentCommand.getParent().createClient();
+        ObjectMapper mapper = new ObjectMapper();
+        AddSubscriptionsRequest requestApi = mapper.readValue(file, AddSubscriptionsRequest.class);
+        AddSubscriptionsResponse result = client.addSubscription(requestApi);
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
+        return 0;
+    }
+}
