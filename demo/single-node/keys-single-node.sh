1f4027fb144030e9cc23af8334424f404c0a0f25
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-03-07 14:55:37 +0100

    Allow the keys to be created as the current user

--- demo/single-node/keys-single-node.sh
+++ demo/single-node/keys-single-node.sh
@@ -4,6 +4,7 @@
 TMP_FOLDER=$(realpath -s ../tmp)
 echo Generating systemtest keys to folder $TMP_FOLDER
 
+mkdir -p $TMP_FOLDER/keys
 docker build ../../key-gen -t key-gen
-docker run -it -e CA_CN=dns.lookupdomain.com -e KEY_CNS="nap.a.interchangedomain.com a.interchangedomain.com king_olav.a.interchangedomain.com" -v $TMP_FOLDER/keys/:/jks/keys key-gen:latest
+docker run -it --user=$UID -e CA_CN=dns.lookupdomain.com -e KEY_CNS="a.interchangedomain.com king_olav.a.interchangedomain.com" -v $TMP_FOLDER/keys/:/jks/keys key-gen:latest
 
