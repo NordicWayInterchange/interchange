59df999e7788eefd12232f6ecbc363804f6e4b54
Johan <johan.hansen@bouvet.no>
2024-03-11 10:41:14 +0100

    Added tests for new checks in NeighbourRestController. Also adjusted CapabilitiesSplitApi object

--- neighbour-server/src/main/java/no/vegvesen/ixn/federation/server/NeighbourRestController.java
+++ neighbour-server/src/main/java/no/vegvesen/ixn/federation/server/NeighbourRestController.java
@@ -59,7 +59,7 @@ public class NeighbourRestController {
 	public SubscriptionResponseApi requestSubscriptions(@RequestBody SubscriptionRequestApi neighbourSubscriptionRequest) {
 		logger.debug("Received incoming subscription request: {}", neighbourSubscriptionRequest.toString());
 
-		if(neighbourSubscriptionRequest == null || neighbourSubscriptionRequest.getSubscriptions() == null || neighbourSubscriptionRequest.getSubscriptions().isEmpty()){
+		if(neighbourSubscriptionRequest.getSubscriptions() == null || neighbourSubscriptionRequest.getSubscriptions().isEmpty()){
 			throw new SubscriptionRequestException("Subscriptions can not be null");
 		}
 
@@ -91,7 +91,6 @@ public class NeighbourRestController {
 		return reponse;
 	}
 
-
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(method = RequestMethod.GET, value = "/{ixnName}/subscriptions/{subscriptionId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@Secured("ROLE_USER")
@@ -118,7 +117,7 @@ public class NeighbourRestController {
 	public CapabilitiesSplitApi updateCapabilities(@RequestBody CapabilitiesSplitApi neighbourCapabilities) {
 		logger.info("Received capability post: {}", neighbourCapabilities.toString());
 
-		if(neighbourCapabilities == null || neighbourCapabilities.getCapabilities() == null){
+		if(neighbourCapabilities.getCapabilities() == null){
 			throw new CapabilityPostException("Capabilities can not be null");
 		}
 
