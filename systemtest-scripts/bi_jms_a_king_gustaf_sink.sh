8e1786dd8c8d2c248d6e71ce564a9d5723f19605
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-03-12 13:34:10 +0100

    Some more changes to reflect the new clients

--- systemtest-scripts/bi_jms_a_king_gustaf_sink.sh
+++ systemtest-scripts/bi_jms_a_king_gustaf_sink.sh
@@ -3,15 +3,9 @@
 docker run \
     -it \
     --rm \
-    --name jms_client_sink \
+    --name jms_client_sink_app \
     --network=systemtest-scripts_testing_net \
     --dns=172.28.1.1 \
     -v ${PWD}/../tmp/keys:/keys \
-    -e URL=amqps://a.bouvetinterchange.eu/ \
-    -e QUEUE=bi-queue \
-    -e KEY_STORE_PATH=/keys/king_gustaf.bouvetinterchange.eu.p12 \
-    -e KEY_STORE_PASS=password \
-    -e TRUST_STORE_PATH=/keys/truststore.jks \
-    -e TRUST_STORE_PASS=password \
     --link a_qpid:a.bouvetinterchange.eu \
-    jms_client_sink
+    jms_client_sink_app -k /keys/king_gustaf.bouvetinterchange.eu.p12 -s password  -t /keys/truststore.jks -w password amqps://a.bouvetinterchange.eu/ receivemessages bi-queue
--- systemtest-scripts/systemtest.sh
+++ systemtest-scripts/systemtest.sh
@@ -6,12 +6,9 @@ export BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
 
 echo Running system test on branch $BRANCH with tag $BRANCH_TAG
 
-cd ../onboard-rest-client
-docker build . -t onboard_rest_client
-cd ../napcore-rest-client
-docker build . -t napcore_rest_client
-cd ../jms-client-source-app
-docker build . -t jms_client_source_app
-cd ../systemtest-scripts
+docker build ../onboard-rest-client -t onboard_rest_client
+docker build ../napcore-rest-client -t napcore_rest_client
+docker build ../jms-client-source-app -t jms_client_source_app
+docker build ../jms-client-sink-app -t jms_client_sink_app
 [ -f ../tmp/keys/a.bouvetinterchange.eu.p12 ] || ./systemtest-keys.sh
 docker-compose -f systemtest.yml build && docker-compose -f systemtest.yml up
