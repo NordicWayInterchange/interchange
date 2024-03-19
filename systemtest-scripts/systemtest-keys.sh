97bacb69486caaca660c6b6000529ab2e75b505a
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-03-19 15:35:42 +0100

    Extracted internal users for HTTPS to its own keystore

--- systemtest-scripts/systemtest-keys.sh
+++ systemtest-scripts/systemtest-keys.sh
@@ -8,5 +8,5 @@ mkdir -p $TMP_FOLDER
 REALFOLDER=$(realpath -s $TMP_FOLDER)
 
 docker build ../key-gen -t key-gen
-docker run -it --user=$(id -u):$(id -g) -e CA_CN=ca.bouvetinterchange.eu -e KEY_CNS="nap.bouvetinterchange.eu a.bouvetinterchange.eu b.bouvetinterchange.eu c.bouvetinterchange.eu king_gustaf.bouvetinterchange.eu king_olav.bouvetinterchange.eu king_charles.bouvetinterchange.eu" -v $REALFOLDER:/jks/keys key-gen:latest
+docker run -it --user=$(id -u):$(id -g) -e CA_CN=ca.bouvetinterchange.eu -e KEY_CNS="nap.bouvetinterchange.eu a.bouvetinterchange.eu b.bouvetinterchange.eu c.bouvetinterchange.eu king_gustaf.bouvetinterchange.eu king_olav.bouvetinterchange.eu king_charles.bouvetinterchange.eu a_qpid a_routing_configurer b_qpid b_routing_configurer" -v $REALFOLDER:/jks/keys key-gen:latest
 
