35df64e2798b8e6b004696b882a79e2b1b5de071
Johan <johan.hansen@bouvet.no>
2024-04-26 16:06:47 +0200

    Revert "Override equals and hashcode for Capabilities"
    This reverts commit ddcd57ad8e51578c7fdd5591109bfaba1ad0b9ef.

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/Capabilities.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/Capabilities.java
@@ -140,18 +140,6 @@ public class Capabilities {
 		setLastCapabilityExchange(LocalDateTime.now());
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Capabilities that = (Capabilities) o;
-		return Objects.equals(id, that.id);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
 
 	@Override
 	public String toString() {
