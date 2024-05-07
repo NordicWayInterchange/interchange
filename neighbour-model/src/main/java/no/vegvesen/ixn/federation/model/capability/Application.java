7a84ab24d55381d22c24f2faa355646c4f195408
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-05-07 15:52:28 +0200

    Small fix to hashCode

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/capability/Application.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/capability/Application.java
@@ -115,7 +115,7 @@ public abstract class Application {
                 publicationId,
                 originatingCountry,
                 protocolVersion,
-                (quadTree != null ? new ArrayList<>(quadTree).hashCode() : new ArrayList<>()).hashCode());
+                (quadTree != null ? new ArrayList<>(quadTree).hashCode() : new ArrayList<>().hashCode()));
     }
 
     @Override
