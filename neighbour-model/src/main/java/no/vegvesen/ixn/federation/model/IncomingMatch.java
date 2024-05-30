c56bff35b14a40c06223e05a04133596da49f41a
Johan <johan.hansen@bouvet.no>
2024-05-30 17:03:32 +0200

    Made some changes, match creation and deletion happens right after subscription is set to CREATED. Some logic moved to RoutingConfigurer

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/IncomingMatch.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/model/IncomingMatch.java
@@ -3,9 +3,6 @@ package no.vegvesen.ixn.federation.model;
 import jakarta.persistence.*;
 import no.vegvesen.ixn.federation.model.capability.Capability;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Entity
 @Table(name="incoming_matches")
 public class IncomingMatch {
@@ -14,26 +11,20 @@ public class IncomingMatch {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inc_match_seq")
     private Integer id;
 
-    @OneToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private NeighbourSubscription neighbourSubscription;
 
-    @OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @OneToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     @JoinColumn(name="incoming_match_id")
-    private Set<Capability> capabilities = new HashSet<>();
+    private Capability capability;
 
     public IncomingMatch(){}
 
-    public IncomingMatch(NeighbourSubscription neighbourSubscription, Set<Capability> capabilities){
+    public IncomingMatch(NeighbourSubscription neighbourSubscription, Capability capability){
         this.neighbourSubscription = neighbourSubscription;
-        addCapabilities(capabilities);
+        this.capability = capability;
     }
 
-    public void addCapability(Capability capability){
-        this.capabilities.add(capability);
-    }
-    public void addCapabilities(Set<Capability> capabilities){
-        this.capabilities.addAll(capabilities);
-    }
     public NeighbourSubscription getNeighbourSubscription() {
         return neighbourSubscription;
     }
@@ -42,15 +33,12 @@ public class IncomingMatch {
         this.neighbourSubscription = neighbourSubscription;
     }
 
-    public Set<Capability> getCapabilities() {
-        return capabilities;
+    public Capability getCapability() {
+        return capability;
     }
 
-    public void setCapabilities(Set<Capability> capabilities) {
-        this.capabilities.clear();
-        if(capabilities != null){
-            this.capabilities.addAll(capabilities);
-        }
+    public void setCapability(Capability capability) {
+        this.capability = capability;
     }
 
     public void setId(Integer id) {
--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/repository/IncomingMatchRepository.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/repository/IncomingMatchRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface IncomingMatchRepository extends CrudRepository<IncomingMatch, Integer> {
-    IncomingMatch findByNeighbourSubscriptionId(Integer id);
+    List<IncomingMatch> findAllByNeighbourSubscriptionId(Integer id);
     List<IncomingMatch> findAll();
 }
