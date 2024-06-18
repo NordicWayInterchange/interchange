4580c4f281915c0448f24ca8317f183b8db90be4
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-06-18 14:15:00 +0200

    Updated old structure to new keys. Need to update the config to reflect the new split between internal and external cas

--- demo/single-node/keys-single-node.sh
+++ demo/single-node/keys-single-node.sh
@@ -3,9 +3,9 @@
 
 TMP_FOLDER=../keys/a
 mkdir -p $TMP_FOLDER
-REAL_FOLDER=$(realpath -s $TMP_FOLDER)
-echo Generating systemtest keys to folder $REAL_FOLDER
+#REAL_FOLDER=$(realpath -s $TMP_FOLDER)
+#echo Generating systemtest keys to folder $REAL_FOLDER
 
-docker build ../../key-gen -t key-gen
-docker run -it --user=$(id -u):$(id -g) -e CA_CN=dns.lookupdomain.com -e KEY_CNS="a.interchangedomain.com king_olav.a.interchangedomain.com" -v $REAL_FOLDER:/jks/keys key-gen:latest
-
+#docker build ../../key-gen -t key-gen
+#docker run -it --user=$(id -u):$(id -g) -e CA_CN=dns.lookupdomain.com -e KEY_CNS="a.interchangedomain.com king_olav.a.interchangedomain.com" -v $REAL_FOLDER:/jks/keys key-gen:latest
+java -jar ../../keys-generator/target/keys-generator-1.0.3-SNAPSHOT.jar generate -f ../keys/keys_a.json -o ../keys/a
