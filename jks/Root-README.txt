b8b66b82e8c7530f9cd65681c6a20d96e2144a81
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2024-06-18 14:48:18 +0200

    Removed the key-gen and jks folders, as they should not be needed anymore

--- jks/Root-README.txt
+++
@@ -1,6 +0,0 @@
-RootCA:
-	run makeRootCA.sh
-	
-	get CSR from nodeCA
-	run signCSR.sh
-	send back ca/certs/* and ca/intermediate/certs/*
--- jks/node/generate_trust_store.sh
+++
@@ -1,8 +0,0 @@
-if [ "$#" -ne 2 ]; then
-	echo "USAGE: $0 <ca domain name> <password>"
-	exit 1 
-fi
-keytool \
-	-import \
-	-trustcacerts \
-	-file ca/certs/ca.${1}.crt.pem -keystore truststore.jks -storepass $2 -noprompt
