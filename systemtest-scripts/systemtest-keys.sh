8b5ade34d0862cc68a2e07e08978ebaecd9dfdbe
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-06-18 13:06:34 +0200

    Added key generation using java code instead of container

--- systemtest-scripts/systemtest-keys.sh
+++ systemtest-scripts/systemtest-keys.sh
@@ -5,8 +5,4 @@ TMP_FOLDER=../tmp/keys/
 echo Generating systemtest keys to folder $TMP_FOLDER
 mkdir -p $TMP_FOLDER
 
-REALFOLDER=$(realpath -s $TMP_FOLDER)
-
-docker build ../key-gen -t key-gen
-docker run -it --user=$(id -u):$(id -g) -e CA_CN=ca.bouvetinterchange.eu -e KEY_CNS="nap.bouvetinterchange.eu a.bouvetinterchange.eu b.bouvetinterchange.eu c.bouvetinterchange.eu king_gustaf.bouvetinterchange.eu king_olav.bouvetinterchange.eu king_charles.bouvetinterchange.eu a_qpid a_routing_configurer b_qpid b_routing_configurer" -v $REALFOLDER:/jks/keys key-gen:latest
-
+java -jar ../keys-generator/target/keys-generator-1.0.3-SNAPSHOT.jar generate -f systemtest-keys.yml -o "$TMP_FOLDER"
