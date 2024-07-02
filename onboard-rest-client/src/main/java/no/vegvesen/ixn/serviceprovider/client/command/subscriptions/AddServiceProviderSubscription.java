28ecfa5eadad42f770cfc65652b19f539b0cbd1c
Johan <johan.hansen@bouvet.no>
2024-07-02 16:08:16 +0200

    Subscriptions can now be added without using file

--- onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/subscriptions/AddServiceProviderSubscription.java
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/subscriptions/AddServiceProviderSubscription.java
@@ -2,15 +2,15 @@ package no.vegvesen.ixn.serviceprovider.client.command.subscriptions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.vegvesen.ixn.serviceprovider.client.OnboardRESTClient;
-import no.vegvesen.ixn.serviceprovider.client.OnboardRestClientApplication;
+import no.vegvesen.ixn.serviceprovider.model.AddSubscription;
 import no.vegvesen.ixn.serviceprovider.model.AddSubscriptionsRequest;
 import no.vegvesen.ixn.serviceprovider.model.AddSubscriptionsResponse;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 import picocli.CommandLine.ParentCommand;
 
 import java.io.File;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 @Command(
@@ -24,16 +24,34 @@ public class AddServiceProviderSubscription implements Callable<Integer> {
     @ParentCommand
     SubscriptionsCommand parentCommand;
 
-    @Option(names = {"-f", "--filename"}, required = true, description = "The subscription json file")
-    File file;
+    @CommandLine.ArgGroup(exclusive = true, multiplicity = "1")
+    Option option;
 
     @Override
     public Integer call() throws Exception {
-        OnboardRESTClient client = parentCommand.getParent().createClient();
-        ObjectMapper mapper = new ObjectMapper();
-        AddSubscriptionsRequest requestApi = mapper.readValue(file, AddSubscriptionsRequest.class);
-        AddSubscriptionsResponse result = client.addSubscription(requestApi);
-        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
+        if(option.file != null) {
+            OnboardRESTClient client = parentCommand.getParent().createClient();
+            ObjectMapper mapper = new ObjectMapper();
+            AddSubscriptionsRequest requestApi = mapper.readValue(option.file, AddSubscriptionsRequest.class);
+            AddSubscriptionsResponse result = client.addSubscription(requestApi);
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
+        }
+        else{
+            OnboardRESTClient client = parentCommand.getParent().createClient();
+            ObjectMapper mapper = new ObjectMapper();
+            AddSubscriptionsRequest requestApi = new AddSubscriptionsRequest(client.getUser(), Set.of(new AddSubscription(option.selector)));
+            AddSubscriptionsResponse result = client.addSubscription(requestApi);
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
+        }
         return 0;
     }
+
+    static class Option {
+        @CommandLine.Option(names = {"-f", "--filename"}, required = true, description = "The subscription json file")
+        File file;
+
+        @CommandLine.Option(names = {"-s", "--selector"}, required = true, description = "The subscription selector")
+        String selector;
+    }
 }
+
