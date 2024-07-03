73c0522482cf9986f84fb74183ce45d38c44ef04
Johan <johan.hansen@bouvet.no>
2024-07-03 10:28:42 +0200

    Add delivery, privatechannel and subscription in Onboard rest client no longer needs a file.

--- onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/deliveries/AddDeliveries.java
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/deliveries/AddDeliveries.java
@@ -2,38 +2,52 @@ package no.vegvesen.ixn.serviceprovider.client.command.deliveries;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.vegvesen.ixn.serviceprovider.client.OnboardRESTClient;
-import no.vegvesen.ixn.serviceprovider.client.OnboardRestClientApplication;
 import no.vegvesen.ixn.serviceprovider.model.AddDeliveriesRequest;
 import no.vegvesen.ixn.serviceprovider.model.AddDeliveriesResponse;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.ParentCommand;
+import no.vegvesen.ixn.serviceprovider.model.SelectorApi;
+import static picocli.CommandLine.*;
 
 import java.io.File;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 @Command(
         name = "add",
         description = "Add deliveries for service provider",
-        defaultValueProvider = CommandLine.PropertiesDefaultProvider.class,
+        defaultValueProvider = PropertiesDefaultProvider.class,
         mixinStandardHelpOptions = true
 )
 public class AddDeliveries implements Callable<Integer> {
     @ParentCommand
     DeliveriesCommand parentCommand;
 
-    @Option(names = {"-f", "--filename"}, required = true, description = "The deliveries json file")
-    File file;
+    @ArgGroup(exclusive = true, multiplicity = "1")
+    AddDeliveriesOption option;
 
 
     @Override
     public Integer call() throws Exception {
         OnboardRESTClient client = parentCommand.getParent().createClient();
         ObjectMapper mapper = new ObjectMapper();
-        AddDeliveriesRequest request = mapper.readValue(file, AddDeliveriesRequest.class);
-        AddDeliveriesResponse response = client.addServiceProviderDeliveries(request);
-        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+
+        if (option.file != null) {
+            AddDeliveriesRequest request = mapper.readValue(option.file, AddDeliveriesRequest.class);
+            AddDeliveriesResponse response = client.addServiceProviderDeliveries(request);
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+        }
+        else{
+            AddDeliveriesRequest request = new AddDeliveriesRequest(client.getUser(), Set.of(new SelectorApi(option.selector)));
+            AddDeliveriesResponse response = client.addServiceProviderDeliveries(request);
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+        }
         return 0;
     }
+}
+
+class AddDeliveriesOption{
+    @Option(names = {"-f", "--filename"}, required = true, description = "The deliveries json file")
+    File file;
+
+    @Option(names = {"-s", "--selector"}, required = true, description = "The delivery selector")
+    String selector;
 }
--- onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/subscriptions/AddServiceProviderSubscription.java
+++ onboard-rest-client/src/main/java/no/vegvesen/ixn/serviceprovider/client/command/subscriptions/AddServiceProviderSubscription.java
@@ -5,9 +5,7 @@ import no.vegvesen.ixn.serviceprovider.client.OnboardRESTClient;
 import no.vegvesen.ixn.serviceprovider.model.AddSubscription;
 import no.vegvesen.ixn.serviceprovider.model.AddSubscriptionsRequest;
 import no.vegvesen.ixn.serviceprovider.model.AddSubscriptionsResponse;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.ParentCommand;
+import static picocli.CommandLine.*;
 
 import java.io.File;
 import java.util.Set;
@@ -16,7 +14,7 @@ import java.util.concurrent.Callable;
 @Command(
         name = "add",
         description = "Add a subscription for the service provider",
-        defaultValueProvider = CommandLine.PropertiesDefaultProvider.class,
+        defaultValueProvider = PropertiesDefaultProvider.class,
         mixinStandardHelpOptions = true
 )
 public class AddServiceProviderSubscription implements Callable<Integer> {
@@ -24,34 +22,32 @@ public class AddServiceProviderSubscription implements Callable<Integer> {
     @ParentCommand
     SubscriptionsCommand parentCommand;
 
-    @CommandLine.ArgGroup(exclusive = true, multiplicity = "1")
-    Option option;
+    @ArgGroup(exclusive = true, multiplicity = "1")
+    AddServiceProviderSubscriptionOption option;
 
     @Override
     public Integer call() throws Exception {
+        OnboardRESTClient client = parentCommand.getParent().createClient();
+        ObjectMapper mapper = new ObjectMapper();
+
         if(option.file != null) {
-            OnboardRESTClient client = parentCommand.getParent().createClient();
-            ObjectMapper mapper = new ObjectMapper();
             AddSubscriptionsRequest requestApi = mapper.readValue(option.file, AddSubscriptionsRequest.class);
             AddSubscriptionsResponse result = client.addSubscription(requestApi);
             System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
         }
         else{
-            OnboardRESTClient client = parentCommand.getParent().createClient();
-            ObjectMapper mapper = new ObjectMapper();
             AddSubscriptionsRequest requestApi = new AddSubscriptionsRequest(client.getUser(), Set.of(new AddSubscription(option.selector)));
             AddSubscriptionsResponse result = client.addSubscription(requestApi);
             System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
         }
         return 0;
     }
-
-    static class Option {
-        @CommandLine.Option(names = {"-f", "--filename"}, required = true, description = "The subscription json file")
-        File file;
-
-        @CommandLine.Option(names = {"-s", "--selector"}, required = true, description = "The subscription selector")
-        String selector;
-    }
 }
 
+class AddServiceProviderSubscriptionOption {
+    @Option(names = {"-f", "--filename"}, required = true, description = "The subscription json file")
+    File file;
+
+    @Option(names = {"-s", "--selector"}, required = true, description = "The subscription selector")
+    String selector;
+}
