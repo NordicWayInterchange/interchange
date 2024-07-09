61a0af4e9c70d5ddf668f6b058c961224c1baa67
Johan <johan.hansen@bouvet.no>
2024-07-09 12:13:25 +0200

    Moved qpid-client to it's own module

---
+++ qpid-client/src/main/java/no/vegvesen/ixn/federation/qpidclient/QpidClientApplication.java
@@ -0,0 +1,13 @@
+package no.vegvesen.ixn.federation.qpidclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpidClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QpidClientApplication.class, args);
+    }
+
+}
