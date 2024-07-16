b576c1b7bc982a29c6123d807ec8ad41d9ad57ff
Johan <johan.hansen@bouvet.no>
2024-07-16 15:43:01 +0200

    Forgot depends_on. Things should work, only need to create json-file for key generation

--- demo/single-node/single-node.sh
+++ demo/single-node/single-node.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -eu
 
-mkdir -p ../keys
-cd ../../keys-generator
-docker build . -t keys
-cd -
-docker run --rm -v $PWD/../keys:/keys keys
-IMAGE_TAG=3743e41 docker-compose -f single-node.yml up --build
+IMAGE_TAG=1344c98 docker-compose -f single-node.yml up --build
 
