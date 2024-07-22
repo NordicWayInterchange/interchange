a00453c2b14106e8a994409db3f95e499101d681
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2024-07-22 12:46:32 +0200

    Fixed a few problems, and updated the version in demo

--- systemtest-scripts/systemtest.sh
+++ systemtest-scripts/systemtest.sh
@@ -5,10 +5,10 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 export BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
 
 echo Running system test on branch $BRANCH with tag $BRANCH_TAG
+export JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout)
 
-
-docker build ../onboard-rest-client -t onboard_rest_client --build-arg JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout)
-docker build ../napcore-rest-client -t napcore_rest_client --build-arg JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout)
-docker build ../jms-client-app -t jms_client --build-arg JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout)
+docker build ../onboard-rest-client -t onboard_rest_client --build-arg JAR_VERSION=$JAR_VERSION
+docker build ../napcore-rest-client -t napcore_rest_client --build-arg JAR_VERSION=$JAR_VERSION
+docker build ../jms-client-app -t jms_client --build-arg JAR_VERSION=$JAR_VERSION
 [ -f ../tmp/keys/a.bouvetinterchange.eu.p12 ] || ./systemtest-keys.sh
-docker-compose -f systemtest.yml build --build-arg JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout) && docker-compose -f systemtest.yml up
+docker-compose -f systemtest.yml build --build-arg JAR_VERSION=$JAR_VERSION && docker-compose -f systemtest.yml up
