9bb1705b521c5a42a295986a7629b13436a6631b
Johan <johan.hansen@bouvet.no>
2024-07-29 09:56:05 +0200

    Created admin-api module

---
+++ admin-api/src/main/java/no/vegvesen/ixn/federation/adminapi/AdminApiApplication.java
@@ -0,0 +1,13 @@
+package no.vegvesen.ixn.federation.adminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApiApplication.class, args);
+    }
+
+}
---
+++ admin-api/src/test/java/no/vegvesen/ixn/federation/adminapi/AdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.vegvesen.ixn.federation.adminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
