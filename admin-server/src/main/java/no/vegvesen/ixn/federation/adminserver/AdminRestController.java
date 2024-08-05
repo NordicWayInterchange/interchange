94b9bed625dd5ad4723c06b7300a57829357c704
Johan <johan.hansen@bouvet.no>
2024-08-05 10:32:38 +0200

    Helm chart update. Removed need for ssl-client-auth for testing.

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminRestController.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminRestController.java
@@ -10,7 +10,7 @@ public class AdminRestController {
     /* TODO
        Certservice needs to be created. How should admin users be authenticated?
      */
-    @RequestMapping(method = RequestMethod.GET, path = {"/test"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.GET, path = {"/admin/test"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public String test(String adminName) {
         return "Hello world";
     }
