bb454d0b0067c9c0344eb040bfb7a2aeda490a38
Johan <johan.hansen@bouvet.no>
2024-08-21 19:40:24 +0200

    Created new service-provider-client module

---
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/ServiceProviderClientApplication.java
@@ -0,0 +1,13 @@
+package no.vegvesen.ixn.federation.serviceproviderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderClientApplication.class, args);
+    }
+
+}
