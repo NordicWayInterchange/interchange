d99ea1bc458bc04e40a1d0dfeafbe097dbf3d93b
Johan <johan.hansen@bouvet.no>
2024-08-29 11:30:56 +0200

    Restructued, created tests for messages

--- service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/messages/Message.java
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/messages/Message.java
@@ -1,4 +1,4 @@
-package no.vegvesen.ixn.federation.serviceproviderclient.command.messages;
+package no.vegvesen.ixn.federation.serviceproviderclient.messages;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
