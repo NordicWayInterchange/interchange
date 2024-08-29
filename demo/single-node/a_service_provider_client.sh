c87cc65fd26bd6bec8756afa383f8ef97bb749c4
Johan <johan.hansen@bouvet.no>
2024-08-29 15:48:17 +0200

    Updated single-node project to work with new listen-command

--- demo/single-node/a_service_provider_client.sh
+++ demo/single-node/a_service_provider_client.sh
@@ -4,11 +4,11 @@ SERVICE_PROVIDER="king_olav.interchangedomain.com"
 CONTAINER=""
 URL=""
 
-if [ $1 == 'messages' ]; then
+if [ $1 == 'messages' ] && [ $2 != 'listen' ]; then
 CONTAINER="a_qpid"
-URL="amqps://a.interchangedomain.com"
+URL="amqps://a.qpid.interchangedomain.com"
 
-elif [ $1 == 'capabilities' ] || [ $1 == 'subscriptions' ] || [ $1 == 'deliveries' ] || [ $1 == 'privatechannels' ]; then
+else
 CONTAINER="a_onboard_server"
 URL="https://a.interchangedomain.com:8797/"
 fi
@@ -22,4 +22,5 @@ docker run \
   -v $PWD/../keys/a:/keys \
   -v $PWD:/work \
   --link ${CONTAINER}:a.interchangedomain.com \
+  --link a_qpid:a.qpid.interchangedomain.com \
   europe-west4-docker.pkg.dev/nw-shared-w3ml/nordic-way-interchange/service-provider-client:$(<version) -k /keys/${SERVICE_PROVIDER}.p12 -s password -t /keys/ca.interchangedomain.com.jks -w password $URL -u ${SERVICE_PROVIDER} "$@"
