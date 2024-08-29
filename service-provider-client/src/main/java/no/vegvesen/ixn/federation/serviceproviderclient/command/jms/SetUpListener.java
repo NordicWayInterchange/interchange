59d571f5a44b6d994901630abfbcc10de0537010
Johan <johan.hansen@bouvet.no>
2024-08-29 15:33:14 +0200

    Created pre-defined messages and capabilities for listener command.

--- service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/jms/SetUpListener.java
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/jms/SetUpListener.java
@@ -1,16 +1,15 @@
 package no.vegvesen.ixn.federation.serviceproviderclient.command.jms;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.jms.ExceptionListener;
 import no.vegvesen.ixn.Sink;
-import no.vegvesen.ixn.federation.api.v1_0.EndpointApi;
-import no.vegvesen.ixn.federation.api.v1_0.capability.CapabilityApi;
-import no.vegvesen.ixn.federation.api.v1_0.capability.DatexApplicationApi;
-import no.vegvesen.ixn.federation.api.v1_0.capability.DenmApplicationApi;
-import no.vegvesen.ixn.federation.api.v1_0.capability.MetadataApi;
+import no.vegvesen.ixn.federation.api.v1_0.capability.*;
 import no.vegvesen.ixn.federation.serviceproviderclient.ServiceProviderClient;
 import no.vegvesen.ixn.serviceprovider.model.*;
 import picocli.CommandLine;
 
+import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -24,23 +23,36 @@ public class SetUpListener implements Callable<Integer> {
     @CommandLine.ParentCommand
     MessagesCommand parentCommand;
 
+    @CommandLine.Option(names = {"-f", "--file"}, description = "")
+    File file;
+
     private final CountDownLatch counter = new CountDownLatch(1);
 
     @Override
     public Integer call() throws Exception {
         ServiceProviderClient client = parentCommand.getParent().createClient();
 
-        AddCapabilitiesRequest capabilitiesRequest = new AddCapabilitiesRequest("king_olav.bouvetinterchange.eu", Set.of(new CapabilityApi(
-                new DenmApplicationApi("NO00002", "NO002-pub-1222", "NO", "DENM:2.3", List.of("12"), List.of(6)),
-                new MetadataApi()
-        )));
-        client.addCapability(capabilitiesRequest);
+        AddCapabilitiesRequest capability;
+        String consumerCommonName;
+        if(file != null){
+            ObjectMapper mapper = new ObjectMapper();
+            capability = mapper.readValue(file, AddCapabilitiesRequest.class);
+            consumerCommonName = capability.getName();
+        }
+        else{
+            capability = getCapabilitiesRequest();
+            consumerCommonName = "king_olav.bouvetinterchange.eu";
+        }
 
-        AddDeliveriesRequest deliveriesRequest = new AddDeliveriesRequest("king_olav.bouvetinterchange.eu", Set.of(new SelectorApi("originatingCountry='NO'")));
+        client.addCapability(capability);
+        String selector = generateSelector(capability);
+
+        AddDeliveriesRequest deliveriesRequest = new AddDeliveriesRequest(consumerCommonName, Set.of(new SelectorApi(selector)));
         client.addServiceProviderDeliveries(deliveriesRequest);
 
-        AddSubscriptionsRequest subscriptionsRequest = new AddSubscriptionsRequest("king_olav.bouvetinterchange.eu", Set.of(new AddSubscription("originatingCountry='NO'")));
+        AddSubscriptionsRequest subscriptionsRequest = new AddSubscriptionsRequest(consumerCommonName, Set.of(new AddSubscription(selector)));
         client.addSubscription(subscriptionsRequest);
+
         ListSubscriptionsResponse listSubscriptionsResponse = client.getServiceProviderSubscriptions();
         LocalActorSubscription subscription = listSubscriptionsResponse.getSubscriptions().stream().findFirst().get();
 
@@ -49,6 +61,7 @@ public class SetUpListener implements Callable<Integer> {
         }
         LocalEndpointApi endpointApi = client.getSubscription(subscription.getId()).getEndpoints().stream().findFirst().get();
         String url = "amqps://"+endpointApi.getHost();
+
         System.out.printf("Listening for messages from queue [%s] on server [%s]%n", endpointApi.getHost(), parentCommand.getUrl());
         AtomicInteger returnCode = new AtomicInteger(0);
         ExceptionListener exceptionListener = e -> {
@@ -68,4 +81,47 @@ public class SetUpListener implements Callable<Integer> {
         }
         return 0;
     }
+
+    public AddCapabilitiesRequest getCapabilitiesRequest() {
+        AddCapabilitiesRequest capabilitiesRequest = new AddCapabilitiesRequest();
+        String publisherId = "bouvet";
+        String originatingCountry="NO";
+        List<String> quadTree = List.of("12");
+        List<String> messageTypes = List.of("DATEX2", "DENM", "CAM", "SSEM", "SREM", "IVIM", "MAPEM", "SPATEM");
+        for(int i = 0; i < 8; i++){
+            CapabilityApi capability = new CapabilityApi();
+            if(messageTypes.get(i).equals("DATEX2")){
+                capability.setApplication(
+                        new DatexApplicationApi(publisherId, publisherId + ":" +i, originatingCountry, messageTypes.get(i)+":1", quadTree, "warning", "bouvet")
+                );
+            }
+            else if(messageTypes.get(i).equals("DENM")){
+                capability.setApplication(
+                        new DenmApplicationApi(publisherId, publisherId + ":" + i, originatingCountry, messageTypes.get(i)+":1", quadTree, List.of(6))
+                );
+            }
+            else{
+                capability.setApplication(
+                        new ApplicationApi(messageTypes.get(i), publisherId, publisherId+":"+i, originatingCountry, messageTypes.get(i)+":1", quadTree));
+
+            }
+            capability.setMetadata(new MetadataApi());
+            capabilitiesRequest.getCapabilities().add(capability);
+        }
+        return capabilitiesRequest;
+    }
+
+    public String generateSelector(AddCapabilitiesRequest capability) {
+        StringBuilder sb = new StringBuilder();
+        Iterator<CapabilityApi> capabilityApiIterator = capability.getCapabilities().iterator();
+        while(capabilityApiIterator.hasNext()){
+            CapabilityApi capabilityApi = capabilityApiIterator.next();
+            sb.append("(publicationId='").append(capabilityApi.getApplication().getPublicationId()).append("') ");
+            if(capabilityApiIterator.hasNext()){
+                sb.append("OR ");
+            }
+        }
+        return sb.toString();
+    }
 }
+
