738866342210d0117605bc98cab4aae809f0b00e
Johan <johan.hansen@bouvet.no>
2024-09-11 14:40:06 +0200

    Fixed things after merge

--- admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/AdminRestControllerIT.java
+++ admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/AdminRestControllerIT.java
@@ -1,12 +1,8 @@
 package no.vegvesen.ixn.federation.adminserver;
 
-import jakarta.transaction.Transactional;
-import no.vegvesen.ixn.postgresinit.PostgresTestcontainerInitializer;
+import no.vegvesen.ixn.docker.PostgresContainerBase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(initializers = {PostgresTestcontainerInitializer.Initializer.class})
-@Transactional
-public class AdminRestControllerIT {
+public class AdminRestControllerIT extends PostgresContainerBase {
 }
