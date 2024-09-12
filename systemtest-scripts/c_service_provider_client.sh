f9a885b0bd43f1006f031c3ae1da89b1b8c1367d
Johan <johan.hansen@bouvet.no>
2024-09-12 11:16:46 +0200

    Updated new-systemtest.yml to work

--- systemtest-scripts/c_service_provider_client.sh
+++ systemtest-scripts/c_service_provider_client.sh
@@ -10,15 +10,14 @@ else
 URL="https://c.bouvetinterchange.eu:8595/"
 fi
 
-
 docker run \
   -it \
   --rm \
-  --name b_service_provider_client \
+  --name c_service_provider_client${RANDOM} \
   --network=systemtest-scripts_testing_net \
   --dns=172.28.1.1 \
   -v $PWD/../tmp/keys:/keys \
   -v $PWD:/work \
   --link c_onboard_server:c.bouvetinterchange.eu \
-  --link c_qpid:c.qpid.bouvetinterchange.eu
+  --link c_qpid:c.qpid.bouvetinterchange.eu \
   service_provider_client -k /keys/${SERVICE_PROVIDER}.p12 -s password -t /keys/ca.bouvetinterchange.eu.jks  -w password $URL -u ${SERVICE_PROVIDER} "$@"
