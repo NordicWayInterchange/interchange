1d7c14e74646581a2f6ec00f9a085a5fab2c6c6b
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-11-27 10:13:09 +0100

    set up project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      HELLO
+    </div>
+  );
+}
