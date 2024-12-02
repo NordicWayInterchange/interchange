baa90a1f622acd73cf689ac1a94c7b453af7a0ce
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-12-02 10:42:30 +0100

    Added signin page

---
+++ src/components/styles/StyledElements.ts
@@ -0,0 +1,23 @@
+import {styled} from "@mui/system";
+import {Button} from "@mui/material";
+
+export const StyledButton = styled(Button)(({}) => ({
+    textTransform: 'none',
+    marginLeft: 2,
+    height: 40,
+    width: '200px',
+    borderColor: 'buttonThemeColor',
+    paddingBottom: '10px',
+    position: 'relative',
+    '&:hover': {
+        '&::after': {
+            content: '""',
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            height: '3px',
+            backgroundColor: '#FF9600',
+        },
+    },
+}));
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+import logger from "../../../lib/logger";
+
+export const authOptions = {
+    /**
+     * @Description Providers client id/secret
+     */
+    providers: [
+        Auth0Provider({
+            clientId: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            issuer: process.env.AUTH0_ISSUER,
+            authorization: {
+                params: {
+                    prompt: "login",
+                },
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/login",
+    },
+    session: {
+        maxAge: 1, // Session expires after 1 second
+        updateAge: 0, // Session is not refreshed (expires immediately)
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async session({ session, token }) {
+            session.user.commonName = process.env.INTERCHANGE_PREFIX + token.email;
+            return session;
+        },
+    },
+    logger: {
+        warn: (message) => console.warn(message),
+        debug: (message) => console.debug(message),
+        error: (message, error) => console.error(message, error),
+    },
+    events: {
+        async signIn(message) {
+            const { email, name } = message.user;
+            const { provider, type } = message.account;
+
+            logger.child({ provider, type, name, email }).info("User signed in");
+        },
+        async signOut(message) {
+            const { email, name } = message.token;
+
+            logger.child({ name, email }).info("User signed out");
+        },
+    },
+};
+export default NextAuth(authOptions);
