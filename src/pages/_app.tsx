afa1e4f1ae71325efb9e9b70f71fe6ca6aba7d84
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-12-02 09:16:37 +0100

    Added winston logger

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+
+export default function App({ Component, pageProps }: AppProps) {
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
+}
