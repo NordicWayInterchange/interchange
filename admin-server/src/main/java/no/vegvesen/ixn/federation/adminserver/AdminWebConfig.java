2fa208ea541ff2de2597a0840fcdff56a36a8bb3
Johan <johan.hansen@bouvet.no>
2024-12-04 09:23:30 +0100

    Temporary changes for local development of admin-frontend. Changed CORS-policy.

---
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminWebConfig.java
@@ -0,0 +1,16 @@
+package no.vegvesen.ixn.federation.adminserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class AdminWebConfig implements WebMvcConfigurer {
+
+     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+         registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+     }
+}
