b525e781b068e89f2345b95e06c4a11593f90667
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-12-17 15:39:57 +0100

    Fixed timeconverter test

--- __tests___/timeConverter.test.ts
+++ __tests___/timeConverter.test.ts
@@ -3,8 +3,11 @@ describe("Can correctly convert Unix Epoch", () => {
   const epoch = 1732619387;
   const englishDateFormat = "26 Nov 2024 12:09:47";
 
-  it("can convert to correct english date and time format", () => {
-    jest.spyOn(navigator, 'language', 'get').mockReturnValue('en-GB');
+  it("can convert to correct English date and time format", () => {
+    Object.defineProperty(navigator, 'language', {
+      get: jest.fn().mockReturnValue('en-GB'),
+    });
+
     expect(timeConverter(epoch)).toBe(englishDateFormat);
   });
 
