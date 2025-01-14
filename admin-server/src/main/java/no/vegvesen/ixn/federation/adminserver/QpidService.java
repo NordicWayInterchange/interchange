77daa11a51b89f9ee80cecba27536eadec46e1da
Johan <johan.hansen@bouvet.no>
2025-01-14 13:33:39 +0100

    Created endpoints - exchange/queue/binding exists

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/QpidService.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/QpidService.java
@@ -1,8 +1,13 @@
 package no.vegvesen.ixn.federation.adminserver;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import no.vegvesen.ixn.federation.qpid.Exchange;
 import no.vegvesen.ixn.federation.qpid.QpidClient;
+import no.vegvesen.ixn.federation.qpid.QpidDelta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QpidService {
@@ -20,5 +25,14 @@ public class QpidService {
 
     public boolean queueExists(String queueName){
         return qpidClient.queueExists(queueName);
+    }
+
+    public List<Exchange> exchangeList() throws JsonProcessingException {
+        return qpidClient.getAllExchanges();
+    }
+
+    public boolean bindingExists(String exchangeName, String queueName){
+        QpidDelta delta = qpidClient.getQpidDelta();
+        return delta.exchangeHasBindingToQueue(exchangeName, queueName);
     }
 }
