3d4a607471de9e32c783a0c3f89cd10c79487fb3
Johan <johan.hansen@bouvet.no>
2025-01-17 13:42:37 +0100

    Revert "Restructured packages, makes more sense to have the testssl as a subpackage of adminserver"
    This reverts commit 4d14fa92a0b03e5774bc40e7ea8de079be2485c3.

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/QpidService.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/QpidService.java
@@ -1,9 +1,13 @@
 package no.vegvesen.ixn.federation.adminserver;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import no.vegvesen.ixn.federation.qpid.Exchange;
 import no.vegvesen.ixn.federation.qpid.QpidClient;
 import no.vegvesen.ixn.federation.qpid.QpidDelta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QpidService {
@@ -21,6 +25,10 @@ public class QpidService {
 
     public boolean queueExists(String queueName){
         return qpidClient.queueExists(queueName);
+    }
+
+    public List<Exchange> exchangeList() throws JsonProcessingException {
+        return qpidClient.getAllExchanges();
     }
 
     public boolean bindingExists(String exchangeName, String queueName){
