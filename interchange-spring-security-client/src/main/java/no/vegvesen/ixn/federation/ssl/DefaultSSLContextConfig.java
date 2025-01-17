ece4659a322b43ed1d2dc65a7a5f1bd37ac436bf
Johan <johan.hansen@bouvet.no>
2025-01-17 13:45:04 +0100

    Fixed failing tests

--- interchange-spring-security-client/src/main/java/no/vegvesen/ixn/federation/ssl/DefaultSSLContextConfig.java
+++ interchange-spring-security-client/src/main/java/no/vegvesen/ixn/federation/ssl/DefaultSSLContextConfig.java
@@ -2,7 +2,6 @@ package no.vegvesen.ixn.federation.ssl;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.net.ssl.SSLContext;
 import java.security.NoSuchAlgorithmException;
@@ -11,7 +10,6 @@ import java.security.NoSuchAlgorithmException;
 public class DefaultSSLContextConfig {
 
 	@Bean
-	@Primary
 	public SSLContext defaultSslContext() throws NoSuchAlgorithmException {
 		return SSLContext.getDefault();
 	}
