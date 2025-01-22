fc02ebe7b08eb37c7d25d6e7277e7b1b3251eaca
Johan <johan.hansen@bouvet.no>
2025-01-22 14:29:13 +0100

    Removed debugging statements

--- interchange-spring-security/src/main/java/no/vegvesen/ixn/federation/auth/CertService.java
+++ interchange-spring-security/src/main/java/no/vegvesen/ixn/federation/auth/CertService.java
@@ -26,6 +26,7 @@ public class CertService{
 	public void checkIfCommonNameMatchesNapName(String napName) {
 		Authentication principal = SecurityContextHolder.getContext().getAuthentication();
 		String commonName = principal.getName();
+
 		if (!commonName.equals(napName)) {
 			logger.error("Received request from {}, but CN on certificate did not match the NAP. Rejecting...", commonName);
 			String errorMessage = "Received request from %s, but CN on certificate did not match the NAP. Rejecting...";
