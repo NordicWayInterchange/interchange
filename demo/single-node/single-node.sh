8d3600a14ca9783d06b864e28028693a58234a82
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2025-01-29 15:22:44 +0100

    A couple of fixes to make the client a bit more robust, dynamic update of version, and dump version in file

--- demo/single-node/single-node.sh
+++ demo/single-node/single-node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
-
+if [ "$(git rev-parse --abbrev-ref HEAD)" == "federation-master" ]; then
+        git rev-parse --short=7 HEAD > version
+fi
 [ -d ../keys/a/ ] || ./single-node-keys.sh
 IMAGE_TAG=$(<version) docker-compose -f single-node.yml up --build
 
