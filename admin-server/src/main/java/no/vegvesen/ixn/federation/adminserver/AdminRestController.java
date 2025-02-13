d6ba589e5e31132c1c49e8ff0b955ff4305d9744
Maryam <maryam.mehrabian@bouvet.no>
2025-02-13 23:59:32 +0100

    Added JsonProcessingException catch

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminRestController.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminRestController.java
@@ -69,10 +69,15 @@ public class AdminRestController {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/admin/{adminUser}/exchanges")
-    public List<ExchangeApi> getExchanges(@PathVariable("adminUser") String adminUser) throws JsonProcessingException {
+    public List<ExchangeApi> getExchanges(@PathVariable("adminUser") String adminUser) {
         this.certService.checkIfCommonNameMatchesNameInApiObject(adminProperties.getName());
         logger.info("Log - exchange exists - requesting user {}", adminUser);
-        List<Exchange> exchangesList = qpidClient.getAllExchanges();
+        List<Exchange> exchangesList;
+        try {
+            exchangesList = qpidClient.getAllExchanges();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return typeTransformer.exchangeListToExchangeApiList(exchangesList);
     }
 
