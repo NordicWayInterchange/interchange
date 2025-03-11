307d9bbb6804b8b9f19e60abb3926605e3f573de
Maryam <maryam.mehrabian@bouvet.no>
2025-03-11 11:26:05 +0100

    throwing RuntimeException

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/exceptions/HandleJsonProcessingException.java
+++
@@ -1,8 +0,0 @@
-package no.vegvesen.ixn.federation.adminserver.exceptions;
-
-public class HandleJsonProcessingException extends RuntimeException{
-    public HandleJsonProcessingException(String message, Exception e){
-        super(message, e);
-    }
-
-}
