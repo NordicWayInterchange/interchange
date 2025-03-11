e97fda9ad1c31e0e03be74ef32c272a625f10336
Johan <johan.hansen@bouvet.no>
2025-03-11 09:37:42 +0100

    Fixed bindingExists method

--- admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/QpidServiceIT.java
+++ admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/QpidServiceIT.java
@@ -106,6 +106,30 @@ public class QpidServiceIT extends QpidDockerBaseIT {
     }
 
     @Test
+    public void testBindingExistsReturnsFalse(){
+        String queueName = "bi-queue";
+        String exchangeName = "my-exchange";
+
+        client.createHeadersExchange(exchangeName);
+
+        Capability capability = new Capability(
+                new DenmApplication(
+                        "NO-123",
+                        "pub-1",
+                        "NO",
+                        "1.0",
+                        List.of("12", "13"),
+                        List.of(5, 6)
+                ),
+                new Metadata()
+        );
+        String selector = "originatingCountry='DK'";
+
+        client.addBinding(exchangeName, new Binding(exchangeName, queueName, new Filter(selector)));
+        assertThat(service.bindingExists(exchangeName, queueName)).isFalse();
+    }
+
+    @Test
     public void testGetExchanges() {
         Exchange exchange = client.createDirectExchange("test-exchange");
         assertThat(exchange.getName()).isEqualTo("test-exchange");
