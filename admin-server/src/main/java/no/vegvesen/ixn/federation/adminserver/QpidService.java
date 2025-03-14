a486cf9210c5edc023d8b0d71b622a49087a7062
Maryam <maryam.mehrabian@bouvet.no>
2025-03-14 10:35:23 +0100

    Added if delivery's exchange binds To matching capability

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/QpidService.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/QpidService.java
@@ -3,7 +3,12 @@ package no.vegvesen.ixn.federation.adminserver;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import no.vegvesen.ixn.federation.adminserver.qpid.Exchange;
 import no.vegvesen.ixn.federation.adminserver.qpid.AdminQpidClient;
+import no.vegvesen.ixn.federation.adminserver.qpid.QpidDelta;
 import no.vegvesen.ixn.federation.adminserver.qpid.Queue;
+import no.vegvesen.ixn.federation.model.*;
+import no.vegvesen.ixn.federation.model.capability.Capability;
+import no.vegvesen.ixn.federation.model.capability.CapabilityShard;
+import no.vegvesen.ixn.federation.repository.OutgoingMatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +17,15 @@ import java.util.List;
 @Service
 public class QpidService {
 
+
+    private final OutgoingMatchRepository outgoingMatchRepository;
+
     private final AdminQpidClient adminQpidClient;
 
     @Autowired
-    public QpidService(AdminQpidClient adminQpidClient) {
+    public QpidService(AdminQpidClient adminQpidClient, OutgoingMatchRepository outgoingMatchRepository) {
         this.adminQpidClient = adminQpidClient;
+        this.outgoingMatchRepository = outgoingMatchRepository;
     }
 
     public boolean exchangeExists(String exchangeName) {
@@ -34,6 +43,31 @@ public class QpidService {
         } else {
             return false;
         }
+    }
+
+    public boolean deliverysExchangeBindingToMatchingCapabilityExists(List<ServiceProvider> serviceProviderList) {
+        QpidDelta delta = qpidClient.getQpidDelta();
+            for (ServiceProvider serviceProvider : serviceProviderList) {
+                if (serviceProvider.hasDeliveries()) {
+                    for (LocalDelivery delivery : serviceProvider.getDeliveries()) {
+                        if (delivery.getStatus().equals(LocalDeliveryStatus.CREATED)) {
+                            List<OutgoingMatch> matches = outgoingMatchRepository.findAllByLocalDelivery_Id(delivery.getId());
+
+                            for (OutgoingMatch match : matches) {
+                                Capability capability = match.getCapability();
+                                for (LocalDeliveryEndpoint endpoint : delivery.getEndpoints()) {
+                                    for (CapabilityShard shard : capability.getShards()) {
+                                        if (delta.exchangeHasBindingToQueue(endpoint.getTarget(), shard.getExchangeName())) {
+                                            return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+        }
+        return false;
     }
 
     public List<Exchange> getAllExchanges() {
