37b0495a49f95e931ef8b880768a0541adc5cbae
Maryam <maryam.mehrabian@bouvet.no>
2025-03-26 13:12:42 +0100

    Changed helm version

--- import-export-service/src/main/java/no/vegvesen/ixn/federation/service/ImportExportApplication.java
+++ import-export-service/src/main/java/no/vegvesen/ixn/federation/service/ImportExportApplication.java
@@ -52,5 +52,12 @@ public class ImportExportApplication implements CommandLineRunner {
             );
             exportApplication.run();
         }
+        /*ImportApplication importApplication = new ImportApplication(
+                neighbourRepository,
+                serviceProviderRepository,
+                privateChannelRepository
+        );
+        importApplication.run(Paths.get("dump.json"));
+    }*/
     }
 }
