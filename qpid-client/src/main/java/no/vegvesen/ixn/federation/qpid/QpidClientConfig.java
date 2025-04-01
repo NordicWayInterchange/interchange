17004d97305e8f4435da1f25f1721ccba4c89b64
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2025-04-01 14:03:48 +0200

    Skipping host name verification for now.

--- qpid-client/src/main/java/no/vegvesen/ixn/federation/qpid/QpidClientConfig.java
+++ qpid-client/src/main/java/no/vegvesen/ixn/federation/qpid/QpidClientConfig.java
@@ -31,6 +31,7 @@ public class QpidClientConfig {
 		SSLConnectionSocketFactory sslConnectionSocketFactory = SSLConnectionSocketFactoryBuilder
 				.create()
 				.setSslContext(sslContext)
+				.setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
 				.build();
 		PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder
 				.create()
