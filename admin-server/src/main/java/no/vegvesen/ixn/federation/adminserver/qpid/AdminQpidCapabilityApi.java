ccb8a2cc8a4150fee463503fd091e1ea6520ee25
Maryam <maryam.mehrabian@bouvet.no>
2025-04-14 14:12:12 +0200

    Created AdminQpidCapabilityApi

---
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/qpid/AdminQpidCapabilityApi.java
@@ -0,0 +1,36 @@
+package no.vegvesen.ixn.federation.adminserver.qpid;
+
+import no.vegvesen.ixn.federation.adminserver.model.serviceProvider.CapabilityShardApi;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AdminQpidCapabilityApi {
+
+
+    public AdminQpidCapabilityApi() {
+    }
+
+    private Set<CapabilityShardApi> shards = new HashSet<>();
+
+    public AdminQpidCapabilityApi(Set<CapabilityShardApi> shards) {
+        this.shards = shards;
+    }
+
+    public Set<CapabilityShardApi> getShards() {
+        return shards;
+    }
+
+    public void setShards(Set<CapabilityShardApi> shards) {
+        this.shards.clear();
+        if (shards != null) {
+            this.shards.addAll(shards);
+        }
+    }
+
+    public String toString() {
+        return "QpidCapabilityApi{" +
+                ", shards=" + shards +
+                '}';
+    }
+}
