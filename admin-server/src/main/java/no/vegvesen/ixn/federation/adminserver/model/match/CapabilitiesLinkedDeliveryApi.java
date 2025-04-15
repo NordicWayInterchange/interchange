e56eaf909ce30754fd85f21b8d201b71ec4f4e8d
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2025-04-15 14:22:15 +0200

    Changed the domain model of getting bindings/matches between deliveries and capabilities.

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/model/match/CapabilitiesLinkedDeliveryApi.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/model/match/CapabilitiesLinkedDeliveryApi.java
@@ -1,37 +1,5 @@
 package no.vegvesen.ixn.federation.adminserver.model.match;
 
-public class CapabilitiesLinkedDeliveryApi {
+import java.util.List;
 
-    String deliveryId;
-
-    CapabilityMatchApi capabilityMatchApi;
-
-
-    public CapabilitiesLinkedDeliveryApi(String deliveryId, CapabilityMatchApi capabilityMatchApi) {
-        this.deliveryId = deliveryId;
-        this.capabilityMatchApi = capabilityMatchApi;
-    }
-
-    public void setDeliveryId(String id) {
-        this.deliveryId = deliveryId;
-    }
-
-    public String getDeliveryId() {
-        return deliveryId;
-    }
-
-    public void setCapabilityMatch(CapabilityMatchApi capabilityMatchApi) {
-        this.capabilityMatchApi = capabilityMatchApi;
-    }
-
-    public CapabilityMatchApi getCapabilityMatch() {
-        return capabilityMatchApi;
-    }
-
-    public String toString() {
-        return "CapabilitiesLinkedDeliveryApi{" +
-                "deliveryId=" + deliveryId +
-                ", capabilityMatchApi=" + capabilityMatchApi +
-                '}';
-    }
-}
+public record CapabilitiesLinkedDeliveryApi(String deliveryId, List<CapabilityMatchApi> capabilityMatchApi) { }
--- admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/AdminRestControllerIT.java
+++ admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/AdminRestControllerIT.java
@@ -17,6 +17,7 @@ import no.vegvesen.ixn.federation.model.capability.*;
 import no.vegvesen.ixn.federation.repository.NeighbourRepository;
 import no.vegvesen.ixn.federation.repository.PrivateChannelRepository;
 import no.vegvesen.ixn.federation.repository.OutgoingMatchRepository;
+import no.vegvesen.ixn.federation.repository.OutgoingMatchRepository;
 import no.vegvesen.ixn.federation.repository.ServiceProviderRepository;
 import no.vegvesen.ixn.serviceprovider.NotFoundException;
 import org.assertj.core.api.AssertionsForClassTypes;
@@ -51,6 +52,9 @@ public class AdminRestControllerIT extends PostgresContainerBase {
 
     @Autowired
     PrivateChannelRepository privateChannelRepository;
+
+    @Autowired
+    OutgoingMatchRepository outgoingMatchRepository;
 
     @Autowired
     AdminRestController restController;
@@ -439,23 +443,17 @@ public class AdminRestControllerIT extends PostgresContainerBase {
     }*/
 
     @Test
-    public void testGetDeliverysExchangeBindingToMatchingCapabilities() {
+    public void testGetDeliveriesExchangeBindingToMatchingCapabilities() {
         String serviceProviderName = "my-service-provider";
         String adminUser = "adminUser";
+        CapabilityShard shard = new CapabilityShard(1, "cap-ex3", "publicationId = 'pub-1'");
         Capability aCap1 = new Capability(
+                "uuid",
                 new DatexApplication("DK12345","DK12345:publication-id","NO","1", List.of("1"), "type","name"),
-                new Metadata()
+                new Metadata(),
+                List.of(shard)
         );
 
-        CapabilityShard shard = new CapabilityShard(1, "cap-ex3", "publicationId = 'pub-1'");
-        aCap1.setShards(Collections.singletonList(shard));
-
-        LocalDelivery aDelivery = new LocalDelivery();
-
-        ServiceProvider aServiceProvider = new ServiceProvider(serviceProviderName);
-        serviceProviderRepository.save(aServiceProvider);
-
-        List<CapabilitiesLinkedDeliveryApi> capabilitiesLinkedDeliveryApiList = new ArrayList<>();
 
         CapabilityMatchApi capabilityMatchApi = new CapabilityMatchApi(
                 aCap1.getUuid(),
@@ -463,9 +461,30 @@ public class AdminRestControllerIT extends PostgresContainerBase {
                 new Binding("exchange", "queueName", new Filter("publicationId = 'pub-1'")),
                 true
         );
+        LocalDelivery aDelivery = new LocalDelivery();
 
-        capabilitiesLinkedDeliveryApiList.add(new CapabilitiesLinkedDeliveryApi(aDelivery.getUuid(), capabilityMatchApi));
-        when(qpidService.getCapabilitiesLinkedDelivery(aServiceProvider, aDelivery.getUuid())).thenReturn(capabilitiesLinkedDeliveryApiList);
-        assertThat(restController.getDeliverysExchangeBindingToMatchingCapabilities(adminUser, serviceProviderName, aDelivery.getUuid())).isNotEmpty();
+        ServiceProvider aServiceProvider = new ServiceProvider(
+                serviceProviderName,
+                new Capabilities(Set.of(aCap1)),
+                Set.of(),
+                Set.of(aDelivery),
+                LocalDateTime.now()
+        );
+        serviceProviderRepository.save(aServiceProvider);
+        OutgoingMatch match = new OutgoingMatch(aDelivery,aCap1,serviceProviderName);
+        outgoingMatchRepository.save(match);
+
+        CapabilitiesLinkedDeliveryApi result = new CapabilitiesLinkedDeliveryApi(aDelivery.getUuid(), List.of(capabilityMatchApi));
+        when(qpidService.getCapabilitiesLinkedDelivery(aDelivery, List.of(match))).thenReturn(result);
+        assertThat(
+                restController
+                        .getDeliveriesExchangeBindingToMatchingCapabilities(
+                                adminUser,
+                                serviceProviderName,
+                                aDelivery.getUuid()
+                        )
+                        .capabilityMatchApi()
+        )
+                .isNotEmpty();
     }
 }
