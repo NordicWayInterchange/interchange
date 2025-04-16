fd28616d1cb81316bdab3f748c0d3f659b7a69ad
Maryam <maryam.mehrabian@bouvet.no>
2025-04-16 20:49:40 +0200

    Shard count more than 1

--- admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/AdminRestControllerIT.java
+++ admin-server/src/test/java/no/vegvesen/ixn/federation/adminserver/AdminRestControllerIT.java
@@ -3,7 +3,6 @@ package no.vegvesen.ixn.federation.adminserver;
 import no.vegvesen.ixn.docker.PostgresContainerBase;
 import no.vegvesen.ixn.federation.adminserver.model.match.CapabilitiesLinkedDeliveryApi;
 import no.vegvesen.ixn.federation.adminserver.model.match.CapabilityMatchApi;
-import no.vegvesen.ixn.federation.adminserver.model.serviceProvider.LocalDeliveryApi;
 import no.vegvesen.ixn.federation.adminserver.model.serviceProvider.LocalDeliveryIdApi;
 import no.vegvesen.ixn.federation.adminserver.model.serviceProvider.MatchingCapabilityApi;
 import no.vegvesen.ixn.federation.adminserver.qpid.*;
@@ -165,7 +164,7 @@ public class AdminRestControllerIT extends PostgresContainerBase {
                                 new NeighbourCapability(
                                         new DatexApplication("NO12345", "NO12345:dk21o2", "NO", "DATEX2:1.2", List.of("1"),
                                                 "situationPublication", "bouvet"),
-                                        new Metadata("https://www.bouvet.no", 1, RedirectStatus.OPTIONAL, 0, 0, 5)
+                                        new Metadata("https://www.bouvet.no", 2, RedirectStatus.OPTIONAL, 0, 0, 5)
                                 )
                         )),
                 new NeighbourSubscriptionRequest(Set.of(
@@ -272,7 +271,7 @@ public class AdminRestControllerIT extends PostgresContainerBase {
     }
 
     @Test
-    public void testGetDeliveriesExchangeBindingToMatchingCapabilities() {
+    public void testGetDeliveriessExchangeBindingToMatchingCapabilities() {
         String serviceProviderName = "my-service-provider";
         String adminUser = "adminUser";
         CapabilityShard shard = new CapabilityShard(1, "cap-ex3", "publicationId = 'pub-1'");
