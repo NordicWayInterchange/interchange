c185bb8777c8878361c5b3b48eb303a56996fdcf
Maryam <maryam.mehrabian@bouvet.no>
2025-04-22 10:52:05 +0200

    Refactored

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminRestController.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/AdminRestController.java
@@ -245,7 +245,7 @@ public class AdminRestController {
             throw new NotFoundException("No match found for capability with" + capabilityId + " and delivery with id " + deliveryId);
         }
 
-        return qpidService.capabilitiesMatchedDeliveryBasedOnCapabilityId(matchedByCapabilityAndDelivery);
+        return typeTransformer.capabilitiesMatchedDeliveryBasedOnCapabilityId(matchedByCapabilityAndDelivery);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/admin/{adminUser}/serviceproviders/{actorCommonName}/deliveries/{deliveryId}/matches/{capabilityId}/{shardId}")
@@ -265,7 +265,7 @@ public class AdminRestController {
             throw new NotFoundException("No match found for capability with" + capabilityId + " and delivery with id " + deliveryId);
         }
 
-        return qpidService.capabilitiesMatchedDeliveryBasedOnShardId(delivery, matchedByCapabilityAndDelivery, shardId);
+        return typeTransformer.capabilitiesMatchedDeliveryBasedOnShardId(delivery, matchedByCapabilityAndDelivery, shardId);
     }
 
     private ServiceProvider serviceProviderExists(String actorCommonName) {
