f8c3e539547c6991a7266866265d3ab32a285ffa
Johan <johan.hansen@bouvet.no>
2025-05-13 10:54:18 +0200

    Corrected example

--- service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/deliveries/Send.java
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/deliveries/Send.java
@@ -27,7 +27,7 @@ import static no.vegvesen.ixn.federation.api.v1_0.Constants.*;
         version = "1.0",
         customSynopsis = {
         """
-                Example: \n
+                Examples: \n
                 serviceproviderclient deliveries send -m message.json -s "originatingCountry='NO'" \n
                 serviceproviderclient deliveries send -m message.json -i 5d16cb60-0534-4469-b525-f92a5953322c \n
                 serviceproviderclient deliveries send -m message.json -f denm_delivery.json \n
