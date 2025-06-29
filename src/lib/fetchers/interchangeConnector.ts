5944277126a61b16f46aab7e3f70cde27f71cece
Maryam <maryam.mehrabian@bouvet.no>
2025-06-29 23:32:56 +0200

    Added privatechannels/peer hook

--- src/lib/fetchers/interchangeConnector.ts
+++ src/lib/fetchers/interchangeConnector.ts
@@ -71,6 +71,11 @@ export const fetchAdminUIPrivateChannelsPeer: extendedGetFunction = async (param
     return await fetchIXN(adminUser, `/serviceproviders/${serviceProviderName}/privatechannels/peer`);
 };
 
+export const fetchAdminUIPrivateChannelsPeers: extendedGetFunction = async (params) => {
+    const { actorCommonName, serviceProviderName} = params;
+    return await fetchIXN(actorCommonName, `/serviceproviders/${serviceProviderName}/privatechannels/peer`);
+};
+
 export const fetchAdminUIDeliveryIds: extendedGetFunction = async (params) => {
     const { adminUser, serviceProviderName} = params;
     return await fetchIXN(adminUser, `/serviceproviders/${serviceProviderName}/deliveries`);
