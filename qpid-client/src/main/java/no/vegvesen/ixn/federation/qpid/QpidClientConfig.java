6286e08ee2d1bb87fbbfe34dc27af40104495ab7
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2025-07-01 14:19:07 +0200

    Have to use noop hostname verifier, as the pod responds to a different name internally

--- qpid-client/src/main/java/no/vegvesen/ixn/federation/qpid/QpidClientConfig.java
+++ qpid-client/src/main/java/no/vegvesen/ixn/federation/qpid/QpidClientConfig.java
@@ -5,6 +5,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,7 @@ public class QpidClientConfig {
 		SSLConnectionSocketFactory sslConnectionSocketFactory = SSLConnectionSocketFactoryBuilder
 				.create()
 				.setSslContext(sslContext)
+				.setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
 				.build();
 		PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder
 				.create()
