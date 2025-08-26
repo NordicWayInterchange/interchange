09759f306b55b4596f38ba6f5f2326d9490946eb
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2025-08-26 12:36:08 +0200

    More testing, removed a constructor that was unused

--- qpid-client/src/test/java/no/vegvesen/ixn/federation/qpid/DlqExchangeIT.java
+++ qpid-client/src/test/java/no/vegvesen/ixn/federation/qpid/DlqExchangeIT.java
@@ -11,14 +11,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.net.ssl.SSLContext;
 import java.nio.file.Path;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class DlqExchangeTest extends QpidDockerBaseIT {
+public class DlqExchangeIT extends QpidDockerBaseIT {
 
     public static final String HOST_NAME = getDockerHost();
-    private static final ClusterKeyGenerator.CaStores stores = generateStores(getTargetFolderPathForTestClass(no.vegvesen.ixn.federation.BiQpidStructureIT.class), "my_ca", HOST_NAME, "routing_configurer", "king_gustaf");
+    private static final ClusterKeyGenerator.CaStores stores = generateStores(getTargetFolderPathForTestClass(no.vegvesen.ixn.federation.BiQpidStructureIT.class), "my_ca", HOST_NAME, "routing_configurer");
 
     SSLContext sslContext;
 
@@ -37,33 +38,42 @@ public class DlqExchangeTest extends QpidDockerBaseIT {
         sslContext = sslClientContext(stores, "routing_configurer");
         QpidClientConfig config = new QpidClientConfig(sslContext);
         qpidClient = new QpidClient(qpidContainer.getHttpsUrl(), qpidContainer.getvHostName(), config.qpidRestTemplate());
+        System.out.println(qpidContainer.getHttpUrl());
     }
 
     @Test
-    public void testSettingDlQueuetoExchange() throws Exception {
+    public void testSettingDlQueuetoExchange() {
         String queueName = "dlqueue";
 
-        Source source = new Source(qpidContainer.getAmqpsUrl(), queueName, sslContext);
-        source.start();
+        String name = "exchange-with-dlqueue";
 
-        Exchange exchange = new Exchange("exchange-with-dlqueue", "direct", new AlternateBinding(queueName));
+        Exchange exchange = qpidClient.createDirectExchangeWithDlq(name, queueName);
 
-        assertThat(exchange.getName()).isEqualTo("exchange-with-dlqueue");
+
+        assertThat(exchange.getName()).isEqualTo(name);
         assertThat(exchange.getAlternateBinding()).isNotNull();
         assertThat(exchange.getAlternateBinding().destination()).isEqualTo(queueName);
     }
 
     @Test
-    public void testExchangeWithoutDlQueue() throws Exception {
-        String queueName = "dlqueue";
-
-        Source source = new Source(qpidContainer.getAmqpsUrl(), queueName, sslContext);
-        source.start();
-
-        Exchange exchange = new Exchange("exchange-without-dlqueue", "direct", null);
+    public void testExchangeWithoutDlQueue() {
+        Exchange exchange = qpidClient.createDirectExchange("exchange-without-dlqueue");
 
         assertThat(exchange.getName()).isEqualTo("exchange-without-dlqueue");
         assertThat(exchange.getAlternateBinding()).isNull();
+    }
+
+    @Test
+    public void testSetupBothExchangeAndDlq()  {
+        UUID uuid =  UUID.randomUUID();
+
+        Queue queue = qpidClient.createQueue("dlq-" + uuid);
+        Exchange exchange = qpidClient.createDirectExchangeWithDlq("del-" + uuid, queue.getName());
+
+        assertThat(exchange).isNotNull();
+        assertThat(exchange.getAlternateBinding().destination()).isEqualTo(queue.getName());
+
+
     }
 
 }
