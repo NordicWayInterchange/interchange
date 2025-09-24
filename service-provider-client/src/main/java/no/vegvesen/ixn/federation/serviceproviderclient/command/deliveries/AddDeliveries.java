2756a3b036032462c7d0f3fdcdab88e09199138e
Maryam <maryam.mehrabian@bouvet.no>
2025-09-24 13:23:58 +0200

    Added -dlq flag to sp delivery

--- service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/deliveries/AddDeliveries.java
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/deliveries/AddDeliveries.java
@@ -21,9 +21,10 @@ import java.util.concurrent.Callable;
         customSynopsis = {
         """
                 Examples: \n
-                serviceproviderclient deliveries add -s "originatingCountry='NO'" -d "NO DELIVERY" \n
-                serviceproviderclient deliveries add -f denmDelivery.json -d "DENM Delivery" \n
+                serviceproviderclient deliveries add -s "originatingCountry='NO'" -d "NO DELIVERY" -dlq true\n
+                serviceproviderclient deliveries add -f denmDelivery.json -d "DENM Delivery" -dlq true\n
                 -d is optional
+                -dlq is optional
                 """
 }
 )
@@ -38,6 +39,11 @@ public class AddDeliveries implements Callable<Integer> {
     @Option(names = {"-d", "--description"})
     String description;
 
+
+    @Option(names = {"-dlq", "--dead letter queue"})
+    Boolean dlqueue = false;
+
+
     @Override
     public Integer call() throws Exception {
         ServiceProviderClient client = parentCommand.getParent().createClient();
@@ -49,7 +55,7 @@ public class AddDeliveries implements Callable<Integer> {
             System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
         }
         else{
-            AddDeliveriesRequest request = new AddDeliveriesRequest(client.getUser(), Set.of(new AddDelivery(option.selector, description)));
+            AddDeliveriesRequest request = new AddDeliveriesRequest(client.getUser(), Set.of(new AddDelivery(option.selector, description, dlqueue)));
             AddDeliveriesResponse response = client.addDeliveries(request);
             System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
         }
