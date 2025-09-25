58f34572a977d1fae70a098523671693b38269da
Maryam <maryam.mehrabian@bouvet.no>
2025-09-25 14:29:19 +0200

    Updated swagger examples

--- napcore-server/src/main/java/no/vegvesen/ixn/napcore/ExampleApiObjects.java
+++ napcore-server/src/main/java/no/vegvesen/ixn/napcore/ExampleApiObjects.java
@@ -93,7 +93,8 @@ public class ExampleApiObjects {
               "endpoints" : [ {
                 "host" : "a.bouvetinterchange.eu",
                 "port" : 5671,
-                "target" : "del-d6728909-0f6e-4a6d-9fee-3e1be3eadd63"
+                "target" : "del-d6728909-0f6e-4a6d-9fee-3e1be3eadd63",
+                "dlqName": "dlq-079f0248-7979-48ae-8563-2ff30afe986c"
               } ],
               "lastUpdatedTimestamp" : 1726567679,
               "description": "Deliver messages from Norway"
@@ -108,7 +109,8 @@ public class ExampleApiObjects {
               "endpoints" : [ {
                 "host" : "a.bouvetinterchange.eu",
                 "port" : 5671,
-                "target" : "del-d6728909-0f6e-4a6d-9fee-3e1be3eadd63"
+                "target" : "del-d6728909-0f6e-4a6d-9fee-3e1be3eadd63",
+                "dlqName": "dlq-f333183a-ad2f-4e46-9ff6-ae6a0c88cfa9"
               } ],
               "lastUpdatedTimestamp" : 1726567679,
               "description": "Deliver messages from Norway"
