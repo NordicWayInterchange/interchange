0264abd57c9fb3e817330221308ba4c02cb2ee2e
Maryam <maryam.mehrabian@bouvet.no>
2025-11-04 14:14:17 +0100

    Revert "Bi-queue as a separate component"
    This reverts commit b9609b3139c01cd571ac5d8a944c5e218336e253.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Divider, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Card, CardContent, Divider, IconButton, Tooltip, Typography } from "@mui/material";
 import { useSession } from "next-auth/react";
 import { Box, Stack } from "@mui/system";
 import Link from "next/link";
@@ -17,8 +17,11 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import PersonIcon from "@mui/icons-material/Person";
 import LockIcon from "@mui/icons-material/Lock";
 import { ContentCopy } from "@/components/shared/actions/ContentCopy";
-import { frontPageCardStyle } from "@/components/shared/styles/CardStyle";
-import BiQueue from "@/pages/biQueue/bi-queue";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { StyledButton } from "@/components/shared/styles/StyledSelectorBuilder";
+import { tooltipFontStyle } from "@/components/shared/styles/TooltipFontStyle";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -65,12 +68,19 @@ export default function Home() {
     },
   ];
 
+  const [hasAccess, setHasAccess] = useState(false);
+
+
+  const handleToggleAccess = () => {
+    setHasAccess((prev) => !prev);
+  };
+
   return (
     <Box flex={1}>
       <Mainheading>Welcome, {session?.user?.name}!</Mainheading>
       <Divider sx={{ marginY: 3 }} />
 
-      <Box sx={frontPageCardStyle}>
+      <Box sx={elementStyle}>
         <Typography
           sx={{ fontSize: "0.85rem", fontWeight: 600}}>
           Your common name:
@@ -82,7 +92,70 @@ export default function Home() {
       </Box>
       <Divider sx={{ marginY: 2, visibility: 'hidden' }} />
 
-      <BiQueue/>
+      <Box>
+        <Card sx={elementStyle}>
+          <CardContent>
+            {hasAccess ? (
+              <>
+                <CheckCircleOutlineIcon color="success" sx={{ fontSize: 50, mb: 2 }} />
+                <Typography variant="h6" gutterBottom>
+                  Bi Queue Access Granted
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
+                  You currently have permission to view Bi queue.
+                  <Tooltip
+                    slotProps={{
+                      tooltip: {
+                        sx: tooltipFontStyle,
+                      },
+                    }}
+                    title="Revoke access to the bi-consumers group"
+                  >
+                    <IconButton size="small">
+                      <InfoOutlinedIcon fontSize="small" sx={{ mt: -2 }} />
+                    </IconButton>
+                  </Tooltip>
+                </Typography>
+              </>
+            ) : (
+              <>
+                <LockOutlinedIcon color="action" sx={{ fontSize: 50, mb: 2 }} />
+                <Typography variant="h6" gutterBottom>
+                  No Access to Bi Queue
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
+                  You currently do not have permission to Bi queue.
+                  <Tooltip
+                    slotProps={{
+                      tooltip: {
+                        sx: tooltipFontStyle,
+                      },
+                    }}
+                    title="Gain access to the bi-consumers group"
+                  >
+                    <IconButton size="small">
+                      <InfoOutlinedIcon fontSize="small" sx={{ mt: -2 }} />
+                    </IconButton>
+                  </Tooltip>
+                </Typography>
+              </>
+            )}
+
+            <Stack spacing={2} sx={{display: "flex", alignItems: "center",textAlign: 'center', justifyContent: "center"}}>
+              <StyledButton
+                variant="contained"
+                color={hasAccess ? 'redLight' : 'buttonThemeColor'}
+                onClick={handleToggleAccess}
+              >
+                {hasAccess ? 'Revoke Access' : 'Grant Access'}
+              </StyledButton>
+
+            </Stack>
+          </CardContent>
+        </Card>
+      </Box>
+
+
       <Divider sx={{ marginY: 3 }} />
       <Subheading>Shortcuts</Subheading>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
@@ -137,3 +210,25 @@ export default function Home() {
   );
 }
 
+const elementStyle = {
+  display: "flex",
+  alignItems: "center",
+  textAlign: 'center',
+  bgcolor: "white",
+  justifyContent: "center",
+  flexDirection: "row",
+  padding: 1,
+  borderRadius: 2,
+  borderBottom: "2px solid #dd7100",
+  boxShadow: 3,
+  maxWidth: "fit-content",
+  margin: "left",
+  gap: 0.5,
+  mt: 1.5,
+  wordBreak: "break-word",
+  flexShrink: 0,
+  "@media (min-width:600px)": {
+    flexDirection: "row",
+    maxWidth: "fit-content"
+  }
+};
