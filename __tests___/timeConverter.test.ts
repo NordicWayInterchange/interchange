78ae2bf9fce3392c019c1f5a08da55a5afca978d
Maryam <maryam.mehrabian@bouvet.no>
2025-11-20 12:43:35 +0100

    rewrite timeConverter.test to work with node 22

--- __tests___/timeConverter.test.ts
+++ __tests___/timeConverter.test.ts
@@ -7,11 +7,12 @@ describe('timeConverter', () => {
       },
     });
 
-    const epoch = 1732619387;
+    const epochSeconds = 1732619387;
+    const epochMillis = epochSeconds * 1000;
     const expectedDate = "26 Nov 2024";
     const expectedTime = "12:09:47";
     const expectedFormat = `${expectedDate}\u2003${expectedTime}`;
 
-    expect(timeConverter(epoch)).toEqual(expectedFormat);
+    expect(timeConverter(epochMillis)).toEqual(expectedFormat);
   });
 });
