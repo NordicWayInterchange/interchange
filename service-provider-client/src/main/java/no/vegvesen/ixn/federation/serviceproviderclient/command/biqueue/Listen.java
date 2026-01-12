e6c7257bdac79792d0a94611c1cbcb7eca02066c
Maryam <maryam.mehrabian@bouvet.no>
2026-01-12 19:14:43 +0100

    SVVNNI-374: Listen to different bi-queue based on messageType

--- service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/biqueue/Listen.java
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/biqueue/Listen.java
@@ -8,6 +8,7 @@ import no.vegvesen.ixn.federation.serviceproviderclient.ServiceProviderClient;
 import no.vegvesen.ixn.serviceprovider.model.*;
 import picocli.CommandLine;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 
@@ -18,7 +19,7 @@ import java.util.concurrent.CountDownLatch;
         customSynopsis = {
                 """ 
                         Examples:\n
-                        serviceproviderclient bi-queue listen -d directory \n
+                        serviceproviderclient bi-queue listen DENM -d directory \n
                         # -d is optional
                         """
         })
@@ -30,17 +31,35 @@ public class Listen implements Callable<Integer> {
     @CommandLine.Option(names = {"-d", "--directory"}, description = "directory to save messages")
     String directory;
 
+    @CommandLine.Parameters(index = "0", description = "Message type")
+    String messageType;
+
     private final CountDownLatch counter = new CountDownLatch(1);
 
     @Override
     public Integer call() throws Exception {
         ServiceProviderClient client = parentCommand.getParent().createClient();
 
-        GetBiqueueEndpointResponse biqueueEndpointResponse = client.getBiqueueEndpoint();
+        List<GetBiqueueEndpointsResponsePerMessageType> biqueueEndpointsResponse = client.listBiqueues();
 
-        String biqueueName = biqueueEndpointResponse.getQueueName();
+        String biqueueName = biqueueEndpointsResponse.stream()
+                .map(biqueueEndpointResponse -> biqueueEndpointResponse.getGetBiqueueEndpointResponse().getQueueName())
+                .filter(s -> isMatched(s, messageType))
+                .findFirst()
+                .orElse(null);
 
-        String url = "amqps://" + biqueueEndpointResponse.getBrokerExternalName();
+        System.out.println(biqueueName);
+
+        if (biqueueName == null) {
+            throw new RuntimeException(String.format("Bi-queue %s does not exist!", biqueueName));
+        }
+
+        String brokerName = biqueueEndpointsResponse.stream()
+                .filter(element -> element.getGetBiqueueEndpointResponse().getQueueName().equals(biqueueName))
+                .map( o -> o.getGetBiqueueEndpointResponse().getBrokerExternalName())
+                .findFirst().orElse(null);
+
+        String url = "amqps://" + brokerName;
 
         System.out.printf("Listening for messages from queue [%s] on server [%s]%n", biqueueName, url);
         ExceptionListener exceptionListener = e -> {
@@ -59,4 +78,17 @@ public class Listen implements Callable<Integer> {
         }
         return 0;
     }
+
+    private static boolean isMatched(String id, String input) {
+        String a = normalize(id);
+        String b = normalize(input);
+        return a.contains(b) || b.contains(a);
+    }
+
+    private static String normalize(String s) {
+        return s.toLowerCase()
+                .replace("_", "")
+                .replace("-", "");
+    }
 }
+
