c7dca50a5383baa038abf4691c44ffb9f52a4551
Maryam <maryam.mehrabian@bouvet.no>
2026-02-03 15:08:43 +0100

    Refactored pc send

--- service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/privatechannels/Send.java
+++ service-provider-client/src/main/java/no/vegvesen/ixn/federation/serviceproviderclient/command/privatechannels/Send.java
@@ -1,22 +1,15 @@
 package no.vegvesen.ixn.federation.serviceproviderclient.command.privatechannels;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.jms.InvalidDestinationException;
-import no.vegvesen.ixn.MessageBuilder;
 import no.vegvesen.ixn.Source;
 import no.vegvesen.ixn.federation.serviceproviderclient.ServiceProviderClient;
-import no.vegvesen.ixn.federation.serviceproviderclient.messages.*;
 import no.vegvesen.ixn.serviceprovider.model.*;
+import org.apache.qpid.jms.message.JmsMessage;
 import picocli.CommandLine;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
-
-import static no.vegvesen.ixn.federation.api.v1_0.Constants.*;
 
 
 @CommandLine.Command(name = "send",
@@ -85,124 +78,15 @@ public class Send implements Callable<Integer> {
         String url = "amqps://" + privateChannelEndpoint.getHost();
 
         System.out.printf("Sending message from file %s%n", messageFile);
-        ObjectMapper mapper = new ObjectMapper();
-        Messages messages = mapper.readValue(messageFile, Messages.class);
-        validateInput(messages);
+
         try (Source source = new Source(url, queueName, parentCommand.getParent().createSSLContext())) {
-
-            while (true) {
-                try {
-                    source.start();
-                    break;
-                } catch (InvalidDestinationException e) {
-                    System.out.println("\nRetrying\n");
-                    TimeUnit.SECONDS.sleep(3);
-                }
-            }
-
-            for (Message message : messages.getMessages()) {
-                MessageBuilder messageBuilder = source.createMessageBuilder();
-                switch (message) {
-                    case DenmMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(DENM)
-                                .causeCode(((DenmMessage) message).getCauseCode())
-                                .subCauseCode(((DenmMessage) message).getSubCauseCode());
-                    }
-                    case DatexMessage ignored -> {
-                        messageBuilder
-                                .textMessage(message.getMessageText())
-                                .messageType(DATEX_2)
-                                .publicationType(((DatexMessage) message).getPublicationType())
-                                .publicationSubType(((DatexMessage) message).getPublicationSubType())
-                                .publisherName(((DatexMessage) message).getPublisherName());
-                    }
-                    case IvimMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(IVIM)
-                                .iviType(((IvimMessage) message).getIviType())
-                                .pictogramCategoryCode(((IvimMessage) message).getPictogramCategoryCode())
-                                .iviContainer(((IvimMessage) message).getIviContainer());
-                    }
-                    case SpatemMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(SPATEM)
-                                .id(((SpatemMessage) message).getId())
-                                .name(((SpatemMessage) message).getName());
-                    }
-                    case MapemMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(MAPEM)
-                                .id(((MapemMessage) message).getId())
-                                .name(((MapemMessage) message).getName());
-                    }
-                    case SsemMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(SSEM)
-                                .id(((SsemMessage) message).getId());
-                    }
-                    case SremMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(SREM)
-                                .id(((SremMessage) message).getId());
-                    }
-                    case CamMessage ignored -> {
-                        messageBuilder
-                                .bytesMessage(binary ? convertFileToByteArray(message.getFileName()) : message.getMessageText().getBytes(StandardCharsets.UTF_8))
-                                .messageType(CAM)
-                                .stationType(((CamMessage) message).getStationType())
-                                .vehicleRole(((CamMessage) message).getVehicleRole());
-                    }
-
-                    default -> throw new Exception("Message is not of valid messagetype");
-                }
-
-                messageBuilder
-                        .userId(message.getUserId())
-                        .publisherId(message.getPublisherId())
-                        .publicationId(message.getPublicationId())
-                        .originatingCountry(message.getOriginatingCountry())
-                        .protocolVersion(message.getProtocolVersion())
-                        .longitude(message.getLongitude())
-                        .latitude(message.getLatitude())
-                        .quadTreeTiles(message.getQuadTree())
-                        .shardId(message.getShardId())
-                        .shardCount(message.getShardCount())
-                        .baselineVersion(message.getBaselineVersion())
-                        .serviceType(message.getServiceType());
-                source.send(messageBuilder.build());
-
-            }
+            JmsMessage message1 = source.createMessageBuilder()
+                    .textMessage("fishy fishy")
+                    .userId(privateChannelEndpoint.getHost())
+                    .build();
+            source.send(message1);
         }
         return 0;
-    }
-
-    private static byte[] convertFileToByteArray(String fileName) throws IOException {
-        File file = new File(fileName);
-        return Files.readAllBytes(file.toPath());
-    }
-
-    private void validateInput(Messages messages) throws Exception {
-        for (Message message : messages.getMessages()) {
-            if (binary) {
-                if (message.getMessageType().equals(DATEX_2)) {
-                    throw new Exception("DATEX messages can not be sent binary.");
-                }
-                if (message.getFile() == null) {
-                    throw new Exception("Message does not contain file");
-                }
-            } else {
-                if (message.getMessageText() == null) {
-                    throw new Exception("Message does not contain messageText");
-                }
-            }
-        }
     }
 
     private static class PrivatechannelsOption {
