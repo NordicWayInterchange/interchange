410f0f518acd199e0b4442ece90a6638251a0c4b
Maryam <maryam.mehrabian@bouvet.no>
2026-02-12 09:38:56 +0100

    Exported the target with timestamp

--- import-export-service/src/main/java/no/vegvesen/ixn/federation/service/ExportApplication.java
+++ import-export-service/src/main/java/no/vegvesen/ixn/federation/service/ExportApplication.java
@@ -7,13 +7,11 @@ import no.vegvesen.ixn.federation.service.exportmodel.ExportApi;
 import no.vegvesen.ixn.federation.repository.NeighbourRepository;
 import no.vegvesen.ixn.federation.repository.PrivateChannelRepository;
 import no.vegvesen.ixn.federation.repository.ServiceProviderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 
 public class ExportApplication {
@@ -40,8 +38,30 @@ public class ExportApplication {
                 privateChannelRepository.findAll().stream().map(exportTransformer::transformPrivateChannelToPrivateChannelExportApi).collect(Collectors.toSet())
         );
         ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
-        String localPath = "";
-        Path path = Paths.get(localPath, "dump.json");
-        writer.writeValue(path.toFile(), exportModel);
+//        String localPath = "";
+//        Path path = Paths.get(localPath, "dump.json");
+//        writer.writeValue(path.toFile(), exportModel);
+
+        String outputDir = System.getenv("EXPORT_PATH");
+
+        if (outputDir == null || outputDir.isBlank()) {
+            outputDir = "./";
+        }
+
+        Path directory = Paths.get(outputDir);
+
+        if (!Files.exists(directory)) {
+            Files.createDirectories(directory);
+        }
+
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+
+        Path filePath = directory.resolve("export-" + timestamp + ".json");
+
+        if (Files.exists(filePath)) {
+            throw new RuntimeException("File already exists: " + filePath);
+        }
+        writer.writeValue(filePath.toFile(), exportModel);
+        System.out.println("Export saved to: " + filePath.toAbsolutePath());
     }
 }
