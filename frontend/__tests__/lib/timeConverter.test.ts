d328ed8ebe597f898d5bdff992cb063b53226ce4
Maryam <maryam.mehrabian@bouvet.no>
2026-02-24 11:14:58 +0100

    Fix the test to pass with node 20

--- frontend/__tests__/lib/timeConverter.test.ts
+++ frontend/__tests__/lib/timeConverter.test.ts
@@ -1,11 +1,17 @@
 import { timeConverter } from "@/lib/timeConverter";
-import { afterEach, describe, it, expect } from '@jest/globals';
+import { afterEach, beforeAll, describe, it, expect } from '@jest/globals';
 import { jest } from '@jest/globals';
 
 describe("Can correctly convert Unix Epoch", () => {
+
+  beforeAll(() => {
+    process.env.TZ = 'UTC';
+  });
+
   const epoch = 1731527848;
-  const englishDateFormat = "13 Nov 2024 20:57:28";
-  const persianDateFormat = "۲۳ آبان ۱۴۰۳ ۲۰:۵۷:۲۸";
+
+  const englishDateFormat = "13 Nov 2024 19:57:28";
+  const persianDateFormat = "۲۳ آبان ۱۴۰۳ ۱۹:۵۷:۲۸";
 
   it("can convert to correct english date and time format", () => {
     jest.spyOn(navigator, 'language', 'get').mockReturnValue('en-GB');
