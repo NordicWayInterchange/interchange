23f85f26045864ad0da90320b8451c769ea1ae0d
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2026-02-25 17:50:18 +0100

    Small fix, set the status explicitly

--- napcore-server/src/main/java/no/vegvesen/ixn/napcore/TypeTransformer.java
+++ napcore-server/src/main/java/no/vegvesen/ixn/napcore/TypeTransformer.java
@@ -59,6 +59,7 @@ public class TypeTransformer {
                         )
                 ),
                 delivery.getSelector(),
+                LocalDeliveryStatus.REQUESTED,
                 delivery.getDescription(),
                 dlqueue
         );
