62fb229e4f62cc44b08e7f81d2c980c811bf5cff
Maryam <maryam.mehrabian@bouvet.no>
2026-03-11 15:08:52 +0100

    Added --platform=linux/amd64

--- demo/single-node/single-node-keys.sh
+++ demo/single-node/single-node-keys.sh
@@ -2,5 +2,5 @@
 VOLUME_NAME=single-node-keys-volume
 echo "Generating systemtest keys to volume $VOLUME_NAME"
 docker volume create $VOLUME_NAME
-docker run -it -v ${PWD}:/work -v $VOLUME_NAME:/keys \
+docker run --platform=linux/amd64 -it -v ${PWD}:/work -v $VOLUME_NAME:/keys \
 	ghcr.io/nordicwayinterchange/keys-generator:${IMAGE_TAG} generate -f single-node-keys.json -o /keys
