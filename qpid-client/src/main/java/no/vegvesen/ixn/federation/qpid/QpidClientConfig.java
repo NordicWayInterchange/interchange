2756ffdf26eea3de10eefad9d49c0a65c7cff410
Ola Holde <ola.holde@bouvet.no>
2026-03-16 12:08:04 +0100

    SVVNNI-393: Add constructor with SSLContext.

--- qpid-client/src/main/java/no/vegvesen/ixn/federation/qpid/QpidClientConfig.java
+++ qpid-client/src/main/java/no/vegvesen/ixn/federation/qpid/QpidClientConfig.java
@@ -8,26 +8,30 @@ import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuil
 import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ssl.SslBundle;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+
 @Configuration
 public class QpidClientConfig {
 
-
-	private final SslBundle sslBundle;
+	private final SSLContext sslContext;
 
 	@Autowired
 	public QpidClientConfig(SslBundles sslBundles) {
-		this.sslBundle = sslBundles.getBundle("qpid-client");
+		this.sslContext = sslBundles.getBundle("qpid-client").createSslContext();
+	}
+
+	public QpidClientConfig(SSLContext sslContext) {
+		this.sslContext = sslContext;
 	}
 
 	private CloseableHttpClient httpsClient() {
-		DefaultClientTlsStrategy strategy = new DefaultClientTlsStrategy(sslBundle.createSslContext(), NoopHostnameVerifier.INSTANCE);
+		DefaultClientTlsStrategy strategy = new DefaultClientTlsStrategy(sslContext, NoopHostnameVerifier.INSTANCE);
 		PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder
 				.create()
 				.setTlsSocketStrategy(strategy)
