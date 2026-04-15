52d1b94176f89c13455e0c81a03d26f89ff46896
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2026-04-15 12:33:56 +0200

    Fixed some settings in keycloak provider that has fallen out

--- frontend/auth/providers.ts
+++ frontend/auth/providers.ts
@@ -6,16 +6,18 @@ export default function buildProviders() {
         const baseInternal = `${process.env.INTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}`;
 
         return Keycloak({
+            jwks_endpoint: `${baseInternal}/protocol/openid-connect/certs`,
+            wellKnown: undefined,
             clientId: process.env.KEYCLOAK_CLIENT_ID ?? "clientId is not defined in environment variables",
             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET ?? "clientSecret is not defined in environment variables",
-            issuer: baseInternal,
+            issuer:  `${process.env.EXTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}`,
             authorization: {
-                url: `${baseInternal}/protocol/openid-connect/auth`,
+                url: `${process.env.EXTERNAL_KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/auth`,
                 params: { prompt: "login" },
             },
             name: `Keycloak (${process.env.KEYCLOAK_REALM})`,
             token: `${baseInternal}/protocol/openid-connect/token`,
-            userinfo: `${baseInternal}/protocol/openid-connect/userinfo`,
+            userinfo:  `${baseInternal}/protocol/openid-connect/userinfo`,
         })
 
     } else {
