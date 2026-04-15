5408557a99d088304dcda5fd5a5bf896f6b2904b
Kyrre Kristiansen <kyrre.kristiansen@bouvet.no>
2026-04-15 14:59:59 +0200

    Fixed the nap client test script

--- systemtest-scripts/a_nap_client.sh
+++ systemtest-scripts/a_nap_client.sh
@@ -8,7 +8,7 @@ docker run \
   --name a_napcore_client \
   --network=systemtest-scripts_testing_net \
   --dns=172.28.1.1 \
-  -v $PWD/../tmp/keys:/keys \
+  -v systemtest-keys-volume:/keys \
   -v $PWD:/work \
   --link a_napcore_server:a.bouvetinterchange.eu \
-  napcore_rest_client -k /keys/nap.a.internal.bouvetinterchange.eu.p12 -s password -t /keys/ca.internal.a.bouvetinterchange.eu.jks -w password https://a.bouvetinterchange.eu:8898/ ${SERVICE_PROVIDER} nap.a.internal.bouvetinterchange.eu "$@"
+  napcore_rest_client -k /keys/nap.a.internal.bouvetinterchange.eu.p12 -s password -t /keys/ca.internal.a.bouvetinterchange.eu.jks -w password https://a-napcore-server:8898/ ${SERVICE_PROVIDER} nap.a.internal.bouvetinterchange.eu "$@"
