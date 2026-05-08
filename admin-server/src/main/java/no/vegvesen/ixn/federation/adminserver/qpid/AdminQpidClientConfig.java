fa253ebf2f8648a909798e0a5a49ad7569ede807
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2026-05-08 11:42:25 +0200

    Changed from deprecated classes and methods

--- admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/qpid/AdminQpidClientConfig.java
+++ admin-server/src/main/java/no/vegvesen/ixn/federation/adminserver/qpid/AdminQpidClientConfig.java
@@ -5,6 +5,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
@@ -30,15 +31,9 @@ public class AdminQpidClientConfig {
 
 	private CloseableHttpClient httpsClient() {
 		SSLContext context = sslBundles.getBundle("qpid").createSslContext();
-
-		SSLConnectionSocketFactory sslConnectionSocketFactory = SSLConnectionSocketFactoryBuilder
-				.create()
-				.setSslContext(context)
-				.setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-				.build();
 		PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder
 				.create()
-				.setSSLSocketFactory(sslConnectionSocketFactory)
+				.setTlsSocketStrategy(new ClientTlsStrategyBuilder().setSslContext(context).buildClassic())
 				.setMaxConnTotal(5)
 				.setMaxConnPerRoute(2)
 				.build();
