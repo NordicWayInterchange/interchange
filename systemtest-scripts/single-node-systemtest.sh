c1771718520147010bed62cd29e0c8977ec038be
Maryam <maryam.mehrabian@bouvet.no>
2026-06-02 22:53:35 +0200

    Edited single-node-systemtest.yml

--- systemtest-scripts/single-node-systemtest.sh
+++ systemtest-scripts/single-node-systemtest.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/bash -eu
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 export BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
-
 export JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout)
-VOLUME_NAME=single-node-systemtest-keys-volume
-docker build ../service-provider-client -t service_provider_client --build-arg JAR_VERSION=$JAR_VERSION
+echo "Running system test on branch $BRANCH with tag $BRANCH_TAG, jar version $JAR_VERSION"
+docker build ../service-provider-client -t onboard_rest_client --build-arg JAR_VERSION=$JAR_VERSION
 docker build ../napcore-rest-client -t napcore_rest_client --build-arg JAR_VERSION=$JAR_VERSION
 docker build ../keys-generator -t keys-generator --build-arg JAR_VERSION=$JAR_VERSION
+VOLUME_NAME=systemtest-keys-volume
 VOL_EXISTS=$( docker volume ls --format '{{.Name}}' -f name=${VOLUME_NAME})
-[ -n "$VOL_EXISTS" ] || ./single-node-systemtest-keys.sh
+[ -n "$VOL_EXISTS" ] || ./systemtest-keys.sh
 docker compose -f single-node-systemtest.yml build --build-arg JAR_VERSION=$JAR_VERSION && docker compose -f single-node-systemtest.yml up
-
