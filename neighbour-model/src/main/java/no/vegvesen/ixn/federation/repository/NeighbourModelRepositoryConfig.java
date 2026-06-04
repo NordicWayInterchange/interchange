f4837067143a09081393b63fd32148611d60b057
Maryam <maryam.mehrabian@bouvet.no>
2026-06-04 22:09:01 +0200

    Fixed @EntityScan

--- neighbour-model/src/main/java/no/vegvesen/ixn/federation/repository/NeighbourModelRepositoryConfig.java
+++ neighbour-model/src/main/java/no/vegvesen/ixn/federation/repository/NeighbourModelRepositoryConfig.java
@@ -1,6 +1,6 @@
 package no.vegvesen.ixn.federation.repository;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
