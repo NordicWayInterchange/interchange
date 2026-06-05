b0240a6834931005f82776d3f1984b2b88be6ba1
kyrre.kristiansen <kyrre.kristiansen@bouvet.no>
2026-06-05 16:40:57 +0200

    Took away the junit version pin, as we are no longer ahead of spring boot on this dependency. Also fix a couple of dependencies where annotations have been moved.

--- napcore-server/src/test/java/no/vegvesen/ixn/napcore/NapRestControllerTest.java
+++ napcore-server/src/test/java/no/vegvesen/ixn/napcore/NapRestControllerTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
