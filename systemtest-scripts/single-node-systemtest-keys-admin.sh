cde0185eedb9d5d8857fdaf8fad38257992f382b
Maryam <maryam.mehrabian@bouvet.no>
2026-07-29 14:32:03 +0200

    Edited single-node-systemtest admin

--- systemtest-scripts/single-node-systemtest-keys-admin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-VOLUME_NAME=single-node-systemtest-keys-admin-volume
-
-echo "Generating keys to volume $VOLUME_NAME"
-docker volume create $VOLUME_NAME
-docker run -it -v${PWD}:/work -v $VOLUME_NAME:/keys keys-generator generate -s -f single-node-systemtest-keys-admin.json -o /keys
