30feeac8c7ea4fbc7fea382fef27351c53232548
Maryam <maryam.mehrabian@bouvet.no>
2026-07-30 11:39:43 +0200

    fixed connecting to admin frontend

--- systemtest-scripts/single-node-systemtest-admin.sh
+++ systemtest-scripts/single-node-systemtest-admin.sh
@@ -5,6 +5,7 @@ export BRANCH_TAG="${BRANCH//[^a-zA-Z_0-9]/_}"
 export JAR_VERSION=$(mvn -f .. org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Running system test on branch $BRANCH with tag $BRANCH_TAG, jar version $JAR_VERSION"
 docker build ../service-provider-client -t onboard_rest_client --build-arg JAR_VERSION=$JAR_VERSION
+docker build ../admin-frontend -t admin_frontend --build-arg JAR_VERSION=$JAR_VERSION
 docker build ../keys-generator -t keys-generator --build-arg JAR_VERSION=$JAR_VERSION
 VOLUME_NAME=single-node-systemtest-keys-volume
 VOL_EXISTS=$( docker volume ls --format '{{.Name}}' -f name=${VOLUME_NAME})
