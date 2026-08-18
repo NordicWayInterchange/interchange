9c7de76920b9687cf098547b41463c0f3552814d
Ola Holde <ola.holde@bouvet.no>
2026-08-18 14:14:15 +0200

    SVVNNI-505: Migrate to Jackson 3 in napcore-rest-client.

--- napcore-rest-client/src/main/java/no/vegvesen/ixn/napcore/client/NapRESTClient.java
+++ napcore-rest-client/src/main/java/no/vegvesen/ixn/napcore/client/NapRESTClient.java
@@ -1,6 +1,5 @@
 package no.vegvesen.ixn.napcore.client;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import no.vegvesen.ixn.cert.CsrGenerator;
 import no.vegvesen.ixn.cert.KeyPairAndCsr;
 import no.vegvesen.ixn.napcore.model.*;
@@ -14,6 +13,7 @@ import org.bouncycastle.operator.OperatorCreationException;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import tools.jackson.core.JacksonException;
 
 import javax.net.ssl.SSLContext;
 import java.security.NoSuchAlgorithmException;
@@ -83,15 +83,15 @@ public class NapRESTClient {
         restTemplate.delete(String.format("%s/nap/%s/subscriptions/%s", server, user, subscriptionId));
     }
 
-    public List<Capability> getMatchingCapabilities(String selector) throws JsonProcessingException {
+    public List<Capability> getMatchingCapabilities(String selector) throws JacksonException {
         String url = String.format("%s/nap/%s/subscriptions/capabilities?selector={selector}", server, user);
-        Map<String,String> parameters = new HashMap<>();
-        parameters.put("selector",selector);
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("selector", selector);
         ResponseEntity<Capability[]> response = restTemplate.getForEntity(url, Capability[].class, parameters);
         return Arrays.asList(response.getBody());
     }
 
-    public Delivery addDelivery(DeliveryRequest deliveryRequest){
+    public Delivery addDelivery(DeliveryRequest deliveryRequest) {
         String url = String.format("%s/nap/%s/deliveries", server, user);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -99,23 +99,23 @@ public class NapRESTClient {
         return restTemplate.exchange(url, HttpMethod.POST, entity, Delivery.class).getBody();
     }
 
-    public Delivery getDelivery(String deliveryId){
+    public Delivery getDelivery(String deliveryId) {
         String url = String.format("%s/nap/%s/deliveries/%s", server, user, deliveryId);
         return restTemplate.getForEntity(url, Delivery.class).getBody();
     }
 
-    public List<Delivery> getDeliveries(){
+    public List<Delivery> getDeliveries() {
         String url = String.format("%s/nap/%s/deliveries", server, user);
         ResponseEntity<Delivery[]> response = restTemplate.getForEntity(url, Delivery[].class);
         return Arrays.asList(response.getBody());
     }
 
-    public void deleteDelivery(String deliveryId){
+    public void deleteDelivery(String deliveryId) {
         String url = String.format("%s/nap/%s/deliveries/%s", server, user, deliveryId);
         restTemplate.delete(url);
     }
 
-    public List<Capability> getMatchingDeliveryCapabilities(String selector){
+    public List<Capability> getMatchingDeliveryCapabilities(String selector) {
         String url = String.format("%s/nap/%s/deliveries/capabilities?selector={selector}", server, user);
         Map<String, String> parameters = new HashMap<>();
         parameters.put("selector", selector);
@@ -123,7 +123,7 @@ public class NapRESTClient {
         return Arrays.asList(response.getBody());
     }
 
-    public OnboardingCapability addCapability(CapabilitiesRequest request){
+    public OnboardingCapability addCapability(CapabilitiesRequest request) {
         String url = String.format("%s/nap/%s/capabilities", server, user);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -131,29 +131,29 @@ public class NapRESTClient {
         return restTemplate.exchange(url, HttpMethod.POST, entity, OnboardingCapability.class).getBody();
     }
 
-    public OnboardingCapability getCapability(String capabilityId){
+    public OnboardingCapability getCapability(String capabilityId) {
         String url = String.format("%s/nap/%s/capabilities/%s", server, user, capabilityId);
         return restTemplate.getForEntity(url, OnboardingCapability.class).getBody();
     }
 
-    public List<OnboardingCapability> getCapabilities(){
+    public List<OnboardingCapability> getCapabilities() {
         String url = String.format("%s/nap/%s/capabilities", server, user);
         ResponseEntity<OnboardingCapability[]> response = restTemplate.getForEntity(url, OnboardingCapability[].class);
         return Arrays.asList(response.getBody());
     }
 
-    public Set<String> getPublicationIds(){
+    public Set<String> getPublicationIds() {
         String url = String.format("%s/nap/%s/capabilities/publicationids", server, user);
         ResponseEntity<String[]> response = restTemplate.getForEntity(url, String[].class);
         return Arrays.stream(response.getBody()).collect(Collectors.toSet());
     }
 
-    public void deleteCapability(String capabilityId){
+    public void deleteCapability(String capabilityId) {
         String url = String.format("%s/nap/%s/capabilities/%s", server, user, capabilityId);
         restTemplate.delete(url);
     }
 
-    public PrivateChannelResponse addPrivateChannel(PrivateChannelRequest privateChannelRequest){
+    public PrivateChannelResponse addPrivateChannel(PrivateChannelRequest privateChannelRequest) {
         String url = String.format("%s/nap/%s/privatechannels", server, user);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -161,23 +161,23 @@ public class NapRESTClient {
         return restTemplate.exchange(url, HttpMethod.POST, entity, PrivateChannelResponse.class).getBody();
     }
 
-    public void deletePrivateChannel(String privateChannelId){
+    public void deletePrivateChannel(String privateChannelId) {
         String url = String.format("%s/nap/%s/privatechannels/%s", server, user, privateChannelId);
         restTemplate.delete(url);
     }
 
-    public List<PrivateChannelResponse> getPrivateChannels(){
+    public List<PrivateChannelResponse> getPrivateChannels() {
         String url = String.format("%s/nap/%s/privatechannels", server, user);
         ResponseEntity<PrivateChannelResponse[]> response = restTemplate.getForEntity(url, PrivateChannelResponse[].class);
         return Arrays.asList(response.getBody());
     }
 
-    public PrivateChannelResponse getPrivateChannel(String privateChannelId){
+    public PrivateChannelResponse getPrivateChannel(String privateChannelId) {
         String url = String.format("%s/nap/%s/privatechannels/%s", server, user, privateChannelId);
         return restTemplate.getForEntity(url, PrivateChannelResponse.class).getBody();
     }
 
-    public List<PeerPrivateChannel> getPeerPrivateChannels(){
+    public List<PeerPrivateChannel> getPeerPrivateChannels() {
         String url = String.format("%s/nap/%s/privatechannels/peer", server, user);
         ResponseEntity<PeerPrivateChannel[]> response = restTemplate.getForEntity(url, PeerPrivateChannel[].class);
         return Arrays.asList(response.getBody());
@@ -211,7 +211,7 @@ public class NapRESTClient {
                             country
                     )
             );
-            return new CsrGenerator("RSA",2048,"SHA512withRSA").generateKeyPairAndCsr(x500Name);
+            return new CsrGenerator("RSA", 2048, "SHA512withRSA").generateKeyPairAndCsr(x500Name);
         } catch (NoSuchAlgorithmException | OperatorCreationException e) {
             throw new RuntimeException(e);
         }
--- napcore-rest-client/src/main/java/no/vegvesen/ixn/napcore/client/command/capabilities/AddNapCapability.java
+++ napcore-rest-client/src/main/java/no/vegvesen/ixn/napcore/client/command/capabilities/AddNapCapability.java
@@ -1,14 +1,16 @@
 package no.vegvesen.ixn.napcore.client.command.capabilities;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import no.vegvesen.ixn.napcore.client.NapRESTClient;
 import no.vegvesen.ixn.napcore.model.CapabilitiesRequest;
 import no.vegvesen.ixn.napcore.model.OnboardingCapability;
-import static picocli.CommandLine.*;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.*;
 
 @Command(
         name = "add",
@@ -27,7 +29,7 @@ public class AddNapCapability implements Callable<Integer> {
     @Override
     public Integer call() throws IOException {
         NapRESTClient client = parentCommand.getParentCommand().createClient();
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = JsonMapper.builder().build();
         CapabilitiesRequest request = mapper.readValue(file, CapabilitiesRequest.class);
         OnboardingCapability response = client.addCapability(request);
         System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
