6a56568b1264351eab382c744d197ab94d911ff3
Maryam <maryam.mehrabian@bouvet.no>
2026-09-01 12:44:30 +0200

    Fixed backend connection

--- systemtest-scripts/single-node-systemtest-keys.sh
+++ systemtest-scripts/single-node-systemtest-keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-VOLUME_NAME=single-node-systemtest-keys-volume
+VOLUME_NAME=single-node-systemtest-admin-keys-volume
 
 echo "Generating keys to volume $VOLUME_NAME"
 docker volume create $VOLUME_NAME
